9ab51991ef424a9036681020820bc7acf7abf36c
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-01-27 16:48:34 +0100

    Minimal setup for persona fisica webapp

--- packages/pn-personafisica-webapp/src/App.tsx
+++ packages/pn-personafisica-webapp/src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { LoadingOverlay, Layout } from "@pagopa-pn/pn-commons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Layout assistanceEmail={"email@pagopa.it"}>
+    <LoadingOverlay />
+    <Routes>
+      <Route
+        path="/"
+        element={<h1>Welcome to Piattaforma Notifiche per il cittadino</h1>}
+      />
+    </Routes>
+  </Layout>
+);
 
 export default App;
---
+++ packages/pn-personafisica-webapp/src/redux/store.ts
@@ -0,0 +1,25 @@
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import { LOG_REDUX_ACTIONS } from '../utils/constants';
+import { appStateReducer } from './slices/appStateSlice';
+
+const additionalMiddlewares = [LOG_REDUX_ACTIONS ? logger : undefined];
+
+export const createStore = () =>
+    configureStore({
+        reducer: {
+            appState: appStateReducer,
+        },
+        middleware: (getDefaultMiddleware) =>
+            additionalMiddlewares.reduce(
+                (array, middleware) => (middleware ? array.concat(middleware) : array),
+                getDefaultMiddleware({ serializableCheck: false })
+            ),
+    });
+
+export const store = createStore();
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
