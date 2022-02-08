8528a0500cd1979f6c20777b4fa8cd48dca781f0
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-02-08 10:33:02 +0100

    WIP setup axios client for  notifications

--- packages/pn-pa-webapp/src/api/axios.ts
+++ packages/pn-pa-webapp/src/api/axios.ts
@@ -3,3 +3,23 @@ import axios from "axios";
 export const authClient = axios.create({
     baseURL: process.env.REACT_APP_AUTH_BASE_URL
 });
+
+export const apiClient = axios.create({
+    baseURL: process.env.REACT_APP_URL_API
+});
+
+apiClient.interceptors.request.use(function (config) {
+    // TODO add authorization token as soon as API are ready
+    // const token = localStorage.getItem("user");
+    // if (token && config.headers) {
+    //     config.headers["Authorization"] = 'Bearer ' + token;
+    // }
+    if (config.headers) {
+        config.headers['X-PagoPA-PN-PA'] = '';
+    }
+    return config;
+},
+    function (error) {
+        return Promise.reject(error);
+    }
+);
