685790ac7c35bec709870c707f36e55ac1870ff9
Liberti <vincenzo.liberti@dxc.com>
2022-02-14 08:49:51 +0100

    filters

--- packages/pn-pa-webapp/src/pages/components/Notifications/FilterNotificationsTable.tsx
+++ packages/pn-pa-webapp/src/pages/components/Notifications/FilterNotificationsTable.tsx
@@ -1,38 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { Formik, Form } from 'formik';
-import { Box, MenuItem, TextField } from '@mui/material';
+import { Box, Divider, MenuItem, TextField } from '@mui/material';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
+// import { NotificationStatus } from '../../../redux/dashboard/types';
 
 const FilterNotificationsTable = () => {
   const [searchForSelection, setSearchForSelection] = useState('');
   const [searchForValue, setSearchForValue] = useState('');
   const [fromDate, setFromDate] = React.useState<Date | null>(null);
   const [toDate, setToDate] = React.useState<Date | null>(null);
+  const [notificationStatus, setNotificationStatus] = useState('Tutti gli stati');
 
   const initialValues = { 1: Number, 2: Number, 3: Number };
+  // const notificationStatus = Object.values(NotificationStatus);
 
-  const searchForValues = [
-    {
-      value: 0,
-      label: 'Codice Fiscale',
-    },
-    {
-      value: 1,
-      label: 'Codice IUN',
-    },
+  const notificationAllowedStatus = [
+    { value: 0, label: 'Tutti gli stati' },
+    { value: 1, label: 'Depositata' },
+    { value: 1, label: 'Consegnata' },
+    { value: 1, label: 'In inoltro' },
+    { value: 1, label: 'Perfezionata per decorrenza termini' },
+    { value: 1, label: 'Perfezionata per visione' },
+    { value: 1, label: 'Pagata' },
+    { value: 1, label: 'Annullata' },
+    { value: 1, label: 'Destinatario irreperibile' },
   ];
 
-  const handleChangSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchForSelection(event.target.value);
-  };
+  const searchForValues = [
+    { value: 0, label: 'Codice Fiscale' },
+    { value: 1, label: 'Codice IUN' },
+  ];
 
-  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchForValue(event.target.value);
-  };
-
-  useEffect(() => {}, []);
+  useEffect(() => {
+    setSearchForSelection(searchForValues[0].label);
+    setNotificationStatus(notificationAllowedStatus[0].label);
+  }, []);
 
   return (
     <React.Fragment>
@@ -57,7 +61,9 @@ const FilterNotificationsTable = () => {
               select
               variant="outlined"
               value={searchForSelection}
-              onChange={handleChangSearch}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setSearchForSelection(event.target.value);
+              }}
             >
               {searchForValues.map((option) => (
                 <MenuItem key={option.value} value={option.value}>
@@ -69,9 +75,11 @@ const FilterNotificationsTable = () => {
               id="outlined-basic"
               variant="outlined"
               value={searchForValue}
-              onChange={handleChangeValue}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setSearchForValue(event.target.value);
+              }}
             />
-            
+            <Divider orientation="vertical" flexItem />
             <LocalizationProvider dateAdapter={AdapterDateFns}>
               <DesktopDatePicker
                 label="Da"
@@ -88,10 +96,28 @@ const FilterNotificationsTable = () => {
                 label="A"
                 inputFormat="MM/dd/yyyy"
                 value={toDate}
-                onChange={(newValue: Date | null) => {setToDate(newValue);}}
+                onChange={(newValue: Date | null) => {
+                  setToDate(newValue);
+                }}
                 renderInput={(params) => <TextField {...params} />}
               />
             </LocalizationProvider>
+            <TextField
+              id="outlined-basic"
+              label="Stato"
+              select
+              variant="outlined"
+              value={notificationStatus}
+              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                setNotificationStatus(event.target.value);
+              }}
+            >
+              {notificationAllowedStatus.map((option2) => (
+                <MenuItem key={option2.value} value={option2.value}>
+                  {option2.label}
+                </MenuItem>
+              ))}
+            </TextField>
             <button type="submit">Submit</button>
           </Box>
         </Form>
