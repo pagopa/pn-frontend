b95396f36562b54816d16ff8b5035967152cfe75
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-02-17 11:19:30 +0100

    chore: minimal refactoring to avoid duplication

--- packages/pn-personafisica-webapp/src/redux/auth/reducers.ts
+++ packages/pn-personafisica-webapp/src/redux/auth/reducers.ts
@@ -7,32 +7,19 @@ const userSlice = createSlice({
   name: 'userSlice',
   initialState: {
     loading: false,
-    // user: sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user') || '') : {
-    //     sessionToken: '',
-    //     family_name: '',
-    //     fiscal_number: '',
-    //     organization: {
-    //         id: '',
-    //         role: ''
-    //     }
-    // } as User
-    user: {
-      token: '',
-    } as User,
+    user: sessionStorage.getItem('user')
+      ? JSON.parse(sessionStorage.getItem('user') || '')
+      : ({
+          token: '',
+        } as User),
   },
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(exchangeToken.fulfilled, (state, action) => {
       state.user = action.payload;
     });
-    builder.addCase(exchangeToken.pending, (state) => {
-      state.loading = true;
-    });
     builder.addCase(logout.fulfilled, (state, action) => {
       state.user = action.payload;
-    });
-    builder.addCase(logout.pending, (state) => {
-      state.loading = true;
     });
   },
 });
