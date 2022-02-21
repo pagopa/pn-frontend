eea394ebb87e8bc87bd1e8919966849a5d363169
Liberti <vincenzo.liberti@dxc.com>
2022-02-21 16:08:58 +0100

    fix SQ hotspot fiscal code regex

--- packages/pn-pa-webapp/src/pages/components/Notifications/FilterNotificationsTable.tsx
+++ packages/pn-pa-webapp/src/pages/components/Notifications/FilterNotificationsTable.tsx
@@ -23,7 +23,7 @@ const FilterNotificationsTable = () => {
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
 
-  const fiscalCode_regex = /^(?:[A-Z][AEIOU][AEIOUX]|[AEIOU]X{2}|[B-DF-HJ-NP-TV-Z]{2}[A-Z]){2}(?:[\dLMNP-V]{2}(?:[A-EHLMPR-T](?:[04LQ][1-9MNP-V]|[15MR][\dLMNP-V]|[26NS][0-8LMNP-U])|[DHPS][37PT][0L]|[ACELMRT][37PT][01LM]|[AC-EHLMPR-T][26NS][9V])|(?:[02468LNQSU][048LQU]|[13579MPRTV][26NS])B[26NS][9V])(?:[A-MZ][1-9MNP-V][\dLMNP-V]{2}|[A-M][0L](?:[1-9MNP-V][\dLMNP-V]|[0L][1-9MNP-V]))[A-Z]$/i;
+  const fiscalCode_regex = /^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})$/i;
   // TODO inserire regex corretta
   const IUN_regex = /^[A-Z]$/i;
 
