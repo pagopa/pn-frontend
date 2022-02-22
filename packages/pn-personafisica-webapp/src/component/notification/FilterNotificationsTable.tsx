ce5037fa0dd91431b768f89bf5bd37c2e0a99241
Liberti <vincenzo.liberti@dxc.com>
2022-02-22 17:23:22 +0100

    creazione pagina visualizzazione notifiche PF - first commit

---
+++ packages/pn-personafisica-webapp/src/component/notification/FilterNotificationsTable.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+
+const FilterNotificationsTable = () => {
+
+  return (
+    <Fragment>
+      new
+    </Fragment>
+  );
+};
+
+export default FilterNotificationsTable;
