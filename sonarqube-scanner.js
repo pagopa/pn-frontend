2591c5c41b0b76d09901528e61d2197f8193dbe9
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-02-28 14:38:14 +0100

    modified sonar config

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,7 +2,6 @@ const scanner = require("sonarqube-scanner");
 scanner(
   {
     serverUrl: "https://sonarcloud.io",
-    //token: "c44a4934f4c5a35954974b7435a0731e43992785",
     options: {
       "sonar.organization": "pagopa",
       "sonar.projectKey": "pagopa_pn-frontend",
