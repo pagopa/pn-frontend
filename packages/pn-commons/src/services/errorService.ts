2b691efcc8376f71954e48e8e4f231f8737ef2d5
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-02-28 16:36:41 +0100

    chore: refactoring for error interceptor function

--- packages/pn-commons/src/services/errorService.ts
+++ packages/pn-commons/src/services/errorService.ts
@@ -1,4 +1,5 @@
 import { AppError } from '../types/AppError';
+import _ from 'lodash';
 
 export const handleErrors = (errors: Array<AppError>) => {
   errors
@@ -7,3 +8,24 @@ export const handleErrors = (errors: Array<AppError>) => {
       console.error('An error occurred: ', e);
     });
 };
+
+export const interceptErrors = (error: { response: { status: number; }; }): AppError => {
+  const e: AppError = {
+    id: _.uniqueId(),
+    title: '',
+    message: '',
+    blocking: false,
+    toNotify: true
+  };
+  if (error.response?.status === 404) {
+    e.title = 'Risorsa non trovata';
+    e.message = 'Si è verificato un errore. Si prega di riprovare più tardi';
+  } else if (error.response?.status === 403) {
+    e.title = 'Utente non autenticato';
+    e.message = 'L\'utente corrente non è autenticato';
+  } else {
+    e.title = 'Errore generico';
+    e.message = 'Si è verificato un errore. Si prega di riprovare più tardi';
+  }
+  throw e;
+}
