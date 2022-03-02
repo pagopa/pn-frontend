b7e749a811833e924353e4aca03db7eb6557ed3d
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-03-02 17:30:01 +0100

    chore: refactoring and test for error service and appStateSlice

--- packages/pn-commons/src/services/error.service.ts
+++ packages/pn-commons/src/services/error.service.ts
@@ -6,23 +6,23 @@ import _ from 'lodash';
  * @param  {{response:{status:number;};}} error
  * @returns AppError
  */
-export const createAppError = (error: any): AppError => {
+export const createAppError = (error: { response: { status: number } }): AppError => {
   const e: AppError = {
     id: _.uniqueId(),
     title: '',
     message: '',
     blocking: false,
-    toNotify: true
+    toNotify: true,
   };
   if (error.response?.status === 404) {
     e.title = 'Risorsa non trovata';
     e.message = 'Si è verificato un errore. Si prega di riprovare più tardi';
   } else if (error.response?.status === 403) {
     e.title = 'Utente non autenticato';
-    e.message = 'L\'utente corrente non è autenticato';
+    e.message = "L'utente corrente non è autenticato";
   } else {
     e.title = 'Errore generico';
     e.message = 'Si è verificato un errore. Si prega di riprovare più tardi';
   }
   return e;
-}
+};
