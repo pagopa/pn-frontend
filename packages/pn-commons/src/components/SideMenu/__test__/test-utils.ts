94eca7841862b9aec739b82eb23b56e264e89062
Andrea Cimini <andrea.cimini@keypartner.com>
2022-03-09 10:13:19 +0100

    Mobile - side menu

---
+++ packages/pn-commons/src/components/SideMenu/__test__/test-utils.ts
@@ -0,0 +1,35 @@
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+
+import { SideMenuItem } from "../../../types/SideMenuItem";
+
+export const sideMenuItems: Array<SideMenuItem> = [
+  { 
+    label: 'Item 1',
+    icon: QuestionMarkIcon,
+    route: 'mocked-route'
+  },
+  { 
+    label: 'Item 2',
+    icon: QuestionMarkIcon,
+    route: '',
+    children: [{
+      label: 'Item 2-1',
+      icon: QuestionMarkIcon,
+      route: ''
+    }]
+  },
+  { 
+    label: 'Item 3',
+    icon: QuestionMarkIcon,
+    route: '',
+    children: [{
+      label: 'Item 3-1',
+      icon: QuestionMarkIcon,
+      route: ''
+    }, {
+      label: 'Item 3-2',
+      icon: QuestionMarkIcon,
+      route: ''
+    }]
+  }
+];
