66e46c3bb410a4f3a1e372b1d563885eba6b8512
Andrea Cimini <andrea.cimini@keypartner.com>
2022-03-10 11:27:02 +0100

    Mobile - tests

--- packages/pn-pa-webapp/src/pages/__test__/Dashboard.page.test.tsx
+++ packages/pn-pa-webapp/src/pages/__test__/Dashboard.page.test.tsx
@@ -1,38 +1,41 @@
-import { act, screen } from '@testing-library/react';
-import * as redux from 'react-redux';
+import { screen } from '@testing-library/react';
 import { tenYearsAgo, today } from '@pagopa-pn/pn-commons';
 
 import { render } from '../../__test__/test-utils';
+import * as hooks from '../../redux/hooks';
+import { notificationsToFe } from '../../redux/dashboard/__test__/test-utils';
 import Dashboard from '../Dashboard.page';
 
 describe('Dashboard Page', () => {
-  it('renders dashboard page', async () => {
-    const spy = jest.spyOn(redux, 'useSelector');
-    spy.mockReturnValue({
-      notifications: [],
-      pagination: {
-        nextPagesKey: ['1'],
-        size: 0,
-        page: 0,
-        moreResult: false,
-      },
-      filters: {
+  beforeEach(() => {
+    const spy = jest.spyOn(hooks, 'useAppSelector');
+    spy
+      .mockReturnValueOnce(notificationsToFe.result)
+      .mockReturnValueOnce({
         startDate: tenYearsAgo.toISOString(),
         endDate: today.toISOString(),
         recipientId: '',
         status: '',
-        subjectRegExp: '',
-      },
-      sort: {
+        subjectRegExp: ''
+      })
+      .mockReturnValueOnce({
         orderBy: '',
         order: 'asc'
-     }
-    });
+      })
+      .mockReturnValueOnce({
+        nextPagesKey: [],
+        size: 10,
+        page: 0,
+        moreResult: false
+      });
+  });
 
-    await act( async () => {
-      render(<Dashboard/>);
-      expect(screen.getByRole('heading')).toHaveTextContent(/Notifiche/i);
-      spy.mockClear();
-    });    
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders dashboard page', () => {
+    render(<Dashboard />);
+    expect(screen.getByRole('heading')).toHaveTextContent(/Notifiche/i);
   });
 });
---
+++ packages/pn-pa-webapp/src/utils/__test__/status.utility.test.ts
@@ -0,0 +1,97 @@
+import { getNotificationStatusLabelAndColor, NotificationStatus } from "@pagopa-pn/pn-commons";
+
+import { NotificationStatusHistory, NotificationDetailTimeline, TimelineCategory } from "../../redux/notification/types";
+import { getNotificationStatusLabelAndColorFromTimelineCategory } from "../status.utility";
+
+const notificationStatusHistory: Array<NotificationStatusHistory> = [
+  {status: NotificationStatus.ACCEPTED, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-1']},
+  {status: NotificationStatus.DELIVERED, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-2']},
+  {status: NotificationStatus.REFUSED, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-3']},
+  {status: NotificationStatus.DELIVERING, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-4']},
+  {status: NotificationStatus.UNREACHABLE, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-5']},
+  {status: NotificationStatus.PAID, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-6']},
+  {status: NotificationStatus.IN_VALIDATION, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-7']},
+  {status: NotificationStatus.EFFECTIVE_DATE, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-8']},
+  {status: NotificationStatus.VIEWED, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-9']},
+  {status: NotificationStatus.CANCELED, activeFrom: '', relatedTimelineElements: ['mocked-timeline-id-10']},
+];
+
+const timelineStep: NotificationDetailTimeline = {
+  elementId: '',
+  timestamp: '',
+  category: TimelineCategory.ANALOG_FAILURE_WORKFLOW,
+  details: {
+    category: TimelineCategory.ANALOG_FAILURE_WORKFLOW,
+    taxdId: ''
+  }
+};
+
+test('return timeline step data - ACCEPTED', () => {
+  timelineStep.elementId = 'mocked-timeline-id-1';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.ACCEPTED);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - DELIVERED', () => {
+  timelineStep.elementId = 'mocked-timeline-id-2';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.DELIVERED);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - REFUSED', () => {
+  timelineStep.elementId = 'mocked-timeline-id-3';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.REFUSED);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - DELIVERING', () => {
+  timelineStep.elementId = 'mocked-timeline-id-4';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.DELIVERING);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - UNREACHABLE', () => {
+  timelineStep.elementId = 'mocked-timeline-id-5';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.UNREACHABLE);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - PAID', () => {
+  timelineStep.elementId = 'mocked-timeline-id-6';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.PAID);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - IN_VALIDATION', () => {
+  timelineStep.elementId = 'mocked-timeline-id-7';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.IN_VALIDATION);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - EFFECTIVE_DATE', () => {
+  timelineStep.elementId = 'mocked-timeline-id-8';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.EFFECTIVE_DATE);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - VIEWED', () => {
+  timelineStep.elementId = 'mocked-timeline-id-9';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.VIEWED);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
+
+test('return timeline step data - CANCELED', () => {
+  timelineStep.elementId = 'mocked-timeline-id-10';
+  const timeLineData = getNotificationStatusLabelAndColorFromTimelineCategory(timelineStep, notificationStatusHistory);
+  const notificationData = getNotificationStatusLabelAndColor(NotificationStatus.CANCELED);
+  expect(timeLineData).toStrictEqual(notificationData);
+});
