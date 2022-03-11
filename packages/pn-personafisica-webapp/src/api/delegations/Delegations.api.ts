088f88cfeaad792375d3f2ca41601189b1c79826
Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>
2022-03-11 09:45:05 +0100

    wip: add revoke delegation api call

--- packages/pn-personafisica-webapp/src/api/delegations/Delegations.api.ts
+++ packages/pn-personafisica-webapp/src/api/delegations/Delegations.api.ts
@@ -1,14 +1,18 @@
 import { AxiosPromise } from 'axios';
-import { authClient } from '../axios';
+import { apiClient, authClient } from '../axios';
 import { Delegation } from '../../redux/delegation/types';
 
 export const DelegatesApi = {
-   /*
+  /*
    * Get all the delegation for the authenticated user
    * @returns Promise array of delegations
    */
-    getDelegations : ():AxiosPromise<Array<Delegation>> => (
-        authClient.get('/delegations')
-    )
-
+  getDelegations: (): AxiosPromise<Array<Delegation>> => authClient.get('/delegations'),
+  revokeDelegation: (id: string): Promise<'success' | 'error'> =>
+    apiClient.patch(`/delegations/${id}/revoke`).then((response) => {
+      if (response.status === 204) {
+        return 'success';
+      }
+      return 'error';
+    }),
 };
--- packages/pn-personafisica-webapp/src/redux/delegation/actions.ts
+++ packages/pn-personafisica-webapp/src/redux/delegation/actions.ts
@@ -1,15 +1,27 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import {DelegationsList} from "./types";
-
+import { DelegatesApi } from '../../api/delegations/Delegations.api';
+import { DelegationsList } from './types';
 
 /**
  *
  */
 export const delegations = createAsyncThunk<DelegationsList, string>(
-    'delegations',
-    async (selfCareToken: string) => ({
-            delegators:[],
-            delegations: [],
-            isCompany: false
-        } as DelegationsList)
+  'delegations',
+  async (selfCareToken: string) =>
+    ({
+      delegators: [],
+      delegations: [],
+      isCompany: false,
+    } as DelegationsList)
+);
+
+export const revokeDelegation = createAsyncThunk<'success' | 'error', string>(
+  'revokeDelegation',
+  async (id: string, { rejectWithValue }) => {
+    try {
+      return await DelegatesApi.revokeDelegation(id);
+    } catch (e) {
+      return rejectWithValue(e);
+    }
+  }
 );
