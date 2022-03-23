b66a2716e49c9ada9cc1b02fec68b23ce09a6b3d
Boca Andrei <a.boca@appsonrails.it>
2022-03-23 12:06:25 +0100

    feat(NuovaDelega): campi obbligatori label

--- packages/pn-personafisica-webapp/src/pages/NuovaDelega.page.tsx
+++ packages/pn-personafisica-webapp/src/pages/NuovaDelega.page.tsx
@@ -92,6 +92,9 @@ const NuovaDelega = () => {
             variantTitle="h3"
             variantSubTitle="body1"
           />
+          <Typography sx={{mt: '1rem', mb:'1rem'}}>
+            Campi Obbligatori *
+          </Typography>
           <Card sx={{ padding: '30px', width: '80%', mt: 4 }}>
             <Typography sx={{ fontWeight: 'bold' }}>{t('nuovaDelega.form.personType')}</Typography>
             <Formik
