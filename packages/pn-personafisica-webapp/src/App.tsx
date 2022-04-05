533fb0d09cfb7c24c795383d5f3e6d49e8e78f40
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-04-05 17:21:33 +0200

    fix: merge commits

--- packages/pn-personafisica-webapp/src/App.tsx
+++ packages/pn-personafisica-webapp/src/App.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import MarkunreadMailboxIcon from '@mui/icons-material/MarkunreadMailbox';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import AltRouteIcon from '@mui/icons-material/AltRoute';
 import PersonIcon from '@mui/icons-material/Person';
 import { LoadingOverlay, Layout, AppMessage, SideMenu, SideMenuItem } from '@pagopa-pn/pn-commons';
@@ -8,39 +9,45 @@ import { useEffect, useState } from 'react';
 import * as routes from './navigation/routes.const';
 import Router from './navigation/routes';
 import { getNumberDelegator, logout } from './redux/auth/actions';
-import { useAppDispatch,useAppSelector } from './redux/hooks';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
 import { PAGOPA_HELP_EMAIL } from './utils/constants';
 import { RootState } from './redux/store';
-
 
 const App = () => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation('common');
   const [pendingDelegatorsState, setPendingDelegatorsState] = useState(0);
 
-  const {pendingDelegators, delegators} = useAppSelector((state: RootState) => state.userState);
+  const { pendingDelegators, delegators } = useAppSelector((state: RootState) => state.userState);
 
   useEffect(() => {
     void dispatch(getNumberDelegator());
-  },[]);
+  }, []);
 
   useEffect(() => {
     setPendingDelegatorsState(pendingDelegators);
-  },[pendingDelegators]);
+  }, [pendingDelegators]);
 
-  const AltRouteIconRotate = () => (
-    <AltRouteIcon sx={{ transform: 'rotate(90deg)' }}/>
-  );
-
-  const mapDelegatorSideMenuItem = delegators.map((delegator)=> ({ icon:PersonIcon ,label: `${delegator.delegator.firstName} ${delegator.delegator.lastName}` }));
-  
+  const mapDelegatorSideMenuItem = delegators.map((delegator) => ({
+    icon: PersonIcon,
+    label: `${delegator.delegator.firstName} ${delegator.delegator.lastName}`,
+  }));
 
   const menuItems: Array<SideMenuItem> = [
-    { label: t('menu.notifiche'), icon: MailOutlineIcon, route: routes.NOTIFICHE, children:mapDelegatorSideMenuItem.length ? mapDelegatorSideMenuItem : undefined},
-    { label: t('menu.recapiti'), icon: MarkunreadMailboxIcon, route: routes.PROFILO },
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    { label: t('menu.deleghe'), icon: AltRouteIconRotate, route: routes.DELEGHE, rightBadgeNotification: pendingDelegatorsState ? pendingDelegatorsState : undefined}, 
+    {
+      label: t('menu.notifiche'),
+      icon: MailOutlineIcon,
+      route: routes.NOTIFICHE,
+      children: mapDelegatorSideMenuItem.length ? mapDelegatorSideMenuItem : undefined,
+    },
+    { label: t('menu.contacts'), icon: MarkunreadMailboxIcon, route: routes.RECAPITI },
+    {
+      label: t('menu.deleghe'),
+      icon: AltRouteIcon,
+      route: routes.DELEGHE,
+      rightBadgeNotification: pendingDelegatorsState ? pendingDelegatorsState : undefined,
+    },
+    { label: t('menu.profilo'), icon: SettingsOutlinedIcon, route: routes.PROFILO },
   ];
 
   return (
