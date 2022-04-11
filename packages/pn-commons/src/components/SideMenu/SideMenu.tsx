ee943e485b3152333f62e6f4c7eda34291fcc592
CarlaDimatteo <98387924+CarlaDimatteo@users.noreply.github.com>
2022-04-11 12:10:01 +0200

    feat: pn-695 as a Backoffice user I want to navigate using the side menu (#78)

--- packages/pn-commons/src/components/SideMenu/SideMenu.tsx
+++ packages/pn-commons/src/components/SideMenu/SideMenu.tsx
@@ -1,34 +1,24 @@
 import { FC, Fragment, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Box, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import { Menu } from '@mui/icons-material';
 
 import { SideMenuItem } from '../../types/SideMenuItem';
 import { useIsMobile } from '../../hooks/IsMobile.hook';
 import SideMenuList from './SideMenuList';
 
-interface Props {
+type Props = {
   menuItems: Array<SideMenuItem>;
-}
+  selfCareItems?: Array<SideMenuItem>;
+};
 
-const CustomList = styled(List)(({ theme }) => ({
-  boxShadow: '0px 2px 4px -1px rgba(0, 43, 85, 0.1), 0px 4px 5px rgba(0, 43, 85, 0.05), 0px 1px 10px rgba(0, 43, 85, 0.1)',
-  '& .MuiListItemIcon-root': {
-    color: `${theme.palette.primary.main} !important`
-  },
-  '& .MuiTypography-root': {
-    color: `${theme.palette.primary.main} !important`
-  }
-}));
-
-const SideMenu: FC<Props> = ({ menuItems }) => {
+const SideMenu: FC<Props> = ({ menuItems, selfCareItems }) => {
   const [state, setState] = useState(false);
   const navigate = useNavigate();
   const location = useLocation();
   const isMobile = useIsMobile();
-  const menuItemSelected = menuItems.find(m => m.route === location.pathname) ||
-    menuItems.find(m => location.pathname.indexOf(m.route as string) > -1) || {label: 'Menu'};
+  const menuItemSelected = menuItems.find((m) => m.route === location.pathname) ||
+    menuItems.find((m) => location.pathname.indexOf(m.route as string) > -1) || { label: 'Menu' };
 
   const toggleDrawer = () => {
     setState(!state);
@@ -42,28 +32,47 @@ const SideMenu: FC<Props> = ({ menuItems }) => {
   };
 
   return (
-    <Box height={isMobile ? 'auto' : '100%'} display="flex" flexDirection="column" bgcolor={'common.white'}>
+    <Box
+      height={isMobile ? 'auto' : '100%'}
+      display="flex"
+      flexDirection="column"
+      bgcolor={'common.white'}
+    >
       <Box alignItems="left" display="flex" flexDirection="column">
         {isMobile ? (
           <Fragment>
-            <CustomList>
-              <ListItem button onClick={toggleDrawer}>
+            <List
+              role="navigation"
+              component="nav"
+              aria-label="main piattaforma-notifiche sender mobile"
+              sx={{
+                boxShadow:
+                  '0px 2px 4px -1px rgba(0, 43, 85, 0.1), 0px 4px 5px rgba(0, 43, 85, 0.05), 0px 1px 10px rgba(0, 43, 85, 0.1)',
+              }}
+            >
+              <ListItemButton onClick={toggleDrawer}>
                 <ListItemIcon>
                   <Menu />
                 </ListItemIcon>
-                <ListItemText primary={menuItemSelected?.label}/>
-              </ListItem>
-            </CustomList>
-            <Drawer
-              anchor="left"
-              open={state}
-              onClose={toggleDrawer}
-            >
-              {<SideMenuList menuItems={menuItems} handleLinkClick={handleNavigation}/>}
+                <ListItemText primary={menuItemSelected?.label} />
+              </ListItemButton>
+            </List>
+            <Drawer anchor="left" open={state} onClose={toggleDrawer}>
+              {
+                <SideMenuList
+                  menuItems={menuItems}
+                  selfCareItems={selfCareItems}
+                  handleLinkClick={handleNavigation}
+                />
+              }
             </Drawer>
           </Fragment>
-          ) : (
-          <SideMenuList menuItems={menuItems} handleLinkClick={handleNavigation}/>
+        ) : (
+          <SideMenuList
+            menuItems={menuItems}
+            selfCareItems={selfCareItems}
+            handleLinkClick={handleNavigation}
+          />
         )}
       </Box>
     </Box>
