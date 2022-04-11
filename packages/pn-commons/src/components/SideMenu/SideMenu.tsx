d9ccb485e205775397909478da2ddf38d3c8c0a6
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-04-11 12:00:23 +0200

    chore: fixed test and accessibility test

--- packages/pn-commons/src/components/SideMenu/SideMenu.tsx
+++ packages/pn-commons/src/components/SideMenu/SideMenu.tsx
@@ -1,7 +1,6 @@
 import { FC, Fragment, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Box, Drawer, List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import { Menu } from '@mui/icons-material';
 
 import { SideMenuItem } from '../../types/SideMenuItem';
@@ -12,17 +11,6 @@ type Props = {
   menuItems: Array<SideMenuItem>;
   selfCareItems?: Array<SideMenuItem>;
 };
-
-const CustomList = styled(List)(({ theme }) => ({
-  boxShadow:
-    '0px 2px 4px -1px rgba(0, 43, 85, 0.1), 0px 4px 5px rgba(0, 43, 85, 0.05), 0px 1px 10px rgba(0, 43, 85, 0.1)',
-  '& .MuiListItemIcon-root': {
-    color: `${theme.palette.primary.main} !important`,
-  },
-  '& .MuiTypography-root': {
-    color: `${theme.palette.primary.main} !important`,
-  },
-}));
 
 const SideMenu: FC<Props> = ({ menuItems, selfCareItems }) => {
   const [state, setState] = useState(false);
@@ -53,14 +41,22 @@ const SideMenu: FC<Props> = ({ menuItems, selfCareItems }) => {
       <Box alignItems="left" display="flex" flexDirection="column">
         {isMobile ? (
           <Fragment>
-            <CustomList>
+            <List
+              role="navigation"
+              component="nav"
+              aria-label="main piattaforma-notifiche sender mobile"
+              sx={{
+                boxShadow:
+                  '0px 2px 4px -1px rgba(0, 43, 85, 0.1), 0px 4px 5px rgba(0, 43, 85, 0.05), 0px 1px 10px rgba(0, 43, 85, 0.1)',
+              }}
+            >
               <ListItemButton onClick={toggleDrawer}>
                 <ListItemIcon>
                   <Menu />
                 </ListItemIcon>
                 <ListItemText primary={menuItemSelected?.label} />
               </ListItemButton>
-            </CustomList>
+            </List>
             <Drawer anchor="left" open={state} onClose={toggleDrawer}>
               {
                 <SideMenuList
--- packages/pn-personafisica-webapp/src/__test__/App.test.tsx
+++ packages/pn-personafisica-webapp/src/__test__/App.test.tsx
@@ -2,9 +2,12 @@ import { render, screen } from '@testing-library/react';
 import { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { axe, toHaveNoViolations } from 'jest-axe';
 import App from '../App';
 import i18n from '../i18n';
 import { store } from '../redux/store';
+
+expect.extend(toHaveNoViolations);
 
 describe('App', () => {
   beforeEach(async () => {
@@ -23,5 +26,11 @@ describe('App', () => {
     );
     const loading = screen.getByText(/loading.../i);
     expect(loading).toBeInTheDocument();
+  });
+
+  it.skip('Test if automatic accessibility tests passes', async () => {
+    const { container } = render(<App />);
+    const result = await axe(container);
+    expect(result).toHaveNoViolations();
   });
 });
