56248d0a8bc3bf26ee2a90d744c0e44cfaa35e9f
Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>
2022-04-12 12:02:31 +0200

    fix: add mobile view test to ConfirmDialog

--- packages/pn-personafisica-webapp/src/component/Deleghe/__test__/ConfirmationModal.test.tsx
+++ packages/pn-personafisica-webapp/src/component/Deleghe/__test__/ConfirmationModal.test.tsx
@@ -1,25 +1,23 @@
-import { fireEvent, RenderResult } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import * as hooks from '@pagopa-pn/pn-commons/src/hooks/IsMobile.hook';
 import { render } from '../../../__test__/test-utils';
+
 import ConfirmationModal from '../ConfirmationModal';
 
-jest.mock('@pagopa-pn/pn-commons', () => {
-  // this mock makes sure any components using the translate hook can use it without a warning being shown
-  const original = jest.requireActual('@pagopa-pn/pn-commons');
-  return {
-    ...original,
-    useIsMobile: () => false,
-  };
-});
+const useIsMobileSpy = jest.spyOn(hooks, 'useIsMobile');
 
 const mockCancelFunction = jest.fn();
 const mockConfirmFunction = jest.fn();
 
 describe('ConfirmationModal Component', () => {
-  // eslint-disable-next-line
-  let result: RenderResult | undefined;
+  afterEach(() => {
+    useIsMobileSpy.mockClear();
+    useIsMobileSpy.mockReset();
+  });
 
-  beforeEach(() => {
-    result = render(
+  it('renders the component', () => {
+    useIsMobileSpy.mockReturnValue(false);
+    const result = render(
       <ConfirmationModal
         open={true}
         title={'Test Title'}
@@ -29,14 +27,6 @@ describe('ConfirmationModal Component', () => {
         onCloseLabel={'Annulla'}
       />
     );
-  });
-
-  afterEach(() => {
-    result = undefined;
-    jest.resetAllMocks();
-  });
-
-  it('renders the component', () => {
     const dialog = result!.queryByRole('dialog');
 
     expect(dialog).toBeInTheDocument();
@@ -45,21 +35,101 @@ describe('ConfirmationModal Component', () => {
     expect(dialog).toHaveTextContent(/Annulla/i);
   });
 
-  it('checks that the confirm function is executed', () => {
+  it('checks that the confirm and cancel functions are executed', () => {
+    useIsMobileSpy.mockReturnValue(false);
+    const result = render(
+      <ConfirmationModal
+        open={true}
+        title={'Test Title'}
+        handleClose={mockCancelFunction}
+        onConfirm={mockConfirmFunction}
+        onConfirmLabel={'Conferma'}
+        onCloseLabel={'Cancella'}
+      />
+    );
     const confirm = result!.queryAllByTestId('dialogAction')[1];
-    expect(confirm).toBeInTheDocument();
+    const cancel = result!.queryAllByTestId('dialogAction')[0];
     expect(confirm).toHaveTextContent(/Conferma/i);
+    expect(cancel).toHaveTextContent(/Cancella/i);
 
     fireEvent.click(confirm);
     expect(mockConfirmFunction).toBeCalledTimes(1);
-  });
-
-  it('checks that the cancel function is executed', () => {
-    const cancel = result!.queryAllByTestId('dialogAction')[0];
-    expect(cancel).toBeInTheDocument();
-    expect(cancel).toHaveTextContent(/Annulla/i);
 
     fireEvent.click(cancel);
     expect(mockCancelFunction).toBeCalledTimes(1);
+  });
+
+  it('renders the dialog with default labels', () => {
+    useIsMobileSpy.mockReturnValue(false);
+
+    const result = render(
+      <ConfirmationModal
+        open={true}
+        title={'Test Title'}
+        handleClose={mockCancelFunction}
+        onConfirm={mockConfirmFunction}
+      />
+    );
+    const confirm = result!.queryAllByTestId('dialogAction')[1];
+    const cancel = result!.queryAllByTestId('dialogAction')[0];
+    expect(confirm).toHaveTextContent(/Riprova/i);
+    expect(cancel).toHaveTextContent(/Annulla/i);
+  });
+
+  it('renders the dialog with no confirm button', () => {
+    useIsMobileSpy.mockReturnValue(false);
+
+    const result = render(
+      <ConfirmationModal
+        open={true}
+        title={'Test Title'}
+        handleClose={mockCancelFunction}
+        onConfirm={mockConfirmFunction}
+      />
+    );
+    const cancel = result!.queryAllByTestId('dialogAction')[0];
+    expect(cancel).toHaveTextContent(/Annulla/i);
+  });
+
+  it('renders the dialog closed', () => {
+    useIsMobileSpy.mockReturnValue(false);
+
+    const result = render(
+      <ConfirmationModal
+        open={false}
+        title={'Test Title'}
+        handleClose={mockCancelFunction}
+        onConfirm={mockConfirmFunction}
+        onConfirmLabel={'Conferma'}
+        onCloseLabel={'Annulla'}
+      />
+    );
+    const dialog = result!.queryByRole('dialog');
+
+    expect(dialog).toBeNull();
+  });
+
+  it('renders the mobile view of the dialog', () => {
+    useIsMobileSpy.mockReturnValue(true);
+
+    const result = render(
+      <ConfirmationModal
+        open={true}
+        title={'Test Title'}
+        handleClose={mockCancelFunction}
+        onConfirm={mockConfirmFunction}
+        onConfirmLabel={'Conferma'}
+        onCloseLabel={'Annulla'}
+        minHeight={'6em'}
+        width={'20em'}
+      />
+    );
+    const buttons = result!.queryAllByTestId('dialogAction');
+    const dialog = result!.queryByRole('dialog');
+    const stack = result.queryByTestId('dialogStack');
+
+    expect(dialog).toBeInTheDocument();
+    expect(buttons).toHaveLength(2);
+    expect(stack).toHaveStyle('flex-direction: column');
   });
 });
