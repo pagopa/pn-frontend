9281561e68fe72f134f517a6a6974d5fd262c678
Boca Andrei <a.boca@appsonrails.it>
2022-04-13 16:35:34 +0200

    feat/fix: refactoring of delegation interface and delegations utils function testing

--- packages/pn-personafisica-webapp/src/redux/delegation/types.ts
+++ packages/pn-personafisica-webapp/src/redux/delegation/types.ts
@@ -8,10 +8,8 @@ export interface DelegationsList {
   isCompany: boolean;
 }
 
-export interface Delegation {
+export interface IDelegation {
   mandateId: string;
-  delegator: Person;
-  delegate: Person;
   status: 'active' | 'pending';
   visibilityIds: Array<{
     name: string;
@@ -21,6 +19,18 @@ export interface Delegation {
   datefrom: string;
   dateto: string;
 }
+
+export interface Delegator extends IDelegation {
+  delegator: Person;
+}
+
+export interface Delegate extends IDelegation {
+  delegate: Person;
+}
+
+
+export type Delegation = Delegator | Delegate;
+
 
 export interface OrganizationId {
   id: string;
@@ -80,3 +90,4 @@ export interface CreateDelegationResponse {
     uniqueIdentifier: string;
   }>;
 }
+
--- packages/pn-personafisica-webapp/src/utils/delegation.utility.ts
+++ packages/pn-personafisica-webapp/src/utils/delegation.utility.ts
@@ -1,25 +1,28 @@
 import { formatDate, Item } from '@pagopa-pn/pn-commons';
+import {Delegation, Person } from '../redux/delegation/types';
 
-import { Delegation, Person } from '../redux/delegation/types';
+
 /**
  * Maps Delegation object to Item, in order to be visualised in an ItemsCard or ItemsTable component
  * @param  {Array<Delegation>} delegations
  * @param  {boolean} isDelegator
  * @returns Array<Item>
  */
+
 export default function delegationToItem(
   delegations: Array<Delegation>,
-  isDelegator: boolean
 ): Array<Item> {
   // TODO to be tested
   return delegations.map((delegation: Delegation) => ({
     id: delegation.mandateId,
-    name: isDelegator
-      ? `${delegation.delegator.firstName} ${delegation.delegator.lastName}`
-      : `${delegation.delegate.firstName} ${delegation.delegate.lastName}`,
+    name: 'delegator' in delegation && delegation.delegator
+      ? `${delegation.delegator?.firstName} ${delegation.delegator?.lastName}`
+      : 'delegate' in delegation && delegation.delegate 
+      ? `${delegation.delegate?.firstName} ${delegation.delegate?.lastName}`
+      : '',
     startDate: formatDate(delegation.datefrom),
     endDate: formatDate(delegation.dateto),
-    email: isDelegator ? delegation.delegator.email : delegation.delegate.email,
+    email: getEmailFromDelegation(delegation),
     visibilityIds: delegation.visibilityIds.map(
       (entity: { name: string; uniqueIdentifier: string }) => entity.uniqueIdentifier
     ),
@@ -28,31 +31,52 @@ export default function delegationToItem(
   }));
 }
 
+export const getEmailFromDelegation = (delegation: Delegation): string => {
+  if ('delegator' in delegation && delegation.delegator) {
+    return delegation.delegator.email;
+  }
+  if ('delegate' in delegation && delegation.delegate) {
+    return delegation.delegate.email;
+  }
+  return '';
+};
+
 export function generateVCode() {
   const crypto = window.crypto;
   const array = new Uint32Array(1);
   return crypto.getRandomValues(array).toString().slice(0, 5);
 }
 
+
+// eslint-disable-next-line sonarjs/cognitive-complexity
 export function compareDelegationsStrings(
   a: Delegation,
   b: Delegation,
   orderAttr: string,
-  key: 'delegate' | 'delegator'
 ) {
   // TODO: change when displayName can be retrieved
-  const delegate1 =
-    orderAttr === 'name' ? a[key].firstName.toLowerCase() : a[key][orderAttr as keyof Person] || '';
-  const delegate2 =
-    orderAttr === 'name' ? b[key].firstName.toLowerCase() : b[key][orderAttr as keyof Person] || '';
-  return delegate1 < delegate2 ? 1 : -1;
+  if (('delegator' in a && (a.delegator)) && ('delegator' in b && (b.delegator))) {
+    const delegator1 =
+      orderAttr === 'name' ? a.delegator.firstName.toLowerCase() : a.delegator[orderAttr as keyof Person] || '';
+    const delegator2 =
+      orderAttr === 'name' ? b.delegator.firstName.toLowerCase() : b.delegator[orderAttr as keyof Person] || '';
+    return delegator1 < delegator2 ? 1 : -1;
+  }
+  if (('delegate' in a && (a.delegate)) && ('delegate' in b && (b.delegate))) {
+    const delegate1 =
+      orderAttr === 'name' ? a.delegate.firstName.toLowerCase() : a.delegate[orderAttr as keyof Person] || '';
+    const delegate2 =
+      orderAttr === 'name' ? b.delegate.firstName.toLowerCase() : b.delegate[orderAttr as keyof Person] || '';
+    return delegate1 < delegate2 ? 1 : -1;
+  }
+  return 0;
 }
+
 
 export function sortDelegations(
   order: string,
   sortAttr: string,
   values: Array<Delegation>,
-  isDelegate: boolean
 ) {
   /* eslint-disable-next-line functional/immutable-data */
   return values.sort((a: Delegation, b: Delegation) => {
@@ -61,10 +85,10 @@ export function sortDelegations(
       const dateA = new Date(a.dateto).getTime();
       const dateB = new Date(b.dateto).getTime();
       return orderDirection * (dateB - dateA);
-    }
+    };
     return (
       orderDirection *
-      compareDelegationsStrings(a, b, sortAttr, isDelegate ? 'delegate' : 'delegator')
+      compareDelegationsStrings(a, b, sortAttr)
     );
   });
 }
