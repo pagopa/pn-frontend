f9fc046b7eca70fb67dea0a540eb195d7f614178
Andri Boca <100524063+AndriBoca@users.noreply.github.com>
2022-05-04 16:44:14 +0200

    feat: pn-1149 add disclaimer and download content componets jsx (#119)
    * feat: add disclaimer and download content componets jsx
    
    * chore: refactoring, tests fixing
    
    Co-authored-by: CarlaDimatteo <carlotta.dimatteo@dgsspa.com>

---
+++ packages/pn-commons/src/components/NotificationDetail/HelpNotificationDetails.tsx
@@ -0,0 +1,56 @@
+import { Grid, Typography, Divider } from '@mui/material';
+import { ButtonNaked } from "@pagopa/mui-italia";
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import WebIcon from '@mui/icons-material/Web';
+
+interface HelpNotificationDetailsProps {
+    title: string;
+    subtitle: string;
+    courtName: string;
+    phoneNumber: string;
+    mail: string;
+    website: string;
+};
+
+/**
+ * Getting help for the user's notification
+ * @param title card title
+ * @param subtitle card subtitle
+ * @param courtName court name that user need to contact in order to get help
+ * @param phoneNumber the phone number of the court
+ * @param mail the email of the court
+ * @param website the website of the court
+ */
+
+const HelpNotificationDetails: React.FC<HelpNotificationDetailsProps> = ({ title,subtitle,courtName, phoneNumber, mail, website }) => (
+    <>
+        <Grid container direction="row" justifyContent="space-between" alignItems="center">
+            <Grid item>
+                <Typography color="text.primary" fontWeight={700} textTransform="uppercase" fontSize={14}>
+                    {title.toUpperCase()}
+                </Typography>
+            </Grid>
+        </Grid>
+        <Typography variant="body1" sx={{ mt: 1 }}>
+            {subtitle} {courtName}.
+        </Typography>
+        <Grid container direction="row" alignItems="center" mt={2}>
+            <ButtonNaked color="primary" startIcon={<LocalPhoneIcon />} href={`tel:${phoneNumber}`}>
+                 {phoneNumber}
+            </ButtonNaked>
+            <ButtonNaked color="primary" startIcon={<MailOutlineIcon />} sx={{ ml: 2 }} href={`mailto:${mail}`} target="_blank">
+                {mail}
+            </ButtonNaked>
+            <ButtonNaked color="primary" startIcon={<WebIcon />} sx={{ ml: 2 }} href={website}>
+                Vai al sito
+            </ButtonNaked>
+        </Grid>
+        <Divider sx={{ mt: 2 }} />
+        <Typography variant="body1" sx={{ mt: 2 }}>
+            Per problemi tecnici o domande relative a Piattaforma Notifiche, premi su “Assisistenza” in alto a destra.
+        </Typography>
+    </>
+);
+
+export default HelpNotificationDetails;
--- packages/pn-commons/src/components/NotificationDetail/NotificationDetailDocuments.tsx
+++ packages/pn-commons/src/components/NotificationDetail/NotificationDetailDocuments.tsx
@@ -1,13 +1,15 @@
 import { Fragment } from 'react';
-import { Grid, Typography, Button } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
-
+// import DownloadIcon from '@mui/icons-material/Download';
+import { ButtonNaked } from '@pagopa/mui-italia';
 import { NotificationDetailDocument } from '../../types/NotificationDetail';
-
 type Props = {
   title: string;
   documents: Array<NotificationDetailDocument>;
   clickHandler: (documentIndex: number) => void;
+  documentsAvailable: boolean;
+  downloadFilesMessage?: string;
 };
 
 /**
@@ -15,29 +17,59 @@ type Props = {
  * @param title title to show
  * @param documents data to show
  * @param clickHandler function called when user clicks on the download button
+ * @param documentsAvailable flag that allows download file or not (after 120 days)
  */
-const NotificationDetailDocuments = ({ title, documents, clickHandler }: Props) => {
 
-  return (
-    <Fragment>
-      <Grid container direction="row" justifyContent="space-between" alignItems="center">
-        <Grid item>
-          <Typography color="text.primary" fontWeight={700} textTransform="uppercase" fontSize={14}>
-            {title}
-          </Typography>
-        </Grid>
-        {/* TODO: ripristinare quando sarà completata la issue pn-720 */}
-        {/* <Grid item>
-          <Button startIcon={<DownloadIcon />}>Scarica tutti gli Atti</Button>
-        </Grid> */}
+const NotificationDetailDocuments = ({
+  title,
+  documents,
+  clickHandler,
+  documentsAvailable = false,
+  downloadFilesMessage,
+}: Props) => (
+  <Fragment>
+    <Grid
+      key={'files-section'}
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Grid key={'detail-documents-title'} item>
+        <Typography color="text.primary" fontWeight={700} textTransform="uppercase" fontSize={14}>
+          {title}
+        </Typography>
       </Grid>
-      {documents.map((d, i) => (
-        <Button data-testid="documentButton" key={d.digests.sha256} startIcon={<AttachFileIcon />} onClick={() => clickHandler(i)}>
+      {/* TODO: ripristinare quando sarà completata la issue pn-720 */}
+      {/* !documentsAvailable &&
+        <Grid item>
+          <Button startIcon={<DownloadIcon />}>Scarica tutti gli Atti</Button>
+        </Grid>
+      */}
+    </Grid>
+    <Grid key={'detail-documents-message'} item>
+      {downloadFilesMessage && <Typography variant="body1">{downloadFilesMessage}</Typography>}
+    </Grid>
+    <Grid sx={{ mt: 1 }} key={'download-files-section'} />
+    {documents.map((d, i) =>
+      !documentsAvailable ? (
+        <Typography key={d.digests.sha256}>{d.title}</Typography>
+      ) : (
+        <ButtonNaked
+          data-testid="documentButton"
+          key={d.digests.sha256}
+          color={'primary'}
+          startIcon={<AttachFileIcon />}
+          onClick={() => clickHandler(i)}
+        >
           {d.title}
-        </Button>
-      ))}
-    </Fragment>
-  );
-};
+          <Typography sx={{ fontWeight: 600, ml: '10px' }}>
+            {'650 KB'} {/* TODO: integrate specific dimension of file */}
+          </Typography>
+        </ButtonNaked>
+      )
+    )}
+  </Fragment>
+);
 
 export default NotificationDetailDocuments;
