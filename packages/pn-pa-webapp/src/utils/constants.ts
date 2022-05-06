7c95f44a5df327d2a7d04cc03d5473d13de48187
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-05-06 12:37:35 +0200

    feat: pn-774 removed env variables for pa

--- packages/pn-pa-webapp/src/utils/constants.ts
+++ packages/pn-pa-webapp/src/utils/constants.ts
@@ -8,4 +8,3 @@ export const API_BASE_URL = process.env.REACT_APP_URL_API;
 export const SELFCARE_BASE_URL = process.env.REACT_APP_URL_SELFCARE_BASE;
 export const SELFCARE_URL_FE_LOGIN = process.env.REACT_APP_URL_SELFCARE_LOGIN;
 export const PAGOPA_HELP_EMAIL = process.env.REACT_APP_PAGOPA_HELP_EMAIL;
-export const ENABLE_ASSISTANCE = process.env.REACT_APP_ENABLE_ASSISTANCE === 'true';
