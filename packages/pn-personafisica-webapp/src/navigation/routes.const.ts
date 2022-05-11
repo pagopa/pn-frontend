98f926f3a17dd5631c65b3a3df5286b3f9e12bdd
Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>
2022-05-11 08:48:44 +0200

    fix: changed route with fiscalCode to mandateId

--- packages/pn-personafisica-webapp/src/navigation/routes.const.ts
+++ packages/pn-personafisica-webapp/src/navigation/routes.const.ts
@@ -7,4 +7,4 @@ export const DETTAGLIO_NOTIFICA = `${NOTIFICHE}/:id${NOTIFICA}`;
 export const NUOVA_DELEGA = `${DELEGHE}/nuova`;
 export const GET_DETTAGLIO_NOTIFICA_PATH = (id: string) => `${NOTIFICHE}/${id}${NOTIFICA}`;
 export const RECAPITI = '/recapiti';
-export const GET_NOTIFICHE_DELEGATO_PATH = (fiscalCode: string) => `${NOTIFICHE}/${fiscalCode}`;
+export const GET_NOTIFICHE_DELEGATO_PATH = (mandateId: string) => `${NOTIFICHE}/${mandateId}`;
