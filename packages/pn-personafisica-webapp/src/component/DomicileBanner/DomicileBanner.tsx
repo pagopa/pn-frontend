754a434fc7db2e39ec0389c8eebe90f86e4beba6
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-05-18 13:32:04 +0200

    feat: integration for domicile banner

--- packages/pn-personafisica-webapp/src/component/DomicileBanner/DomicileBanner.tsx
+++ packages/pn-personafisica-webapp/src/component/DomicileBanner/DomicileBanner.tsx
@@ -1,14 +1,17 @@
 import { Alert, Box, Link, Typography } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 import * as routes from '../../navigation/routes.const';
+import { useAppSelector } from '../../redux/hooks';
+import { RootState } from '../../redux/store';
 
 const DomicileBanner = () => {
   const { t } = useTranslation(['notifiche']);
   const navigate = useNavigate();
   const [open, setOpen] = useState(true);
+  const legalDomicile = useAppSelector((state: RootState) => state.generalInfoState.legalDomicile);
   const messageIndex = Math.floor(Math.random() * 4);
   const messages = [
     'detail.domicile_1',
@@ -23,6 +26,12 @@ const DomicileBanner = () => {
   const handleAddDomicile = () => {
     navigate(routes.RECAPITI);
   };
+
+  useEffect(() => {
+    if (legalDomicile && legalDomicile.length > 0) {
+      setOpen(false);
+    }
+  }, [legalDomicile]);
 
   return open ? (
     <Box mb={2.5}>
