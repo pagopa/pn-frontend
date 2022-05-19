30bd88c522b360fd96e6bc5bc894686e864ca37d
Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>
2022-05-19 15:48:34 +0200

    fix: remove analytics variables from env.ts

--- packages/pn-personafisica-login/src/utils/env.ts
+++ packages/pn-personafisica-login/src/utils/env.ts
@@ -32,14 +32,14 @@ export const ENV = {
 
   SPID_CIE_ENTITY_ID: env.get('REACT_APP_SPID_CIE_ENTITY_ID').required().asString(),
 
-  ANALYTCS: {
-    ENABLE: env.get('REACT_APP_ANALYTICS_ENABLE').default('false').asBool(),
-    MOCK: env.get('REACT_APP_ANALYTICS_MOCK').default('false').asBool(),
-    DEBUG: env.get('REACT_APP_ANALYTICS_DEBUG').default('false').asBool(),
-    TOKEN: env.get('REACT_APP_MIXPANEL_TOKEN').required().asString(),
-    API_HOST: env
-      .get('REACT_APP_MIXPANEL_API_HOST')
-      .default('https://api-eu.mixpanel.com')
-      .asString(),
-  },
+  // ANALYTCS: {
+  //   ENABLE: env.get('REACT_APP_ANALYTICS_ENABLE').default('false').asBool(),
+  //   MOCK: env.get('REACT_APP_ANALYTICS_MOCK').default('false').asBool(),
+  //   DEBUG: env.get('REACT_APP_ANALYTICS_DEBUG').default('false').asBool(),
+  //   TOKEN: env.get('REACT_APP_MIXPANEL_TOKEN').required().asString(),
+  //   API_HOST: env
+  //     .get('REACT_APP_MIXPANEL_API_HOST')
+  //     .default('https://api-eu.mixpanel.com')
+  //     .asString(),
+  // },
 };
