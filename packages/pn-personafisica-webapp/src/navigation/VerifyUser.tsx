ccfcdcdc47cef033f5c71055ff945253933af4f9
Lorenzo Cocciarelli <100523847+LorenzoCocciarelli@users.noreply.github.com>
2022-06-01 13:05:54 +0200

    feat: pn-1432 Gestione codice errato in pagina dei recapiti

--- packages/pn-personafisica-webapp/src/navigation/VerifyUser.tsx
+++ packages/pn-personafisica-webapp/src/navigation/VerifyUser.tsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { exchangeToken } from '../redux/auth/actions';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { RootState } from '../redux/store';
 import { goToLogin } from './navigation.utility';
+import * as routes from './routes.const';
 
 const VerifyUser = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const [spidToken, setSpidToken] = useState('');
   const dispatch = useAppDispatch();
   const token = useAppSelector((state: RootState) => state.userState.user.sessionToken);
+  const { tos, fetchedTos } = useAppSelector((state: RootState) => state.userState);
 
   useEffect(() => {
     const params = new URLSearchParams(location.hash);
@@ -31,6 +34,15 @@ const VerifyUser = () => {
       });
     }
   }, [spidToken]);
+
+  useEffect(() => {
+    if (token !== '' && fetchedTos && !tos) {
+      navigate(routes.TOS);
+    }
+    if (token !== '' && fetchedTos && tos && location.pathname === '/') {
+      navigate(routes.NOTIFICHE);
+    }
+  }, [fetchedTos, tos]);
 
   return <Outlet />;
 };
