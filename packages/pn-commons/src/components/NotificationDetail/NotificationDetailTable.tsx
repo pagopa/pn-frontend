a769ccbdde98aaa82748d7b20a09566a45437140
Giornetta Nicola <nicola.giornetta@dgsspa.com>
2022-06-13 12:28:44 +0200

    css fix in NotificationDetailTable component

--- packages/pn-commons/src/components/NotificationDetail/NotificationDetailTable.tsx
+++ packages/pn-commons/src/components/NotificationDetail/NotificationDetailTable.tsx
@@ -9,21 +9,19 @@ type Props = {
  * Table with the details of a notification
  * @param rows data to show
  */
-const NotificationDetailTable = ({ rows }: Props) => {
-  return (
-    <TableContainer component={Paper} sx={{ margin: '20px 0' }} className="paperContainer">
-      <Table aria-label="Dettaglio notifica">
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell>{row.label}</TableCell>
-              <TableCell>{row.value}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-};
+const NotificationDetailTable = ({ rows }: Props) => (
+  <TableContainer component={Paper} sx={{ margin: '20px 0', padding: '16px 0' }} className="paperContainer">
+    <Table aria-label="Dettaglio notifica">
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.id} sx={{ '&:last-child td, &:last-child th': { border: 0 }}}>
+            <TableCell sx={{borderBottom: 'none', padding: '8px 16px'}}>{row.label}</TableCell>
+            <TableCell sx={{borderBottom: 'none', padding: '8px 16px'}}>{row.value}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
 
 export default NotificationDetailTable;
