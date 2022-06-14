78bd25738729f161d0ed27db3168afd40a890984
Maurizio Flauti <100599605+maurizioflauti@users.noreply.github.com>
2022-06-14 16:34:55 +0200

    feat: pn-1428 Gestione verifica avviso / Checkout

---
+++ packages/pn-commons/src/components/CopyToClipboard.tsx
@@ -0,0 +1,40 @@
+import { Button, Link, SxProps, Theme } from "@mui/material";
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { useIsMobile } from "../hooks/IsMobile";
+
+interface Props {
+  /** callback used to retrieve the text to be copied */
+  getValue: () => string;
+  /** an optional text to be displayed near the "copy to clipboard" icon */
+  text?: string;
+}
+
+const CopyToClipboard: React.FC<Props> = ({ getValue, text }) => {
+  const alertButtonStyle: SxProps<Theme> = useIsMobile()
+    ? { textAlign: 'center' }
+    : { textAlign: 'center', minWidth: 'max-content' };
+
+  const doCopyToClipboard = async () => {
+    const value = getValue();
+
+    if ('clipboard' in navigator) {
+      return await navigator.clipboard.writeText(value);
+    } else {
+      return document.execCommand('copy', true, value);
+    }
+  };
+  
+  return (
+    <Button
+      component={Link}
+      color="primary"
+      sx={alertButtonStyle}
+      onClick={doCopyToClipboard}
+    >
+      <ContentCopyIcon fontSize="small" sx={{ m: '5px'}} />
+      {text}
+    </Button>
+  );
+};
+
+export default CopyToClipboard;
