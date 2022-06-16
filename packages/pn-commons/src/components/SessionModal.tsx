edbe5a71f80414f2fdd0ceeb73614c3a3669b50b
Giornetta Nicola <nicola.giornetta@dgsspa.com>
2022-06-16 11:59:35 +0200

    Modified values and types of padding elements

--- packages/pn-commons/src/components/SessionModal.tsx
+++ packages/pn-commons/src/components/SessionModal.tsx
@@ -14,7 +14,7 @@ type Props = {
 
 /**
  * Session Modal to handle end of session or unauthenticated scenarios.
- * @param open boolean - if modal is open or no 
+ * @param open boolean - if modal is open or no
  * @param title string - title of the modal
  * @param message string - message to show inside the modal
  * @param onConfirm action to perform when user click on "confirm" button
@@ -33,14 +33,13 @@ const SessionModal = ({
 
   return (
     <Dialog open={open} onClose={handleClose} aria-labelledby="session-dialog-title">
-      <DialogTitle sx={{ textAlign: 'center' }}>{title}</DialogTitle>
-      <DialogContentText
-        id="session-dialog-description"
-        sx={{ textAlign: 'center', margin: '10px 0', padding: '0 24px' }}
-      >
+      <DialogTitle sx={{ textAlign: 'center', pt: 3 }}>{title}</DialogTitle>
+      <DialogContentText id="session-dialog-description" sx={{ textAlign: 'center', px: 3, pb: 1 }}>
         {message}
       </DialogContentText>
-      <DialogActions sx={{ textAlign: 'center', flexDirection: isMobile ? 'column' : 'row', padding: '24px' }}>
+      <DialogActions
+        sx={{ textAlign: 'center', flexDirection: isMobile ? 'column' : 'row', padding: 3 }}
+      >
         {onConfirm && (
           <Button sx={{ width: '100%' }} color="primary" variant="contained" onClick={onConfirm}>
             {onConfirmLabel}
