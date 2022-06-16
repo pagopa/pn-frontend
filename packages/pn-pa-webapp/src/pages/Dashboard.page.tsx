8353ba8d4441ad63680a3c72a190a5f98ce8fb4c
Andrea Cimini <andrea.cimini@keypartner.com>
2022-06-16 16:56:58 +0200

    Dekstop version

--- packages/pn-pa-webapp/src/pages/Dashboard.page.tsx
+++ packages/pn-pa-webapp/src/pages/Dashboard.page.tsx
@@ -1,24 +1,15 @@
-import { useEffect, Fragment, useRef } from 'react';
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
   calculatePages,
   CustomPagination,
-  getNotificationStatusInfos,
-  NotificationStatus,
   PaginationData,
-  Notification,
-  Column,
   Sort,
-  StatusTooltip,
-  ItemsTable,
-  EmptyState,
-  Item,
   tenYearsAgo,
   today,
-  useIsMobile
+  useIsMobile,
 } from '@pagopa-pn/pn-commons';
 import { Box, Button, Typography } from '@mui/material';
-import { Tag, TagGroup } from '@pagopa/mui-italia';
 
 import * as routes from '../navigation/routes.const';
 import { RootState } from '../redux/store';
@@ -29,9 +20,8 @@ import {
   setPagination,
   setSorting,
 } from '../redux/dashboard/actions';
-import { trackEventByType } from '../utils/mixpanel';
-import { TrackEventType } from '../utils/events';
-import FilterNotificationsTable from './components/Notifications/FilterNotificationsTable';
+import DesktopNotifications from './components/Notifications/DesktopNotifications';
+import MobileNotifications from './components/Notifications/MobileNotifications';
 
 const Dashboard = () => {
   const dispatch = useAppDispatch();
@@ -40,7 +30,6 @@ const Dashboard = () => {
   const sort = useAppSelector((state: RootState) => state.dashboardState.sort);
   const pagination = useAppSelector((state: RootState) => state.dashboardState.pagination);
   const navigate = useNavigate();
-  const filterNotificationsTableRef = useRef({ filtersApplied: false });
   const isMobile = useIsMobile();
   // back end return at most the next three pages
   // we have flag moreResult to check if there are more pages
@@ -57,92 +46,6 @@ const Dashboard = () => {
     pagination.page + 1
   );
 
-  const columns: Array<Column> = [
-    {
-      id: 'sentAt',
-      label: 'Data',
-      width: '11%',
-      sortable: false, // TODO: will be re-enabled in PN-1124
-      getCellLabel(value: string) {
-        return value;
-      },
-      onClick(row: Item, column: Column) {
-        handleRowClick(row, column);
-      },
-    },
-    {
-      id: 'recipients',
-      label: 'Destinatario',
-      width: '13%',
-      sortable: false, // TODO: will be re-enabled in PN-1124
-      getCellLabel(value: Array<string>) {
-        return value.map((v) => (
-          <Typography key={v} variant="body2">
-            {v}
-          </Typography>
-        ));
-      },
-      onClick(row: Item, column: Column) {
-        handleRowClick(row, column);
-      },
-    },
-    {
-      id: 'subject',
-      label: 'Oggetto',
-      width: '23%',
-      getCellLabel(value: string) {
-        return value.length > 65 ? value.substring(0, 65) + '...' : value;
-      },
-      onClick(row: Item, column: Column) {
-        handleRowClick(row, column);
-      },
-    },
-    {
-      id: 'iun',
-      label: 'Codice IUN',
-      width: '20%',
-      getCellLabel(value: string) {
-        return value;
-      },
-      onClick(row: Item, column: Column) {
-        handleRowClick(row, column);
-      },
-    },
-    {
-      id: 'group',
-      label: 'Gruppi',
-      width: '15%',
-      getCellLabel(value: string) {
-        return (
-          value && (
-            <TagGroup visibleItems={4}>
-              <Tag value={value} />
-            </TagGroup>
-          )
-        );
-      },
-      onClick(row: Item, column: Column) {
-        handleRowClick(row, column);
-      },
-    },
-    {
-      id: 'notificationStatus',
-      label: 'Stato',
-      width: '18%',
-      align: 'center',
-      sortable: false, // TODO: will be re-enabled in PN-1124
-      getCellLabel(value: string) {
-        const { label, tooltip, color } = getNotificationStatusInfos(value as NotificationStatus);
-        return <StatusTooltip label={label} tooltip={tooltip} color={color}></StatusTooltip>;
-      },
-    },
-  ];
-
-  const rows: Array<Item> = notifications.map((n: Notification, i: number) => ({
-    ...n,
-    id: i.toString(),
-  }));
-
   // Pagination handlers
   const handleChangePage = (paginationData: PaginationData) => {
     dispatch(setPagination({ size: paginationData.size, page: paginationData.page }));
@@ -151,13 +54,6 @@ const Dashboard = () => {
   // Sort handlers
   const handleChangeSorting = (s: Sort) => {
     dispatch(setSorting(s));
-  };
-
-  // Navigation handlers
-  const handleRowClick = (row: Item, _column: Column) => {
-    navigate(routes.GET_DETTAGLIO_NOTIFICA_PATH(row.iun as string));
-    // log event
-    trackEventByType(TrackEventType.NOTIFICATIONS_GO_TO_DETAIL);
   };
 
   // Remove filter
@@ -173,25 +69,14 @@ const Dashboard = () => {
     );
   };
 
-  // route to API keys
-  const handleRouteApiKeys = () => {
-    navigate(routes.API_KEYS);
-  };
-
   // route to Manual Send
   const handleRouteManualSend = () => {
     navigate(routes.NUOVA_NOTIFICA);
   };
 
-  const emptyMessage: string = "L'ente non ha ancora inviato nessuna notifica. Usa le";
-  const emptyActionLabel: string = 'Chiavi API';
-
-  const secondaryMessage: object = {
-    emptyMessage: 'o fai un',
-    emptyActionLabel: 'invio manuale',
-    emptyActionCallback: () => {
-      handleRouteManualSend();
-    },
+  // route to API keys
+  const handleRouteApiKeys = () => {
+    navigate(routes.API_KEYS);
   };
 
   useEffect(() => {
@@ -204,21 +89,13 @@ const Dashboard = () => {
     void dispatch(getSentNotifications(params));
   }, [filters, pagination.size, pagination.page, sort]);
 
-  const filtersApplied: boolean = filterNotificationsTableRef.current.filtersApplied;
-
-  const EmptyStateProps = {
-    emptyMessage: filtersApplied ? undefined : emptyMessage,
-    emptyActionLabel: filtersApplied ? undefined : emptyActionLabel,
-    disableSentimentDissatisfied: !filtersApplied,
-    emptyActionCallback: filtersApplied ? handleCancelSearch : handleRouteApiKeys,
-    secondaryMessage: filtersApplied ? undefined : secondaryMessage,
-  };
-
   return (
     <Box p={3}>
-      <Typography variant="h4" mb={isMobile ? 3 : undefined}>Notifiche</Typography>
-      <Box display={isMobile ? "block" : 'flex'} justifyContent="space-between" alignItems="center">
-        <Typography variant="body1" sx={{marginBottom: isMobile ? 3 : undefined}}>
+      <Typography variant="h4" mb={isMobile ? 3 : undefined}>
+        Notifiche
+      </Typography>
+      <Box display={isMobile ? 'block' : 'flex'} justifyContent="space-between" alignItems="center">
+        <Typography variant="body1" sx={{ marginBottom: isMobile ? 3 : undefined }}>
           Qui trovi tutte le notifiche inviate dall&apos;ente. Puoi filtrarle per Codice Fiscale,
           Codice IUN, data di invio e stato.
         </Typography>
@@ -230,35 +107,29 @@ const Dashboard = () => {
           Invia una nuova notifica
         </Button>
       </Box>
-      <Fragment>
-        {notifications && (
-          <Fragment>
-            <FilterNotificationsTable ref={filterNotificationsTableRef} />
-            {notifications.length > 0 ? (
-              <>
-                <ItemsTable 
-                  columns={columns}
-                  sort={sort}
-                  rows={rows}
-                  onChangeSorting={handleChangeSorting}
-                />
-                <CustomPagination
-                  paginationData={{
-                    size: pagination.size,
-                    page: pagination.page,
-                    totalElements,
-                  }}
-                  onPageRequest={handleChangePage}
-                  pagesToShow={pagesToShow}
-                  sx={{ padding: '0 10px' }}
-                />
-              </>
-            ) : (
-              <EmptyState {...EmptyStateProps}/>
-            )}
-          </Fragment>
-        )}
-      </Fragment>
+      {isMobile ? (
+        <MobileNotifications />
+      ) : (
+        <DesktopNotifications
+          notifications={notifications}
+          onChangeSorting={handleChangeSorting}
+          onCancelSearch={handleCancelSearch}
+          onManualSend={handleRouteManualSend}
+          onApiKeys={handleRouteApiKeys}
+        />
+      )}
+      {notifications.length > 0 && (
+        <CustomPagination
+          paginationData={{
+            size: pagination.size,
+            page: pagination.page,
+            totalElements,
+          }}
+          onPageRequest={handleChangePage}
+          pagesToShow={pagesToShow}
+          sx={{ padding: '0 10px' }}
+        />
+      )}
     </Box>
   );
 };
