ba80a5dcab9aaa7efe2a83e990c000598551305b
Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>
2022-06-17 11:07:32 +0200

    wip: webpack config

--- packages/pn-landing-webapp/pages/_app.tsx
+++ packages/pn-landing-webapp/pages/_app.tsx
@@ -1,14 +1,16 @@
-import '../styles/globals.css';
+import React, { Suspense } from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from "@mui/material";
 import { theme } from "@pagopa/mui-italia";
 import LandingLayout from "../src/layout/LandingLayout";
 
+import '../styles/globals.css';
+
 function Main({ Component, pageProps }: AppProps) {
   // TODO: fix typescript error and remove ts-ignore
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   /* @ts-ignore */
-  return <ThemeProvider theme={theme}><LandingLayout><Component {...pageProps} /></LandingLayout></ThemeProvider>;
+  return <ThemeProvider theme={theme}><Suspense fallback={'loading...'}><LandingLayout><Component {...pageProps} /></LandingLayout></Suspense></ThemeProvider>;
 }
 
 export default Main;
