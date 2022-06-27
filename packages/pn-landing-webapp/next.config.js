5e4dd98575537a77ce355b3000fe8903fd53ffe4
CarlaDimatteo <carlotta.dimatteo@dgsspa.com>
2022-06-27 11:57:15 +0200

    chore: versioni di mui-italia

--- packages/pn-landing-webapp/next.config.js
+++ packages/pn-landing-webapp/next.config.js
@@ -1,12 +1,21 @@
 const withPlugins = require("next-compose-plugins");
 const withTM = require("next-transpile-modules")(["@pagopa-pn/pn-commons"]);
+const { withGlobalCss } = require('next-global-css')
 
-module.exports = withPlugins([withTM()], {
+const withConfig = withGlobalCss();
+const webpackNodeExternals = require('webpack-node-externals');
+
+module.exports = withPlugins([withTM(), withConfig()], {
   reactStrictMode: true,
   trailingSlash: true,
-  webpack: (config) => {
+  webpack: (config, options) => {
     // custom webpack config
+    if (options.isServer) {
+      config.externals = webpackNodeExternals({
+        // Uses list to add this modules for server bundle and process.
+        allowlist: [/@pagopa\/mui-italia/],
+      });
+    }
     return config;
   },
-  images: {},
 });
