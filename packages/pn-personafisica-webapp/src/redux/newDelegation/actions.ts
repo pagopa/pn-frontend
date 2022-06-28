f00b78baac2bfd3ebbeec59d5e0124958fce365d
Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>
2022-06-28 12:31:53 +0200

    fix: PN-1662 the end delegation date has the correct day, adds format date function

--- packages/pn-personafisica-webapp/src/redux/newDelegation/actions.ts
+++ packages/pn-personafisica-webapp/src/redux/newDelegation/actions.ts
@@ -1,5 +1,6 @@
 import { createAction, createAsyncThunk } from '@reduxjs/toolkit';
 
+import {formatToSlicedISOString} from "@pagopa-pn/pn-commons/src/services/date.service";
 import { DelegationsApi } from '../../api/delegations/Delegations.api';
 import { ExternalRegistriesAPI } from '../../api/external-registries/External-registries.api';
 import { CreateDelegationResponse, NewDelegationFormProps } from '../delegation/types';
@@ -16,7 +17,7 @@ export const createDelegation = createAsyncThunk<CreateDelegationResponse, NewDe
       },
       visibilityIds: data.selectTuttiEntiOrSelezionati === 'tuttiGliEnti' ? [] : [data.enteSelect],
       verificationCode: data.verificationCode,
-      dateto: data.expirationDate.toISOString().split('T')[0],
+        dateto: formatToSlicedISOString(data.expirationDate),
     };
     try {
       return await DelegationsApi.createDelegation(payload);
