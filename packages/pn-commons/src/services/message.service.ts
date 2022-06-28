cc0a28bc31329aa7451d6688f8883e14ac1aea21
Maurizio Flauti <maurizio.flauti@appsonrails.it>
2022-06-28 12:06:45 +0200

    refactor: add optional customMessage param on createAppError function to override the default error message based solely on status code refactor: add custom error message displayed when the user try to add a duplicated delegation

--- packages/pn-commons/src/services/message.service.ts
+++ packages/pn-commons/src/services/message.service.ts
@@ -1,12 +1,26 @@
-import { IAppMessage } from '../types/AppMessage';
 import _ from 'lodash';
+import { IAppMessage } from '../types/AppMessage';
+
+
+
+interface IAppErrorProps {
+  response: {
+    status: number;
+    customMessage?: {
+      title: string;
+      message: string;
+    };
+  };
+}
 
 /**
- * This method get an http error code and return a formatted IAppMessage message.
- * @param  {{response:{status:number;};}} error
+ * This method get an http error code and an optional custom message as params and return a formatted IAppMessage message.
+ * If customMessage is set than its title and message fields are used into the returned IAppMessage message otherwise a 
+ * default title and message body are set based on the message status
+ * @param  {IAppErrorProps} error
  * @returns IAppMessage
  */
-export const createAppError = (error: { response: { status: number; data: { title: string} } }): IAppMessage => {
+export const createAppError = (error: IAppErrorProps): IAppMessage => {
   const e: IAppMessage = {
     id: _.uniqueId(),
     title: '',
@@ -16,7 +30,10 @@ export const createAppError = (error: { response: { status: number; data: { titl
     toNotify: true,
   };
   e.status = error.response?.status;
-  if (error.response?.status === 404) {
+  if (error.response?.customMessage) {
+    e.title = error.response.customMessage.title;
+    e.message = error.response.customMessage.message;
+  } else if (error.response?.status === 404) {
     e.title = 'Risorsa non trovata';
     e.message = 'Si è verificato un errore. Si prega di riprovare più tardi';
   } else if (error.response?.status === 403) {
@@ -25,10 +42,7 @@ export const createAppError = (error: { response: { status: number; data: { titl
   } else if (error.response?.status === 401) {
     e.title = 'Utente non autorizzato';
     e.message = "L'utente corrente non ha le autorizzazioni";
-  } else if (error.response?.status === 400 && error.response?.data?.title === "Delega già presente") {
-    e.title = 'Delega già presente';
-    e.message = "La persona che hai indicato ha già una delega per questo ente.";
-  } else if (error.response?.status === 500 && error.response?.data?.title === "Errore generico") {
+  } else if (error.response?.status === 500) {
     e.title = 'Il servizio non è disponibile';
     e.message = "Per un problema temporaneo del servizio, la tua richiesta non è stata inviata. Riprova più tardi.";
   } else {
