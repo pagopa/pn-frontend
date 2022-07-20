e0edc5eab741ed793ade7821b2bce29fae5aa71a
Maurizio Flauti <maurizio.flauti@appsonrails.it>
2022-07-20 13:00:39 +0200

    wip: add content

--- packages/pn-landing-webapp/pages/cittadini.tsx
+++ packages/pn-landing-webapp/pages/cittadini.tsx
@@ -1,36 +1,42 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
 import {
-    Hero,
-    HorizontalNav,
-    Infoblock,
-    Showcase,
-    Walkthrough
+  HorizontalNav,
+  Infoblock,
+  Showcase,
+  Walkthrough,
+  Hero,
 } from "@pagopa/mui-italia";
-
 import {
-    getHeroData,
-    getHorizontalNavData,
-    getInfoblockData,
-    getShowcaseData,
-    getWalkthroughData,
-    UserType
+  getHeroData,
+  getHorizontalNavData,
+  getInfoblockData,
+  getShowcaseData,
+  getWalkthroughData,
+  UserType,
 } from "../api";
 
-const USER_TYPE = UserType.PA;
+const USER_TYPE = UserType.PF;
+
+// const FAVICON_PATH = `${process.env.NEXT_PUBLIC_ASSETS_URL}/favicon.svg`;
 
 const Cittadini: NextPage = () => (
-    <>
-        <Head>
-            <title>Piattaforma Notifiche</title>
-            <meta name="description" content="Landing Piattaforma notifiche" />
-            <link rel="icon" href="static/favicon.svg" />
-        </Head>
+  <>
+    <Head>
+      <title>Piattaforma Notifiche</title>
+      <meta name="description" content="Landing Piattaforma notifiche" />
+      <link rel="icon" href="static/favicon.svg" />
+    </Head>
 
-        <main>
-            Cittadini
-        </main>
-    </>
+    <main>
+      <Hero {...getHeroData(USER_TYPE)} />
+      <Infoblock {...getInfoblockData(USER_TYPE, "infoblock 1")}></Infoblock>
+      <Showcase {...getShowcaseData(USER_TYPE)} />
+      <Infoblock {...getInfoblockData(USER_TYPE, "infoblock 2")}></Infoblock>
+      <Infoblock {...getInfoblockData(USER_TYPE, "infoblock 3")}></Infoblock>
+      <Walkthrough {...getWalkthroughData(USER_TYPE)} />
+    </main>
+  </>
 );
 
 export default Cittadini;
