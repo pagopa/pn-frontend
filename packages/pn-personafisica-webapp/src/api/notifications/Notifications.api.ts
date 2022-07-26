745479f7e06c93c0b3c8929f9dace5b9dc16a438
Andrea Cimini <andrea.cimini@keypartner.com>
2022-07-26 15:35:07 +0200

    fixes after merge

--- packages/pn-personafisica-webapp/src/api/notifications/Notifications.api.ts
+++ packages/pn-personafisica-webapp/src/api/notifications/Notifications.api.ts
@@ -11,6 +11,7 @@ import { AxiosResponse } from 'axios';
 
 import { Delegator } from '../../redux/delegation/types';
 import { parseNotificationDetailForRecipient } from '../../utils/notification.utility';
+import { NotificationDetailForRecipient } from '../../types/NotificationDetail';
 import { apiClient } from '../axios';
 
 import {
