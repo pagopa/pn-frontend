52dcf1e5ede186e55e1b7db13d4d3a18ed71be27
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2022-08-03 10:17:41 +0200

    feat(PN-1369): removed some comments related to mixpanel init

--- packages/pn-personafisica-login/src/utils/mixpanel.ts
+++ packages/pn-personafisica-login/src/utils/mixpanel.ts
@@ -1,38 +1,37 @@
-import { init, track } from 'mixpanel-browser';
+import { init, Mixpanel, track } from 'mixpanel-browser';
 import { MIXPANEL_TOKEN } from './constants';
 import { events, TrackEventType } from './events';
 /**
  * Function that initialize Mixpanel (must be called once)
  */
 export const mixpanelInit = function (): void {
-
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        // eslint-disable-next-line no-console
-        console.log('Mixpanel events mock on console log.');
-    } else if (process.env.NODE_ENV === 'test') {
-        return;
-    } else {
-        init(MIXPANEL_TOKEN, {
-            api_host: 'https://api-eu.mixpanel.com',
-            persistence: 'localStorage',
-            // if this is true, Mixpanel will automatically determine
-            // City, Region and Country data using the IP address of
-            // the client
-            ip: false,
-            // names of properties/superproperties which should never
-            // be sent with track() calls
-            property_blacklist: ["$current_url", "$initial_referrer", "$referrer"],
-            debug: true,
-            // function called after mixpanel has finished loading
-            // loaded(mixpanel: Mixpanel) {
-            //     // this is useful to obtain a new distinct_id every session
-            //     // the distinct_id is the user identifier that mixpanel automatically assign
-            //     if (sessionStorage.getItem('') === null) {
-            //         mixpanel.reset();
-            //     }
-            // },
-        });
-    }
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-console
+    console.log('Mixpanel events mock on console log.');
+  } else if (process.env.NODE_ENV === 'test') {
+    return;
+  } else {
+    init(MIXPANEL_TOKEN, {
+      api_host: 'https://api-eu.mixpanel.com',
+      persistence: 'localStorage',
+      // if this is true, Mixpanel will automatically determine
+      // City, Region and Country data using the IP address of
+      // the client
+      ip: false,
+      // names of properties/superproperties which should never
+      // be sent with track() calls
+      property_blacklist: ['$current_url', '$initial_referrer', '$referrer'],
+      debug: true,
+      // function called after mixpanel has finished loading
+      loaded(mixpanel: Mixpanel) {
+        // this is useful to obtain a new distinct_id every session
+        // the distinct_id is the user identifier that mixpanel automatically assign
+        if (sessionStorage.getItem('') === null) {
+          mixpanel.reset();
+        }
+      },
+    });
+  }
 };
 
 /**
@@ -41,18 +40,18 @@ export const mixpanelInit = function (): void {
  * @param properties event data
  */
 function trackEvent(event_name: string, properties?: any): void {
-    if (process.env.NODE_ENV === 'test') {
-        return;
-    } else if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-        console.log(event_name, properties);
-    } else {
-        try {
-            track(event_name, { ...properties, ...{ environment: "DEV" } });
-        } catch (_) {
-            // eslint-disable-next-line no-console
-            console.log(event_name, properties);
-        }
+  if (process.env.NODE_ENV === 'test') {
+    return;
+  } else if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    console.log(event_name, properties);
+  } else {
+    try {
+      track(event_name, { ...properties, ...{ environment: 'DEV' } });
+    } catch (_) {
+      // eslint-disable-next-line no-console
+      console.log(event_name, properties);
     }
+  }
 }
 
 /**
@@ -61,9 +60,9 @@ function trackEvent(event_name: string, properties?: any): void {
  * @param attributes event attributes
  */
 export const trackEventByType = (trackEventType: TrackEventType, attributes?: object) => {
-    const eventParameters = attributes
-        ? { ...events[trackEventType], attributes: { ...attributes } }
-        : events[trackEventType];
+  const eventParameters = attributes
+    ? { ...events[trackEventType], attributes: { ...attributes } }
+    : events[trackEventType];
 
-    trackEvent(trackEventType, eventParameters);
+  trackEvent(trackEventType, eventParameters);
 };
--- packages/pn-personafisica-webapp/src/utils/mixpanel.ts
+++ packages/pn-personafisica-webapp/src/utils/mixpanel.ts
@@ -1,17 +1,18 @@
 import { AnyAction, Dispatch, Middleware, PayloadAction } from '@reduxjs/toolkit';
 import { init, track, Mixpanel } from 'mixpanel-browser';
+import { MIXPANEL_TOKEN } from './constants';
 import { events, TrackEventType } from './events';
 /**
  * Function that initialize Mixpanel (must be called once)
  */
 export const mixpanelInit = function (): void {
-  // if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  //   // eslint-disable-next-line no-console
-  //   console.log('Mixpanel events mock on console log.');
-  // } else if (process.env.NODE_ENV === 'test') {
-  //   return;
-  // } else {
-    init("aef0b0d89c3355510e9cdfdb18e7d0b5", {
+  if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    // eslint-disable-next-line no-console
+    console.log('Mixpanel events mock on console log.');
+  } else if (process.env.NODE_ENV === 'test') {
+    return;
+  } else {
+    init(MIXPANEL_TOKEN, {
       api_host: 'https://api-eu.mixpanel.com',
       persistence: 'localStorage',
       // if this is true, Mixpanel will automatically determine
@@ -23,15 +24,15 @@ export const mixpanelInit = function (): void {
       debug: true,
       property_blacklist: [],
       // function called after mixpanel has finished loading
-      // loaded(mixpanel: Mixpanel) {
+      loaded(mixpanel: Mixpanel) {
         // this is useful to obtain a new distinct_id every session
         // the distinct_id is the user identifier that mixpanel automatically assign
-        // if (sessionStorage.getItem('user') === null) {
-        //  mixpanel.reset();
-        // }
-      // },
+        if (sessionStorage.getItem('user') === null) {
+          mixpanel.reset();
+        }
+      },
     });
-  // }
+  }
 };
 
 /**
@@ -41,24 +42,28 @@ export const mixpanelInit = function (): void {
  */
 function trackEvent(event_name: string, properties?: any): void {
   try {
-    track(event_name, { ...properties, ...{ environment: "DEV" } });
+    track(event_name, { ...properties, ...{ environment: 'DEV' } });
   } catch (_) {
     // eslint-disable-next-line no-console
     console.log(event_name, properties);
   }
-  // if (process.env.NODE_ENV === 'test') {
-  //   return;
-  // } else if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development' || MIXPANEL_TOKEN === 'DUMMY') {
-  //   // eslint-disable-next-line no-console
-  //   console.log(event_name, properties);
-  // } else {
-  //   try {
-  //     track(event_name, { ...properties, ...{ environment: "DEV" } });
-  //   } catch (_) {
-  //     // eslint-disable-next-line no-console
-  //     console.log(event_name, properties);
-  //   }
-  // }
+  if (process.env.NODE_ENV === 'test') {
+    return;
+  } else if (
+    !process.env.NODE_ENV ||
+    process.env.NODE_ENV === 'development' ||
+    MIXPANEL_TOKEN === 'DUMMY'
+  ) {
+    // eslint-disable-next-line no-console
+    console.log(event_name, properties);
+  } else {
+    try {
+      track(event_name, properties);
+    } catch (_) {
+      // eslint-disable-next-line no-console
+      console.log(event_name, properties);
+    }
+  }
 }
 
 /**
@@ -66,22 +71,27 @@ function trackEvent(event_name: string, properties?: any): void {
  */
 export const trackingMiddleware: Middleware =
   // ({getState}: MiddlewareAPI<any>) =>
-  () =>
+
+    () =>
     (next: Dispatch<AnyAction>) =>
-      (action: PayloadAction<any, string>): any => {
-        if (action.type in events) {
-          const idx = Object.values(TrackEventType).indexOf(action.type as TrackEventType);
-          const eventKey = Object.keys(TrackEventType)[idx];
-          const attributes = events[action.type].getAttributes?.(action.payload);
+    (action: PayloadAction<any, string>): any => {
+      if (action.type in events) {
+        const idx = Object.values(TrackEventType).indexOf(action.type as TrackEventType);
+        const eventKey = Object.keys(TrackEventType)[idx];
+        const attributes = events[action.type].getAttributes?.(action.payload);
 
-          const eventParameters = attributes
-            ? { category: events[action.type].category, action: events[action.type].action, attributes }
-            : events[action.type];
-          trackEvent(eventKey, eventParameters);
-        }
+        const eventParameters = attributes
+          ? {
+              category: events[action.type].category,
+              action: events[action.type].action,
+              attributes,
+            }
+          : events[action.type];
+        trackEvent(eventKey, eventParameters);
+      }
 
-        return next(action);
-      };
+      return next(action);
+    };
 
 /**
  * Function to track events outside redux
