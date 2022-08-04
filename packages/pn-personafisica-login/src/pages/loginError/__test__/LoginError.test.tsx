53a90f9e434eb1279ae6fc830c2c6b94b0cd6420
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-08-04 16:19:43 +0200

    fix(PN-1989): transient error page avoided if user cancels the login or other user-related situations (#357)

--- packages/pn-personafisica-login/src/pages/loginError/__test__/LoginError.test.tsx
+++ packages/pn-personafisica-login/src/pages/loginError/__test__/LoginError.test.tsx
@@ -1,4 +1,4 @@
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, waitFor, screen, act } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import LoginError from '../LoginError';
@@ -7,12 +7,24 @@ import '../../../locales/i18n';
 
 const mockNavigateFn = jest.fn();
 
+let spidErrorCode: string;
+
+// simulate a particular SPID error code, which must be set to the spidErrorCode variable
+// cfr. SPID error codes in https://www.agid.gov.it//sites/default/files/repository_files/tabella-messaggi-spid-v1.4.pdf
+function mockCreateMockedSearchParams() {
+  const mockedSearchParams = new URLSearchParams();
+  mockedSearchParams.set("errorCode", spidErrorCode);
+  return mockedSearchParams;
+}
+
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useNavigate: () => mockNavigateFn,
+  useSearchParams: () => [mockCreateMockedSearchParams(), null],
 }));
 
-test('test login error', async () => {
+test('login technical error - show transient error screen before redirecting to login', async () => {
+  spidErrorCode = '2';           // system not available
   render(
     <BrowserRouter>
       <LoginError />
@@ -36,4 +48,18 @@ test('test login error', async () => {
       timeout: 3500,
     }
   );
+});
+
+test('user cancelled the login - immediate redirect to login page', async () => {
+  spidErrorCode = '25';           // user cancel by clicking "Annulla" button or link
+  await act(async () => {
+      render(
+        <BrowserRouter>
+          <LoginError />
+        </BrowserRouter>
+      );
+  });
+
+  expect(mockNavigateFn).toBeCalledTimes(1);
+  expect(mockNavigateFn).toBeCalledWith(ROUTE_LOGIN);
 });
