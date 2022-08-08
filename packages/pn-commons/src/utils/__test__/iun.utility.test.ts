0d679e5609ac84a4a811e0c9a9dad0dfdef8b56b
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2022-08-08 17:06:21 +0200

    chore: added more test cases

--- packages/pn-commons/src/utils/__test__/iun.utility.test.ts
+++ packages/pn-commons/src/utils/__test__/iun.utility.test.ts
@@ -8,9 +8,51 @@ describe('Iun utility', () => {
     expect(formattedIun).toBe('MRTW');
   });
 
-  it('Return iun with dash and uppercase letters', () => {
+  it('Return iun with dash and uppercase letters (input not uppercase)', () => {
     const unformattedIun = 'mrtwa';
     const formattedIun = formatIun(unformattedIun);
     expect(formattedIun).toBe('MRTW-A');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase with dash)', () => {
+    const unformattedIun = 'MRTW-A';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('MRTW-A');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase - letters and numbers)', () => {
+    const unformattedIun = 'DDDDDDDDDDDDDDD11111';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('DDDD-DDDD-DDDD-DDD111-1-1');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase - special chars)', () => {
+    const unformattedIun = 'DDDDDDDDDDDD$$$11111';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('DDDD-DDDD-DDDD-11111');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase - special chars at the end)', () => {
+    const unformattedIun = 'DDDDDDDDDDDDDDD11111££$%&$%';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('DDDD-DDDD-DDDD-DDD111-1-1');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase - only letters)', () => {
+    const unformattedIun = 'DDDDDDDDDDDDDDDDDDDD';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('DDDD-DDDD-DDDD-DDDDDD-D-D');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase - only letters and dash)', () => {
+    const unformattedIun = 'DDDD-DDDD-DDDD-DDDDDD-D-D';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('DDDD-DDDD-DDDD-DDDDDD-D-D');
+  });
+
+  it('Return iun with dash and uppercase letters (input uppercase - only letters and extra dash)', () => {
+    const unformattedIun = 'DDDD-DDDD-DDDD-DDDDDD----';
+    const formattedIun = formatIun(unformattedIun);
+    expect(formattedIun).toBe('DDDD-DDDD-DDDD-DDDDDD');
   });
 });
