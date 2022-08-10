0d579fe898c618404d385749d10021f94a669a47
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-08-10 13:14:58 +0200

    PN-2038 - aggiustamenti a tests di pf-webapp/App.tsx per evitare messaggi in console durante l'esecuzione

--- packages/pn-personafisica-webapp/src/__test__/App.test.tsx
+++ packages/pn-personafisica-webapp/src/__test__/App.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable functional/no-let */
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { Suspense } from 'react';
 import * as redux from 'react-redux';
 import App from '../App';
@@ -46,8 +46,6 @@ describe('App', () => {
   let axiosMock: MockAdapter;
 
   beforeEach(() => {
-    void i18n.init();
-
     axiosMock = new MockAdapter(axios);
     axiosMock.onAny().reply(200);
 
@@ -72,26 +70,42 @@ describe('App', () => {
     jest.restoreAllMocks();
   });
 
-  it('Renders Piattaforma notifiche', () => {
-    render(<Component />);
-    const loading = screen.getByText(/loading.../i);
-    expect(loading).toBeInTheDocument();
+  describe("tests che non analizzano dettagli", () => {
+    beforeEach(() => {
+      void i18n.init();
+    });
+
+    it('Renders Piattaforma notifiche', () => {
+      render(<Component />);
+      const loading = screen.getByText(/loading.../i);
+      expect(loading).toBeInTheDocument();
+    });
+  
+    it('Test if automatic accessibility tests passes', async () => {
+      const { container } = render(<Component />);
+      const result = await axe(container);
+      expect(result).toHaveNoViolations();
+    });
+    
   });
 
-  it('Dispatches proper actions when session token is not empty', async () => {
-    render(<Component />, initialState('mocked-session-token'));
+  describe("tests che analizzano dettagli di comportamento", () => {
+    beforeEach(() => {
+      void i18n.init({
+        react: { 
+          useSuspense: false
+        }
+      });
+    });
 
-    await waitFor(() => {
+    it('Dispatches proper actions when session token is not empty', async () => {
+      render(<App />, initialState('mocked-session-token'));
+
       expect(mockDispatchFn).toBeCalledTimes(3);
       expect(mockSidemenuInformationActionFn).toBeCalledTimes(1);
       expect(mockDomicileInfoActionFn).toBeCalledTimes(1);
       expect(mockToSApprovalActionFn).toBeCalledTimes(1);
     });
   });
-
-  it('Test if automatic accessibility tests passes', async () => {
-    const { container } = render(<Component />);
-    const result = await axe(container);
-    expect(result).toHaveNoViolations();
-  });
+  
 });
