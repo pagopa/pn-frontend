ee5f5a950b819c90e0cdb0dcb3541d623342380b
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-08-11 11:47:54 +0200

    chore(Pn-2038): axios mock in app test econnrefused (#369)
    * PN-2038 - aggiustamenti a tests di pf-webapp/App.tsx per evitare messaggi in console durante l'esecuzione
    
    * PN-2038 - added comments
    
    * chore: specified name for tests
    
    Co-authored-by: Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>

--- packages/pn-personafisica-webapp/src/__test__/App.test.tsx
+++ packages/pn-personafisica-webapp/src/__test__/App.test.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable functional/no-let */
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { Suspense } from 'react';
 import * as redux from 'react-redux';
 import { axe, render } from './test-utils';
@@ -9,8 +9,12 @@ import App from '../App';
 import i18n from '../i18n';
 import * as sidemenuActions from '../redux/sidemenu/actions';
 import * as authActions from '../redux/auth/actions';
-// import { apiClient } from '../api/axios';
 
+/**
+ * Componente che mette App all'interno di un Suspense, 
+ * necessario per il test che fa solo un render, 
+ * usato anche nel automatic accessibility test. 
+ */
 const Component = () => (
   <Suspense fallback="loading...">
     <App />
@@ -37,6 +41,21 @@ const initialState = (token: string) => ({
   },
 });
 
+
+/**
+ * Questo test suite si separa in due describe diversi, di tests che hanno una differenza
+ * nella inizializzazione di i18n.
+ * - per il test che analizza dettagli di comportamento serve settare react.useSuspense = false
+ *   per evitare messaggi di ECONNREFUSED, cfr. PN-2038.
+ *   Cfr. https://stackoverflow.com/questions/54432861/a-react-component-suspended-while-rendering-but-no-fallback-ui-was-specified .
+ * - per i altri test, serve non passare nessun parametro. Se si fa lo stesso setting che per il
+ *   caso precedente, appaiono messaggi "A future version of React will block javascript: URLs..."
+ *   e "An update to ForwardRef inside a test was not wrapped in act(...)."
+ * 
+ * Lascio la inizializzazione comune nel describe principale.
+ * ---------------------------------
+ * Carlos, 2022.08.10
+ */
 describe('App', () => {
   // let result: RenderResult | undefined;
   let mockDispatchFn: jest.Mock;
@@ -46,8 +65,6 @@ describe('App', () => {
   let axiosMock: MockAdapter;
 
   beforeEach(() => {
-    void i18n.init();
-
     axiosMock = new MockAdapter(axios);
     axiosMock.onAny().reply(200);
 
@@ -72,26 +89,50 @@ describe('App', () => {
     jest.restoreAllMocks();
   });
 
-  it('Renders Piattaforma notifiche', () => {
-    render(<Component />);
-    const loading = screen.getByText(/loading.../i);
-    expect(loading).toBeInTheDocument();
+
+  /**
+   * Tests che usano Component e inizializzazione "semplice" di i18n.
+   */
+  describe("tests che non analizzano dettagli (test solo di accessibilità e renderizzazione)", () => {
+    beforeEach(() => {
+      void i18n.init();
+    });
+
+    it('Renders Piattaforma notifiche', () => {
+      render(<Component />);
+      const loading = screen.getByText(/loading.../i);
+      expect(loading).toBeInTheDocument();
+    });
+  
+    it('Test if automatic accessibility tests passes', async () => {
+      const { container } = render(<Component />);
+      const result = await axe(container);
+      expect(result).toHaveNoViolations();
+    });
+    
   });
 
-  it('Dispatches proper actions when session token is not empty', async () => {
-    render(<Component />, initialState('mocked-session-token'));
 
-    await waitFor(() => {
+  /**
+   * Tests che usano App e inizializzazione di i18n che include react.useSuspense = false.
+   */
+   describe("tests che analizzano dettagli di comportamento (mock alle chiamate)", () => {
+    beforeEach(() => {
+      void i18n.init({
+        react: { 
+          useSuspense: false
+        }
+      });
+    });
+
+    it('Dispatches proper actions when session token is not empty', async () => {
+      render(<App />, initialState('mocked-session-token'));
+
       expect(mockDispatchFn).toBeCalledTimes(3);
       expect(mockSidemenuInformationActionFn).toBeCalledTimes(1);
       expect(mockDomicileInfoActionFn).toBeCalledTimes(1);
       expect(mockToSApprovalActionFn).toBeCalledTimes(1);
     });
   });
-
-  it('Test if automatic accessibility tests passes', async () => {
-    const { container } = render(<Component />);
-    const result = await axe(container);
-    expect(result).toHaveNoViolations();
-  });
+  
 });
