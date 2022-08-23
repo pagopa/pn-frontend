3377ce9fc75c7a52d5ec40e15890dc20a57e62ff
Andrea Cimini <andrea.cimini@keypartner.com>
2022-08-23 13:06:40 +0200

    code input refactoring

--- packages/pn-commons/src/components/CodeModal/CodeInput.tsx
+++ packages/pn-commons/src/components/CodeModal/CodeInput.tsx
@@ -1,14 +1,4 @@
-import {
-  memo,
-  useMemo,
-  useRef,
-  KeyboardEvent,
-  useEffect,
-  Fragment,
-  forwardRef,
-  useImperativeHandle,
-  useState,
-} from 'react';
+import { memo, useMemo, useRef, KeyboardEvent, Fragment, useState, ChangeEvent, useEffect } from 'react';
 import { TextField } from '@mui/material';
 
 type Props = {
@@ -25,129 +15,121 @@ type Props = {
  * @param hasError set if there is an error
  * @param onChange function to listen on inputs changes
  */
-const CodeInput = memo(
-  forwardRef(({ initialValues, isReadOnly, hasError, onChange }: Props, ref) => {
-    const [currentValues, setCurrentValues] = useState(initialValues);
-    const inputsRef = useRef(new Array(initialValues.length).fill(undefined));
+const CodeInput = ({ initialValues, isReadOnly, hasError, onChange }: Props) => {
+  const [currentValues, setCurrentValues] = useState(initialValues);
+  const inputsRef = useRef(new Array(initialValues.length).fill(undefined));
 
-    const inputStyle = useMemo(() => {
-      /* eslint-disable functional/no-let */
-      let color = isReadOnly ? 'primary.main' : '';
-      color = hasError ? 'error.main' : color;
-      /* eslint-enalbe functional/no-let */
-      return {
-        width: '33px',
-        height: '56px',
-        marginRight: '10px',
-        input: { color },
-      };
-    }, [hasError]);
-
-    const focusInput = (index: number) => {
-      if (index > initialValues.length - 1 || index < 0) {
-        return;
-      }
-      setTimeout(() => {
-        // focus next input
-        inputsRef.current[index].focus();
-        // set cursor position
-        if (inputsRef.current[index].setSelectionRange) {
-          inputsRef.current[index].setSelectionRange(
-            inputsRef.current[index].value,
-            inputsRef.current[index].value
-          );
-        } else if (inputsRef.current[index].createTextRange) {
-          const t = inputsRef.current[index].createTextRange();
-          t.collapse(true);
-          t.moveEnd('character', inputsRef.current[index].value);
-          t.moveStart('character', inputsRef.current[index].value);
-          t.select();
-        }
-      }, 25);
+  const inputStyle = useMemo(() => {
+    /* eslint-disable functional/no-let */
+    let color = isReadOnly ? 'primary.main' : '';
+    color = hasError ? 'error.main' : color;
+    /* eslint-enalbe functional/no-let */
+    return {
+      width: '33px',
+      height: '56px',
+      marginRight: '10px',
+      input: { color },
     };
+  }, [hasError]);
 
-    const keyDownHandler = (event: KeyboardEvent<HTMLDivElement>, index: number) => {
-      if (!isNaN(Number(event.key))) {
-        /* eslint-disable-next-line functional/immutable-data */
-        inputsRef.current[index].value = event.key;
-        changeHandler();
+  const focusInput = (index: number) => {
+    if (index > initialValues.length - 1 || index < 0) {
+      return;
+    }
+    setTimeout(() => {
+      // focus next input
+      inputsRef.current[index].focus();
+      // set cursor position
+      if (inputsRef.current[index].setSelectionRange) {
+        inputsRef.current[index].setSelectionRange(
+          inputsRef.current[index].value,
+          inputsRef.current[index].value
+        );
+      } else if (inputsRef.current[index].createTextRange) {
+        const t = inputsRef.current[index].createTextRange();
+        t.collapse(true);
+        t.moveEnd('character', inputsRef.current[index].value);
+        t.moveStart('character', inputsRef.current[index].value);
+        t.select();
       }
-      if (
-        !isNaN(Number(event.key)) ||
-        event.key === 'Enter' ||
-        (event.key === 'Tab' && !event.shiftKey)
-      ) {
-        // focus next element
-        focusInput(index + 1);
-        return;
-      } else if (
-        event.key === 'ArrowLeft' ||
-        event.key === 'ArrowRight' ||
-        event.key === 'Delete'
-      ) {
-        return;
-      } else if (event.key === 'Backspace' || (event.key === 'Tab' && event.shiftKey)) {
-        focusInput(index - 1);
-        return;
-      }
-      // prevent all values that aren't numbers
-      event.preventDefault();
-    };
+    }, 25);
+  };
 
-    const changeHandler = () => {
-      const inputsValues = inputsRef.current.map((inputElem) => inputElem.value);
-      setCurrentValues(() => inputsValues);
-      onChange(inputsValues);
-    };
+  const keyDownHandler = (event: KeyboardEvent<HTMLDivElement>, index: number) => {
+    const cursorPosition = (event.target as any).selectionStart;
+    if (!isNaN(Number(event.key)) && currentValues[index]) {
+      changeInputValue(event.key, index);
+    }
+    if (
+      !isNaN(Number(event.key)) ||
+      event.key === 'Enter' ||
+      (event.key === 'Tab' && !event.shiftKey) ||
+      (event.key === 'ArrowRight' && cursorPosition === 1)
+    ) {
+      // focus next element
+      focusInput(index + 1);
+      return;
+    } else if (
+      event.key === 'Backspace' ||
+      (event.key === 'Tab' && event.shiftKey) ||
+      (event.key === 'ArrowLeft' && cursorPosition === 0)
+    ) {
+      // focus previous element
+      focusInput(index - 1);
+      return;
+    } else if (event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'Delete') {
+      return;
+    }
+    // prevent all values that aren't numbers
+    event.preventDefault();
+  };
 
-    const initInputsValues = () => {
-      inputsRef.current.forEach((inputElem, index) => {
-        inputElem.value = initialValues[index];
-      });
-    };
+  const changeInputValue = (value: string, index: number) => {
+    setCurrentValues((previousValues) => {
+      const inputsValues = [...previousValues];
+      inputsValues[index] = value;
+      return inputsValues;
+    });
+  };
 
-    useEffect(() => {
-      // reset values
-      if (inputsRef.current.every((v) => v && !v.value)) {
-        initInputsValues();
-      }
-    }, [inputsRef.current.every((v) => v && !v.value)]);
+  const changeHandler = (event: ChangeEvent, index: number) => {
+    changeInputValue((event.target as any).value, index);
+  };
 
-    useImperativeHandle(ref, () => ({
-      inputsValues: inputsRef.current.map((inputElem) => inputElem?.value),
-    }));
+  useEffect(() => {
+    onChange(currentValues);
+  }, [currentValues]);
 
-    return (
-      <Fragment>
-        {initialValues.map((_value, index) => (
-          <TextField
-            autoComplete='off'
-            key={index}
-            id="outlined-basic"
-            variant="outlined"
-            placeholder="-"
-            sx={inputStyle}
-            inputProps={{
-              maxLength: 1,
-              sx: { padding: '16.5px 10px', textAlign: 'center' },
-              readOnly: isReadOnly,
-              onClick: (e) =>
-                e.currentTarget.setSelectionRange(
-                  e.currentTarget.value.length,
-                  e.currentTarget.value.length
-                ),
-            }}
-            onKeyDown={(event) => keyDownHandler(event, index)}
-            onChange={changeHandler}
-            value={currentValues[index]}
-            inputRef={(node) => (inputsRef.current[index] = node)}
-            color={hasError ? 'error' : 'primary'}
-            error={hasError}
-          />
-        ))}
-      </Fragment>
-    );
-  })
-);
+  return (
+    <Fragment>
+      {initialValues.map((_value, index) => (
+        <TextField
+          autoComplete="off"
+          key={index}
+          id="outlined-basic"
+          variant="outlined"
+          placeholder="-"
+          sx={inputStyle}
+          inputProps={{
+            maxLength: 1,
+            sx: { padding: '16.5px 10px', textAlign: 'center' },
+            readOnly: isReadOnly,
+            onClick: (e) =>
+              e.currentTarget.setSelectionRange(
+                e.currentTarget.value.length,
+                e.currentTarget.value.length
+              ),
+          }}
+          onKeyDown={(event) => keyDownHandler(event, index)}
+          onChange={(event) => changeHandler(event, index)}
+          value={currentValues[index]}
+          inputRef={(node) => (inputsRef.current[index] = node)}
+          color={hasError ? 'error' : 'primary'}
+          error={hasError}
+        />
+      ))}
+    </Fragment>
+  );
+};
 
-export default CodeInput;
+export default memo(CodeInput);
