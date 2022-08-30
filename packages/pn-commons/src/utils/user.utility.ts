a269a9166d8a3c977367d2e05288800f274733eb
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2022-08-30 11:09:50 +0200

    chore: small refactoring

--- packages/pn-commons/src/utils/user.utility.ts
+++ packages/pn-commons/src/utils/user.utility.ts
@@ -19,12 +19,15 @@ export const basicUserDataMatcherContents = {
  * Produces the initial user object to set into the Redux store of a PN webapp.
  * If there is a (serialized) valid user object in the session storage, then it returns the corresponding (JSON-deserialized) object.
  * Otherwise, it returns an object which represent "no user logged", and also cleans the session storage.
- * 
+ *
  * @param yupMatcher the matcher to validate the data in session storage.
  * @param noLoggedUserData the "no user logged" object to return if there is no, or invalid, data in the session storage.
  * @returns the suitable User object to set into the initial Redux store.
  */
-export function basicInitialUserData<T extends BasicUser>(yupMatcher: yup.ObjectSchema<any>, noLoggedUserData: T): T {
+export function basicInitialUserData<T extends BasicUser>(
+  yupMatcher: yup.ObjectSchema<any>,
+  noLoggedUserData: T
+): T {
   const rawDataFromStorage = sessionStorage.getItem('user');
   if (rawDataFromStorage) {
     /* eslint-disable functional/no-let */
@@ -45,25 +48,29 @@ export function basicInitialUserData<T extends BasicUser>(yupMatcher: yup.Object
 }
 
 export function adaptedTokenExchangeError(originalError: any, tokenName: string) {
-  return (originalError.response?.status === 403) ? {
-    ...originalError,
-    isUnauthorizedUser: true,
-    response: {
-      ...originalError.response, customMessage: {
-        title: "Non sei autorizzato ad accedere",
-        message: "Stai uscendo da Piattaforma Notifiche",
+  return [403, 401].includes(originalError.response?.status)
+    ? {
+        ...originalError,
+        isUnauthorizedUser: true,
+        response: {
+          ...originalError.response,
+          customMessage: {
+            title: 'Non sei autorizzato ad accedere',
+            message: 'Stai uscendo da Piattaforma Notifiche',
+          },
+        },
       }
-    }
-  } 
-  : originalError.response?.data?.error === "Token is not valid" ? {
-    ...originalError,
-    isUnauthorizedUser: true,
-    response: {
-      ...originalError.response, customMessage: {
-        title: `Non puoi accedere per un problema tecnico - ${tokenName} non valido`,
-        message: "Stai uscendo da Piattaforma Notifiche",
+    : originalError.response?.data?.error === 'Token is not valid'
+    ? {
+        ...originalError,
+        isUnauthorizedUser: true,
+        response: {
+          ...originalError.response,
+          customMessage: {
+            title: `Non puoi accedere per un problema tecnico - ${tokenName} non valido`,
+            message: 'Stai uscendo da Piattaforma Notifiche',
+          },
+        },
       }
-    }
-  }
-  : originalError;
+    : originalError;
 }
