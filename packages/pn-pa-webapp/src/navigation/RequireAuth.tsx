9e43e5eb70aa74e8bdbc6f2e6443a9a9a64be292
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2022-08-31 12:32:29 +0200

    fix: changed PartyRole to PnRole to manage access for PA users

--- packages/pn-pa-webapp/src/navigation/RequireAuth.tsx
+++ packages/pn-pa-webapp/src/navigation/RequireAuth.tsx
@@ -7,11 +7,11 @@ import { DISABLE_INACTIVITY_HANDLER } from '../utils/constants';
 import { logout } from '../redux/auth/actions';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { RootState } from '../redux/store';
-import { PartyRole } from '../models/user';
+import { PNRole } from '../models/user';
 import { goToSelfcareLogin } from './navigation.utility';
 
 interface Props {
-  roles: Array<PartyRole>;
+  roles: Array<PNRole>;
 }
 
 const inactivityTimer = 5 * 60 * 1000;
@@ -20,14 +20,14 @@ const inactivityTimer = 5 * 60 * 1000;
  * This component returns Outlet if user is logged in and has privileges to access some areas.
  * Then all private routes can be accessed
  * 
- * @param roles: List of PartyRole allowed to access some areas
+ * @param roles: List of PNRole allowed to access some areas
  */
 /* eslint-disable functional/immutable-data */
 const RequireAuth = ({ roles }: Props) => {
   const token = useAppSelector((state: RootState) => state.userState.user.sessionToken);
   const role = useAppSelector((state: RootState) => state.userState.user.organization?.roles[0]);
   const expDate = useAppSelector((state: RootState) => state.userState.user.desired_exp);
-  const userHasRequiredRole = role && roles.includes(role.partyRole);
+  const userHasRequiredRole = role && roles.includes(role.role);
   const [accessDenied, setAccessDenied] = useState(token === '' || !token || !userHasRequiredRole);
   const dispatch = useAppDispatch();
   const { t } = useTranslation(['common']);
