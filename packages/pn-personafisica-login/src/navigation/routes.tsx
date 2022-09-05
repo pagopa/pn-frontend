e86b45bfb818e3eac94b022481de894b3783afe5
CarlaDim <carlotta.dimatteo@pagopa.it>
2022-09-05 16:46:10 +0200

    feat(pn-1754): link qr email pec (#384)
    * feat: change login redirect in personafisica app implementing current url, add redirect with token and redirect url in login app
    
    * fix: align storage ops with the rest of the application, use location.replace in goToLogin function
    
    * fix(pn-1754): changed redirect route to success
    
    Co-authored-by: Lorenzo Cocciarelli <l.cocciarelli@appsonrails.it>

--- packages/pn-personafisica-login/src/navigation/routes.tsx
+++ packages/pn-personafisica-login/src/navigation/routes.tsx
@@ -1,11 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import {ROUTE_LOGIN, ROUTE_LOGIN_ERROR, ROUTE_LOGOUT, ROUTE_REDIRECT} from '../utils/constants';
+import {ROUTE_LOGIN, ROUTE_LOGIN_ERROR, ROUTE_LOGOUT, ROUTE_SUCCESS} from '../utils/constants';
 import { storageOnSuccessOps } from "../utils/storage";
 import Login from '../pages/login/Login';
 import Logout from '../pages/logout/Logout';
 import LoginError from '../pages/loginError/LoginError';
-import RedirectPage from "../pages/redirect/Redirect";
+import SuccessPage from "../pages/success/Success";
 
 /** login request operations */
 const onLoginRequest = () => {
@@ -29,7 +29,7 @@ function Router() {
       <Route path={ROUTE_LOGIN} element={onLoginRequest()} />
       <Route path={ROUTE_LOGIN_ERROR} element={<LoginError />} />
       <Route path={ROUTE_LOGOUT} element={<Logout />} />
-      <Route path={ROUTE_REDIRECT} element={<RedirectPage />} />
+      <Route path={ROUTE_SUCCESS} element={<SuccessPage />} />
       <Route path="*" element={<Navigate to={ROUTE_LOGIN} replace />} />
     </Routes>
   );
