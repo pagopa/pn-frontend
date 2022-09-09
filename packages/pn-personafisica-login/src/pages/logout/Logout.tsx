c810275712123ef7998d4c7b6c06f21e24321292
Lorenzo <l.cocciarelli@appsonrails.it>
2022-09-09 15:06:03 +0200

    feat: now the platform logout brings to logout page of login portal, login portal stores the origin depending from type of logout(user or sessioncheck)

--- packages/pn-personafisica-login/src/pages/logout/Logout.tsx
+++ packages/pn-personafisica-login/src/pages/logout/Logout.tsx
@@ -1,12 +1,18 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 
 import { ROUTE_LOGIN } from '../../utils/constants';
+import {storageOriginOps} from "../../utils/storage";
 
 const Logout = () => {
   const navigate = useNavigate();
+  const [params] = useSearchParams();
+
+
 
   useEffect(() => {
+    storageOriginOps.write(params.get('origin') ?? '');
+
     navigate(ROUTE_LOGIN);
   }, []);
 
