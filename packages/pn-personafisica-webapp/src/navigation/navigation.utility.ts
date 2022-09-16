00693688a945a91e109a8709b2b9c64a4f3ef333
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2022-09-16 15:12:19 +0200

    chore: removed some comments

--- packages/pn-personafisica-webapp/src/navigation/navigation.utility.ts
+++ packages/pn-personafisica-webapp/src/navigation/navigation.utility.ts
@@ -1,10 +1,9 @@
-// import { URL_FE_LOGIN } from '../utils/constants';
+import { URL_FE_LOGIN } from '../utils/constants';
 
 export function goToLoginPortal(origin: string) {
-  // const baseUrl = URL_FE_LOGIN ?? '';
+  const baseUrl = URL_FE_LOGIN ?? '';
   const encodedPathname = encodeURIComponent(origin);
-  console.log("origin",origin);
 
   /* eslint-disable functional/immutable-data */
-  window.location.replace('http://localhost:3000/' + 'logout' + `?origin=${encodedPathname}`);
+  window.location.replace(baseUrl + 'logout' + `?origin=${encodedPathname}`);
 }
