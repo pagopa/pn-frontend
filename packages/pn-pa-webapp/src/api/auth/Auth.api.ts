8f31d529f0a721a816ff251d23b2ffed525790ee
Lorenzo <l.cocciarelli@appsonrails.it>
2022-09-21 00:08:34 +0200

    fix: change tokenExchange from get to post

--- packages/pn-pa-webapp/src/api/auth/Auth.api.ts
+++ packages/pn-pa-webapp/src/api/auth/Auth.api.ts
@@ -5,7 +5,7 @@ import { AUTH_TOKEN_EXCHANGE } from "./auth.routes";
 export const AuthApi = {
     exchangeToken: (selfCareToken: string): Promise<User> => {
         const params = new URLSearchParams([['authorizationToken', selfCareToken]]);
-        return authClient.get<User>(AUTH_TOKEN_EXCHANGE(), { params })
+        return authClient.post<User>(AUTH_TOKEN_EXCHANGE(), params)
             .then((response) => response.data);
     }
 };
--- packages/pn-personafisica-webapp/src/api/auth/Auth.api.ts
+++ packages/pn-personafisica-webapp/src/api/auth/Auth.api.ts
@@ -5,7 +5,7 @@ import { AUTH_TOKEN_EXCHANGE } from "./auth.routes";
 export const AuthApi = {
     exchangeToken: (spidToken: string): Promise<User> => {
         const params = new URLSearchParams([['authorizationToken', spidToken]]);
-        return authClient.get<User>(AUTH_TOKEN_EXCHANGE(), { params })
+        return authClient.post<User>(AUTH_TOKEN_EXCHANGE(), params)
             .then((response) => response.data);
     }
 };
