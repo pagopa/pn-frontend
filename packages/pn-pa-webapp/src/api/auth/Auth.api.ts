df42958ee05a1ed7d4c6b54f74e7e45625e6977e
Lorenzo <l.cocciarelli@appsonrails.it>
2022-09-30 09:23:43 +0200

    fix: add userMap function in Auth api

--- packages/pn-pa-webapp/src/api/auth/Auth.api.ts
+++ packages/pn-pa-webapp/src/api/auth/Auth.api.ts
@@ -1,18 +1,36 @@
 import { User } from "../../redux/auth/types";
 import { authClient } from "../axios";
+import { PartyRole, PNRole } from "../../models/user";
 import { AUTH_TOKEN_EXCHANGE } from "./auth.routes";
+
+function userMap<T>(values: Partial<T>, emptyObject: T): T {
+  return Object.keys(emptyObject).reduce((acc: any, key: string) => {
+    // eslint-disable-next-line functional/immutable-data
+    acc[key] = values[key as keyof T];
+    return acc;
+  }, {}) as T;
+}
+
+const emptyUser = {
+  sessionToken: '',
+  name: '',
+  family_name:  '',
+  fiscal_number:  '',
+  email: '',
+  uid: '',
+  organization: {
+    id: '',
+    roles: [{
+      role: PNRole.OPERATOR,
+      partyRole: PartyRole.OPERATOR,
+    }],
+    fiscal_code: '',
+  },
+  desired_exp: 0
+};
 
 export const AuthApi = {
     exchangeToken: (selfCareToken: string): Promise<User> =>
       authClient.post<User>(AUTH_TOKEN_EXCHANGE(), {authorizationToken: selfCareToken})
-        .then((response) => ({
-            desired_exp: response.data.desired_exp,
-            email: response.data.email,
-            name: response.data.name,
-            family_name: response.data.family_name,
-            fiscal_number: response.data.fiscal_number,
-            organization: response.data.organization,
-            sessionToken: response.data.sessionToken,
-            uid: response.data.uid
-        }))
+        .then((response) => userMap(response.data, emptyUser))
 };
--- packages/pn-personafisica-webapp/src/api/auth/Auth.api.ts
+++ packages/pn-personafisica-webapp/src/api/auth/Auth.api.ts
@@ -2,23 +2,34 @@ import { User } from "../../redux/auth/types";
 import { authClient } from "../axios";
 import { AUTH_TOKEN_EXCHANGE } from "./auth.routes";
 
+function userMap<T>(values: Partial<T>, emptyObject: T): T {
+    return Object.keys(emptyObject)
+      .reduce((acc: any, key: string) => {
+          // eslint-disable-next-line functional/immutable-data
+          acc[key] = values[key as keyof T];
+          return acc;
+          }, {}) as T;
+}
+
+const emptyUser = {
+    sessionToken: '',
+    name: '',
+    family_name:  '',
+    fiscal_number:  '',
+    email: '',
+    uid: '',
+    mobile_phone: '',
+    from_aa: false,
+    aud: '',
+    level: '',
+    iat: 0,
+    exp: 0,
+    iss: '',
+    jti: '',
+};
+
 export const AuthApi = {
     exchangeToken: (spidToken: string): Promise<User> =>
       authClient.post<User>(AUTH_TOKEN_EXCHANGE(), {authorizationToken: spidToken})
-        .then((response) => ({
-            sessionToken: response.data.sessionToken,
-            email: response.data.email,
-            name: response.data.name,
-            family_name: response.data.family_name,
-            uid: response.data.uid,
-            fiscal_number: response.data.fiscal_number,
-            mobile_phone: response.data.mobile_phone,
-            from_aa: response.data.from_aa,
-            aud: response.data.aud,
-            level: response.data.level,
-            iat: response.data.iat,
-            exp: response.data.exp,
-            iss: response.data.iss,
-            jti: response.data.jti
-        }))
+        .then((response) => userMap(response.data, emptyUser))
 };
