f392bb737fb9db3053075b386617e8b86035f10c
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-10-18 21:52:08 +0200

    PN-2261 - tests di accesso con route /tos

--- packages/pn-pa-webapp/src/navigation/__test__/routes.test.tsx
+++ packages/pn-pa-webapp/src/navigation/__test__/routes.test.tsx
@@ -1,8 +1,11 @@
-import { act, screen } from '@testing-library/react';
+import { useEffect, useState } from 'react';
+import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { act, fireEvent, screen } from '@testing-library/react';
 import { apiOutcomeTestHelper } from '@pagopa-pn/pn-commons';
-import { render } from '../../__test__/test-utils';
+import { render, renderWithoutRouter } from '../../__test__/test-utils';
 import { AUTH_ACTIONS } from '../../redux/auth/actions';
 import Router from '../routes';
+import { PNRole } from '../../models/user';
 
 const mockSessionCheckFn = jest.fn(() => { });
 
@@ -19,10 +22,72 @@ jest.mock('@pagopa-pn/pn-commons', () => {
     ...original,
     useSessionCheck: () => mockSessionCheckFn,
     ApiErrorWrapper: original.simpleMockForApiErrorWrapper,
+    TermsOfServiceHandler: () => <div data-testid="mock-tos-handler">Terms of Service handler</div>,
   };
 });
 
+/* eslint-disable functional/no-let */
+let mockTosValue: boolean;
+
+jest.mock('../../api/consents/Consents.api', () => {
+  const original = jest.requireActual('../../api/consents/Consents.api');
+  return {
+    ...original,
+    ConsentsApi: {
+      getConsentByType: () => Promise.resolve({
+            recipientId: "mock-consent-id",
+            consentType: "TOS",
+            consentVersion: "V001",
+            accepted: mockTosValue,
+          })
+    },
+  };
+});
+
+jest.mock('../../pages/Dashboard.page', () => 
+  () => <div data-testid="mock-dashboard">Dashboard</div>
+);
+
+
+const FakeComponent = () => {
+  const [status, setStatus] = useState("Closed");
+
+  return <>
+    <div data-testid="statusDisplay">{status}</div>
+    <button data-testid="changeStatus" onClick={() => setStatus("Open")}>Change status</button>
+  </>;
+};
+
+const FakeRouter = () => {
+  // const navigate = useNavigate();
+  const location = useLocation();
+   
+  useEffect(() => {
+    console.log("in FakeRouter");
+    console.log({href: window.location.href, toString: window.location.toString(), host: window.location.host, pathname: window.location.pathname });
+    console.log(location);
+    // if (location.pathname === "/") {
+    //   navigate("/a");
+    // }
+  }, []);
+  
+  return <>
+    <div data-testid="title">The title</div>;
+    
+    <Routes>
+      <Route path="/a" element={<div data-testid="route-a">Route A</div>} />
+      <Route path="/b" element={<div data-testid="route-b">Route B</div>} />
+      <Route path="*" element={<div data-testid="route-not-found">Not found</div>} />
+    </Routes>
+  </>;
+}
+
 describe("router", () => {
+  beforeEach(() => {
+    // mockLocationPathname = "/";
+    mockTosValue = true;
+  });
+
   // expected behavior: it shows the ApiError component launched by OrganizationPartyGuard - see the code of ../routes.tsx
   it("error when retrieving Organization Party", async () => {
     const mockReduxState = {
@@ -32,5 +97,62 @@ describe("router", () => {
   
     await act(async () => void render(<Router />, { preloadedState: mockReduxState }));
     apiOutcomeTestHelper.expectApiErrorComponent(screen);
+  });
+
+  it.skip("interaction example", async () => {
+    await act(async () => void render(<FakeComponent />));
+    const statusDisplayComponent = screen.getByTestId("statusDisplay"); 
+    const statusChangeButton = screen.getByTestId("changeStatus");  
+    expect(statusDisplayComponent).toHaveTextContent("Closed");
+    fireEvent.click(statusChangeButton);
+    expect(statusDisplayComponent).toHaveTextContent("Open");
+  });
+
+  it.skip("router example - doesn't work this way", async () => {
+    // mockLocationPathname = "/a";
+    // window.location = {...window.location, pathname: "/a"};
+    // window.location.assign("http://localhost/a");
+    // console.log("in test");
+    console.log({href: window.location.href, toString: window.location.toString(), host: window.location.host, pathname: window.location.pathname });
+    await act(async () => void render(<FakeRouter />));
+    const routeAComponent = screen.queryByTestId("route-a"); 
+    const routeBComponent = screen.queryByTestId("route-b"); 
+    expect(routeAComponent).toBeInTheDocument();
+    expect(routeBComponent).not.toBeInTheDocument();
+  });
+
+  it.skip("router example - attempt using MemoryRouter", async () => {
+    await act(async () => void renderWithoutRouter(<MemoryRouter initialEntries={["/a"]}><FakeRouter /></MemoryRouter>));
+    const routeAComponent = screen.queryByTestId("route-a"); 
+    const routeBComponent = screen.queryByTestId("route-b"); 
+    expect(routeAComponent).toBeInTheDocument();
+    expect(routeBComponent).not.toBeInTheDocument();
+  });
+
+  // expected behavior: to be redirected automatically to /dashboard
+  it("access to /tos for a user which have accepted the ToS", async () => {
+    const mockReduxState = { userState: { user: { 
+      sessionToken: 'good-token', 
+      organization: { id: 'good-organization', roles: [{ role: PNRole.ADMIN }] } 
+    }}};
+    await act(async () => void renderWithoutRouter(
+      <MemoryRouter initialEntries={["/tos"]}><Router /></MemoryRouter>, { preloadedState: mockReduxState }
+    ));
+    expect(screen.queryByTestId("mock-tos-handler")).not.toBeInTheDocument();
+    expect(screen.queryByTestId("mock-dashboard")).toBeInTheDocument();
+  });
+
+  // expected behavior: to keep in /tos
+  it("access to /tos for a user which have not accepted the ToS", async () => {
+    mockTosValue = false;
+    const mockReduxState = { userState: { user: { 
+      sessionToken: 'good-token', 
+      organization: { id: 'good-organization', roles: [{ role: PNRole.ADMIN }] } 
+    }}};
+    await act(async () => void renderWithoutRouter(
+      <MemoryRouter initialEntries={["/tos"]}><Router /></MemoryRouter>, { preloadedState: mockReduxState }
+    ));
+    expect(screen.queryByTestId("mock-tos-handler")).toBeInTheDocument();
+    expect(screen.queryByTestId("mock-dashboard")).not.toBeInTheDocument();
   });
 });
