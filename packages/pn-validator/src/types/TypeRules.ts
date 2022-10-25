84ccfc4ba8211899fc24b4ecd7a08fc5fd319498
AndreaCimini90 <99173401+AndreaCimini90@users.noreply.github.com>
2022-10-25 13:52:24 +0200

    boolean and optional keys management (#443)
    * boolean and optional keys management
    * converted distributive conditional types into non distributive ones
    * removed useless code

--- packages/pn-validator/src/types/TypeRules.ts
+++ packages/pn-validator/src/types/TypeRules.ts
@@ -3,19 +3,41 @@ import { StringRuleValidator } from './../ruleValidators/StringRuleValidator';
 import { DateRuleValidator } from './../ruleValidators/DateRuleValidator';
 import { ObjectRuleValidator } from '../ruleValidators/ObjectRuleValidator';
 import { ArrayRuleValidator } from './../ruleValidators/ArrayRuleValidator';
+import { CommonRuleValidator } from '../ruleValidators/CommonRuleValidator';
 
-type MixedType = String | Number | Date;
+type MixedType = String | Number | Date | Boolean;
 
-export type TypeRules<TModel, TValue> = (TValue extends String
+type StringTypeRules<TModel, TValue> = [TValue] extends [String | undefined | null]
   ? StringRuleValidator<TModel, TValue>
-  : {}) &
-  (TValue extends Number ? NumberRuleValidator<TModel, TValue> : {}) &
-  (TValue extends Date ? DateRuleValidator<TModel, TValue> : {}) &
-  (TValue extends Array<infer _TEachValue> ? ArrayRuleValidator<TModel, TValue> : {}) &
-  (TValue extends object
-    ? TValue extends Array<infer _TEachValue>
-      ? {}
-      : TValue extends MixedType
-      ? {}
-      : ObjectRuleValidator<TModel, TValue>
-    : {});
+  : {};
+
+type NumberTypeRules<TModel, TValue> = [TValue] extends [Number | undefined | null]
+  ? NumberRuleValidator<TModel, TValue>
+  : {};
+
+type DateTypeRules<TModel, TValue> = [TValue] extends [Date | undefined | null]
+  ? DateRuleValidator<TModel, TValue>
+  : {};
+
+type BoolenaTypeRules<TModel, TValue> = [TValue] extends [Boolean | undefined | null]
+  ? CommonRuleValidator<TModel, TValue>
+  : {};
+
+type ArrayTypeRules<TModel, TValue> = [TValue] extends [Array<infer _TEachValue> | undefined | null]
+  ? ArrayRuleValidator<TModel, TValue>
+  : {};
+
+type ObjectTypeRules<TModel, TValue> = [TValue] extends [object | undefined | null]
+  ? TValue extends Array<infer _TEachValue>
+    ? {}
+    : TValue extends MixedType
+    ? {}
+    : ObjectRuleValidator<TModel, TValue>
+  : {};
+
+export type TypeRules<TModel, TValue> = StringTypeRules<TModel, TValue> &
+  NumberTypeRules<TModel, TValue> &
+  DateTypeRules<TModel, TValue> &
+  BoolenaTypeRules<TModel, TValue> &
+  ArrayTypeRules<TModel, TValue> &
+  ObjectTypeRules<TModel, TValue>;
