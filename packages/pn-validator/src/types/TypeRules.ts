8891d541aa03202379fa21715511cfa95c12e31e
Andrea Cimini <andrea.cimini@keypartner.com>
2022-10-25 10:05:50 +0200

    boolean and optional keys management

--- packages/pn-validator/src/types/TypeRules.ts
+++ packages/pn-validator/src/types/TypeRules.ts
@@ -1,19 +1,22 @@
+import { Validator } from '@pagopa-pn/pn-validator';
 import { NumberRuleValidator } from './../ruleValidators/NumberRuleValidator';
 import { StringRuleValidator } from './../ruleValidators/StringRuleValidator';
 import { DateRuleValidator } from './../ruleValidators/DateRuleValidator';
 import { ObjectRuleValidator } from '../ruleValidators/ObjectRuleValidator';
 import { ArrayRuleValidator } from './../ruleValidators/ArrayRuleValidator';
+import { CommonRuleValidator } from '../ruleValidators/CommonRuleValidator';
 
-type MixedType = String | Number | Date;
+type MixedType = String | Number | Date | Boolean | undefined | null;
 
-export type TypeRules<TModel, TValue> = (TValue extends String
+export type TypeRules<TModel, TValue> = (TValue extends String | undefined | null
   ? StringRuleValidator<TModel, TValue>
   : {}) &
-  (TValue extends Number ? NumberRuleValidator<TModel, TValue> : {}) &
-  (TValue extends Date ? DateRuleValidator<TModel, TValue> : {}) &
-  (TValue extends Array<infer _TEachValue> ? ArrayRuleValidator<TModel, TValue> : {}) &
-  (TValue extends object
-    ? TValue extends Array<infer _TEachValue>
+  (TValue extends Number | undefined | null ? NumberRuleValidator<TModel, TValue> : {}) &
+  (TValue extends Date | undefined | null ? DateRuleValidator<TModel, TValue> : {}) &
+  (TValue extends Boolean | undefined | null ? CommonRuleValidator<TModel, TValue> : {}) &
+  (TValue extends Array<infer _TEachValue> | undefined | null ? ArrayRuleValidator<TModel, TValue> : {}) &
+  (TValue extends object | undefined | null
+    ? TValue extends Array<infer _TEachValue> | undefined | null
       ? {}
       : TValue extends MixedType
       ? {}
