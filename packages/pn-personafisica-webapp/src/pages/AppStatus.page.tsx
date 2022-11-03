4b094aff17b20de14db01cc1f27d81f423394434
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-11-03 13:38:05 +0100

    PN-1953 - mobile version ready

--- packages/pn-personafisica-webapp/src/pages/AppStatus.page.tsx
+++ packages/pn-personafisica-webapp/src/pages/AppStatus.page.tsx
@@ -16,16 +16,19 @@ import { clearLegalFactDocumentData } from '../redux/appStatus/reducers';
 const StatusBar = ({ status }: { status: AppCurrentStatus }) => {
   const { t } = useTranslation(['appStatus']);
   const theme = useTheme();
+  const isMobile = useIsMobile();
+
   const mainColor = status.appIsFullyOperative ? theme.palette.success.main : theme.palette.error.main;
   const statusText = t(`appStatus.statusDescription.${status.appIsFullyOperative ? "ok" : "not-ok"}`);
   const IconComponent = status.appIsFullyOperative ? CheckCircleIcon : ErrorIcon;
+
   return (
-    <Stack component="div" direction="row" justifyContent='center' alignItems="center" sx={(theme) => ({
-      mt: '42px', py: '21px', px: '35px', width: '100%',
+    <Stack component="div" direction={isMobile ? "column" : "row"} justifyContent='center' alignItems="center" sx={(theme) => ({
+      mt: isMobile ? '23px' : '42px', py: '21px', px: '35px', width: '100%',
       backgroundColor: alpha(mainColor, theme.palette.action.hoverOpacity),
       borderColor: mainColor, borderWidth: '1px', borderStyle: 'solid', borderRadius: "10px",
     })}>
-      <IconComponent sx={{ width: "20px", mr: "20px", color: mainColor }} />
+      <IconComponent sx={{ width: "20px", mr: isMobile ? 0 : "20px", mb: isMobile ? "12px" : 0, color: mainColor }} />
       <Typography variant='body2'>
         {statusText}
       </Typography>
@@ -59,20 +62,23 @@ function booleanStringToBoolean(booleanString: string): boolean {
 }
 
 /* eslint-disable-next-line arrow-body-style */
-const DateAndTimeInTwoLines = ({ date }: { date: string }) => {
+const FormattedDateAndTime = ({ date, inTwoLines }: { date: string; inTwoLines?: boolean }) => {
   console.log('in DateAndTimeInTwoLines per questo date');
   console.log(date);
 
   return date ? 
-    <Stack direction="column">
-      <Typography variant="body2">{formatDate(date)},</Typography>
-      <Typography variant="body2">ore {formatTimeHHMM(date)}</Typography>
-    </Stack> 
+    (inTwoLines 
+      ? <Stack direction="column">
+          <Typography variant="body2">{formatDate(date)},</Typography>
+          <Typography variant="body2">ore {formatTimeHHMM(date)}</Typography>
+        </Stack> 
+      : <Typography variant="body2">{formatDate(date)}, ore {formatTimeHHMM(date)}</Typography>
+    )
   : <Typography variant="body2">-</Typography>;
 };
 
 
-const DesktopDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) => {
+const DesktopDowntimeLog = ({ downtimeLog }: { downtimeLog: DowntimeLogPage }) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation(['appStatus']);
   const theme = useTheme();
@@ -92,7 +98,7 @@ const DesktopDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) 
       width: '15%',
       sortable: false, 
       getCellLabel(value: string) {
-        return <DateAndTimeInTwoLines date={value} />;
+        return <FormattedDateAndTime date={value} inTwoLines />;
       },
     },
     {
@@ -101,7 +107,7 @@ const DesktopDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) 
       width: '15%',
       sortable: false, 
       getCellLabel(value: string) {
-        return <DateAndTimeInTwoLines date={value} />;
+        return <FormattedDateAndTime date={value} inTwoLines />;
       },
     },
     {
@@ -151,24 +157,29 @@ const DesktopDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) 
     },
   ];
 
-  const rows: Array<Item> | undefined = downtimeLog?.downtimes.map((n, i) => ({
+  const rows: Array<Item> | undefined = downtimeLog.downtimes.map((n, i) => ({
     ...n,
     fileAvailable: n.fileAvailable ? "true" : "false",
     id: n.startDate + i.toString(),
   }));
 
-  return (downtimeLog && rows && downtimeLog.downtimes.length > 0) 
-    ? <ItemsTable rows={rows} columns={columns} /> 
-    : <EmptyState disableSentimentDissatisfied enableSentimentSatisfied emptyMessage={t('downtimeList.emptyMessage')} />
-  ;
+  return <ItemsTable rows={rows} columns={columns} />;
 };
 
 
-const MobileDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) => {
+const MobileDowntimeLog = ({ downtimeLog }: { downtimeLog: DowntimeLogPage }) => {
   const dispatch = useAppDispatch();
   const { t } = useTranslation(['appStatus']);
   const theme = useTheme();
-  // const legalFactDocumentDetails = useAppSelector((state: RootState) => state.appStatus.legalFactDocumentData);
+  const legalFactDocumentDetails = useAppSelector((state: RootState) => state.appStatus.legalFactDocumentData);
+
+  /* eslint-disable-next-line sonarjs/no-identical-functions */
+  useEffect(() => {
+    if (legalFactDocumentDetails && legalFactDocumentDetails.url) {
+      downloadDocument(legalFactDocumentDetails.url);
+      dispatch(clearLegalFactDocumentData());
+    }
+  }, [legalFactDocumentDetails]);
 
   const cardHeader: [CardElement, CardElement | null] = [
     {
@@ -186,6 +197,30 @@ const MobileDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) =
   ];
 
   const cardBody: Array<CardElement> = [
+    {
+      id: 'startDate',
+      label: t('downtimeList.columnHeader.startDate'),
+      getLabel(value: string) {
+        return <FormattedDateAndTime date={value} />;
+      },
+    },
+    {
+      id: 'endDate',
+      label: t('downtimeList.columnHeader.endDate'),
+      getLabel(value: string) {
+        return <FormattedDateAndTime date={value} />;
+      },
+    },
+    {
+      id: 'functionality',
+      label: t('downtimeList.columnHeader.functionality'),
+      /* eslint-disable-next-line sonarjs/no-identical-functions */
+      getLabel(_: string, i: Item) {
+        return i.knownFunctionality 
+          ? t(`legends.knownFunctionality.${i.knownFunctionality}`)
+          : t('legends.unknownFunctionality', { functionality: i.rawFunctionality });
+      },
+    },
     {
       id: 'downtimeList.columnHeader.legalFactId',
       label: t('downtimeList.columnHeader.legalFactId'),
@@ -209,20 +244,18 @@ const MobileDowntimeLog = ({ downtimeLog }: { downtimeLog?: DowntimeLogPage }) =
   ];
 
   /* eslint-disable-next-line sonarjs/no-identical-functions */
-  const rows: Array<Item> | undefined = downtimeLog?.downtimes.map((n, i) => ({
+  const rows: Array<Item> | undefined = downtimeLog.downtimes.map((n, i) => ({
     ...n,
     fileAvailable: n.fileAvailable ? "true" : "false",
     id: n.startDate + i.toString(),
   }));
 
-  return rows 
-    ? <ItemsCard
-        cardHeader={cardHeader}
-        cardBody={cardBody}
-        cardData={rows}
-        headerGridProps={{ justifyContent: "center" }}
-      />
-    : null;
+  return <ItemsCard
+    cardHeader={cardHeader}
+    cardBody={cardBody}
+    cardData={rows}
+    headerGridProps={{ justifyContent: "center" }}
+  />;
 };
 
 
@@ -259,9 +292,16 @@ const AppStatus = () => {
 
       {currentStatus && <StatusBar status={currentStatus} />}
 
-      <Typography variant="h6" sx={{ mt: "36px" }}>{t('downtimeList.title')}</Typography>
+      <Typography variant="h6" sx={{ mt: "36px", mb: 2 }}>{t('downtimeList.title')}</Typography>
 
-      { downtimeLog && (isMobile ? <MobileDowntimeLog downtimeLog={downtimeLog}/> : <DesktopDowntimeLog downtimeLog={downtimeLog}/>) }
+      { downtimeLog && 
+        (downtimeLog.downtimes.length > 0) 
+          ? (isMobile 
+              ? <Box sx={{ mt: 2 }}><MobileDowntimeLog downtimeLog={downtimeLog} /></Box>
+              : <DesktopDowntimeLog downtimeLog={downtimeLog} />
+            )
+          : <EmptyState disableSentimentDissatisfied enableSentimentSatisfied emptyMessage={t('downtimeList.emptyMessage')} />      
+      }
     </Stack>
   </Box>;
 };
