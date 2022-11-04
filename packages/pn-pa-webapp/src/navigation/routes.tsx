fc77ea3eec5a3d6a575ec0ecb47b431240294745
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-11-04 14:05:59 +0100

    PN-2261 - react to comments of the second review - define OrganizationPartyGuard in a separate file

--- packages/pn-pa-webapp/src/navigation/routes.tsx
+++ packages/pn-pa-webapp/src/navigation/routes.tsx
@@ -1,47 +1,19 @@
-import { useCallback } from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { ApiErrorWrapper, NotFound } from '@pagopa-pn/pn-commons';
+import { NotFound } from '@pagopa-pn/pn-commons';
+import { Route, Routes } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard.page';
-import NotificationDetail from '../pages/NotificationDetail.page';
 import NewNotification from '../pages/NewNotification.page';
+import NotificationDetail from '../pages/NotificationDetail.page';
 // import ApiKeys from '../pages/ApiKeys.page';
 import PrivacyPolicyPage from "../pages/PrivacyPolicy.page";
 import TermsOfServicePage from "../pages/TermsOfService.page";
 
 import { PNRole } from '../models/user';
-import { RootState } from '../redux/store';
-import { useAppDispatch, useAppSelector } from '../redux/hooks';
-import { AUTH_ACTIONS, getOrganizationParty } from '../redux/auth/actions';
 import ToSAcceptance from '../pages/ToSAcceptance.page';
+import RouteGuard from './RouteGuard';
 import * as routes from './routes.const';
 import SessionGuard from './SessionGuard';
-import RouteGuard from './RouteGuard';
-
-function OrganizationPartyGuard() {
-  const loggedUser = useAppSelector((state: RootState) => state.userState.user);
-  const idOrganization = loggedUser.organization?.id;
-  const dispatch = useAppDispatch();
-  const { t } = useTranslation(['common']);
-
-  const fetchOrganizationParty = useCallback(() => {
-    if (idOrganization) {
-      void dispatch(getOrganizationParty(idOrganization));
-    }
-  }, [idOrganization]);
-
-  return (
-    <ApiErrorWrapper 
-      apiId={AUTH_ACTIONS.GET_ORGANIZATION_PARTY} 
-      reloadAction={() => fetchOrganizationParty()}
-      mainText={t('messages.error-in-initial-info-loading')}
-      mt={3}
-    >
-      <Outlet />
-    </ApiErrorWrapper>
-  );
-}
+import OrganizationPartyGuard from './OrganizationPartyGuard';
 
 function Router() {
   return (
