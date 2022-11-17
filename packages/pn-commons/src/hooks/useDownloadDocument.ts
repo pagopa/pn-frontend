70a1129508e6c29b69ac75b6f606777f8a3e274e
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2022-11-17 16:53:59 +0100

    PN-1953 - decided not to test useDownloadDocument

--- packages/pn-commons/src/hooks/useDownloadDocument.ts
+++ packages/pn-commons/src/hooks/useDownloadDocument.ts
@@ -14,6 +14,15 @@ type Props = {
   url?: string;
   clearDownloadAction?: () => any;
 }
+
+// There are no tests for this hook, since for such a test either the document
+// or the downloadDocument action.
+// For the former, jest offers no simple way to do it.
+// For the latter, we were adding a parameter just for testing.
+// Hence the decision of not producing tests of this hook
+// -------------------------------
+// Carlos Lombardi and Andrea Cimini, 2022.11.17
+// -------------------------------
 export function useDownloadDocument({ url, clearDownloadAction }: Props) {
   useEffect(() => {
     if (url) {
