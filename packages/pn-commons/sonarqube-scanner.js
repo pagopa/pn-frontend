370cdc3c2df64bb3c7babc3b77284945a2d7b6af
ZorbaDimatteo <carlotta.dimatteo@pagopa.it>
2022-11-18 18:23:45 +0100

    fix: fixed some param in sonar scanner script (pn-commons)

--- packages/pn-commons/sonarqube-scanner.js
+++ packages/pn-commons/sonarqube-scanner.js
@@ -1,11 +1,19 @@
 const scanner = require('sonarqube-scanner');
+
+const options = {
+  'sonar.organization': 'pagopa',
+  'sonar.projectKey': 'pagopa_pn-fe-commons',
+};
+
 scanner(
   {
     serverUrl: 'https://sonarcloud.io',
-    options: {
-      'sonar.organization': 'pagopa',
-      'sonar.projectKey': 'pagopa_pn-fe-commons',
-    },
+    options,
   },
-  () => process.exit()
+  () => {
+    console.log("Token " + process.env.SONAR_TOKEN);
+    console.log("options ", options);
+    console.log('Sonar has finished');
+    process.exit();
+  }
 );
