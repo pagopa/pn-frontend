9dca69505f18f9475dbbcb6faac7611d23d6bda6
Andrea Cimini <andrea.cimini@keypartner.com>
2022-11-18 16:39:16 +0100

    typed OneTrust variable and moved one trust cdn url to env variable

--- packages/pn-landing-webapp/pages/informativa-privacy.tsx
+++ packages/pn-landing-webapp/pages/informativa-privacy.tsx
@@ -2,19 +2,30 @@ import { useEffect } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 
-declare const OneTrust: any;
+import { ONE_TRUST_PORTAL_CDN } from "@utils/constants";
+
+declare const OneTrust: {
+  NoticeApi: {
+    Initialized: {
+      then: (cbk: () => void) => void
+    }
+    LoadNotices: (noticesUrls: Array<string>, flag: boolean) => void
+  }
+};
 
 const PrivacyPage: NextPage = () => {
 
   useEffect(() => {
-    OneTrust.NoticeApi.Initialized.then(function () {
-      OneTrust.NoticeApi.LoadNotices(
-        [
-          "https://privacyportalde-cdn.onetrust.com/77f17844-04c3-4969-a11d-462ee77acbe1/privacy-notices/draft/b5c8e1dc-89df-4ec2-a02d-1c0f55fac052.json",
-        ],
-        false
-      );
-    });
+    if (ONE_TRUST_PORTAL_CDN) {
+      OneTrust.NoticeApi.Initialized.then(() => {
+        OneTrust.NoticeApi.LoadNotices(
+          [
+            ONE_TRUST_PORTAL_CDN as string,
+          ],
+          false
+        );
+      });
+    }
   }, []);
 
   return (
