fbdb7086af879274eff9729c32bd22b16fb4d337
Andrea Cimini <andrea.cimini@keypartner.com>
2022-11-21 17:49:58 +0100

    restored pr opts

--- packages/pn-commons/sonarqube-scanner.js
+++ packages/pn-commons/sonarqube-scanner.js
@@ -2,16 +2,14 @@ const scanner = require('sonarqube-scanner');
 
 const options = {
   'sonar.organization': 'pagopa',
-  'sonar.projectKey': 'pagopa_pn-fe-commons'
+  'sonar.projectKey': 'pagopa_pn-fe-commons',
 };
 
-// if (typeof process.env.PR_NUM !== 'undefined' ) {
-//   options["sonar.pullrequest.base"] = process.env.BRANCH_TARGET;
-//   options["sonar.pullrequest.branch"] = process.env.BRANCH_NAME;
-//   options["sonar.pullrequest.key"] = process.env.PR_NUM;
-// }
-
-console.log(process.env.SONAR_TOKEN);
+if (typeof process.env.PR_NUM !== 'undefined') {
+  options['sonar.pullrequest.base'] = process.env.BRANCH_TARGET;
+  options['sonar.pullrequest.branch'] = process.env.BRANCH_NAME;
+  options['sonar.pullrequest.key'] = process.env.PR_NUM;
+}
 
 scanner(
   {
