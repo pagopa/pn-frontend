ca544e235e3a55786be64a53378382eeecf6d908
Andrea Cimini <andrea.cimini@keypartner.com>
2022-11-22 11:43:45 +0100

    sonar properties for source and test

--- packages/pn-commons/sonarqube-scanner.js
+++ packages/pn-commons/sonarqube-scanner.js
@@ -3,7 +3,6 @@ const scanner = require('sonarqube-scanner');
 const options = {
   'sonar.organization': 'pagopa',
   'sonar.projectKey': 'pagopa_pn-fe-commons',
-  'sonar.javascript.lcov.reportPaths': './coverage/lcov.info'
 };
 
 if (typeof process.env.PR_NUM !== 'undefined') {
