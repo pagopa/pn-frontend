e2a96a698a1fc8c99feff55a8bb35f0f1eacd21d
ZorbaDimatteo <carlotta.dimatteo@pagopa.it>
2022-11-22 12:31:33 +0100

    chore: moved privacy policy OT to informativa privacy file

--- packages/pn-landing-webapp/pages/informativa-privacy.tsx
+++ packages/pn-landing-webapp/pages/informativa-privacy.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-before-interactive-script-outside-document */
 import { useEffect } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Script from "next/script";
 
 import { ONE_TRUST_PORTAL_CDN } from "@utils/constants";
 
@@ -32,7 +34,13 @@ const PrivacyPage: NextPage = () => {
         />
         <link rel="icon" href="/static/favicon.svg" />
       </Head>
-
+      <Script
+        src="/onetrust/privacy-notice-scripts/otnotice-1.0.min.js"
+        type="text/javascript"
+        charSet="UTF-8"
+        id="otprivacy-notice-script"
+        strategy="beforeInteractive"
+      />
       <div
         id="otnotice-b5c8e1dc-89df-4ec2-a02d-1c0f55fac052"
         className={`otnotice`}
