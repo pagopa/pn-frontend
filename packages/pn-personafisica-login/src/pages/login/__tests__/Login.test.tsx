1be262a843f4d77c48ced153a37324b4a537bd3d
Lorenzo <l.cocciarelli@appsonrails.it>
2022-11-23 10:39:17 +0100

    fix: re-enable CIE button

--- packages/pn-personafisica-login/src/pages/login/__tests__/Login.test.tsx
+++ packages/pn-personafisica-login/src/pages/login/__tests__/Login.test.tsx
@@ -32,14 +32,13 @@ describe('test login page', () => {
     expect(screen.getAllByRole('img')[0]).toHaveAttribute('src', 'spid_big.svg');
   });
 
-  test.skip('renders button Entra con CIE', () => {
+  test('renders button Entra con CIE', () => {
     render(<BrowserRouter><Login /></BrowserRouter>);
+    const ButtonCIE = document.getElementById('cieButton');
+    if (ButtonCIE) {
+      fireEvent.click(ButtonCIE);
+    }
 
-    const ButtonCIE = screen.getByRole(/Button/i, {
-      name: 'Entra con CIE',
-    });
-
-    fireEvent.click(ButtonCIE);
     expect(global.window.location.assign).toBeCalledWith(
       `${ENV.URL_API.LOGIN}/login?entityID=xx_servizicie_test&authLevel=SpidL2`
     );
--- packages/pn-personafisica-login/src/utils/storage.ts
+++ packages/pn-personafisica-login/src/utils/storage.ts
@@ -3,3 +3,5 @@ import { storageOpsBuilder } from '@pagopa-pn/pn-commons';
 export const storageOnSuccessOps = storageOpsBuilder<string>('LOGIN:onSuccess', 'string', false);
 /** An object containing a complete set of operation on the storage regarding the origin where the login portal should redirect the user * */
 export const storageOriginOps = storageOpsBuilder<string>('origin', 'string', false);
+
+export const storageSpidSelectedOps = storageOpsBuilder<string>('SPID_SELECTED', 'object', false);
