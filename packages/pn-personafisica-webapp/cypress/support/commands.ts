72bb2ac267ab756eb4987b624df9f3e9caa7e739
Maurizio Flauti <100599605+maurizioflauti@users.noreply.github.com>
2022-11-28 12:44:30 +0100

    Test(pn-2628): refers subtasks PN-2632, PN-2633, PN-2634, PN-2636, PN-2646, PN-2647, PN-2648, PN-2649, PN-2650(#485)
    * wip: implement integration tests in cypress for NewNotification
    
    * test: add cypress tests for new notification, filter notifications and legal facts download (pa-webapp)
    
    refers to JIRA PN-2632, PN-2633, PN-2634, PN-2636,
    PN-2646, PN-2647, PN-2648, PN-2649, PN-2650
    
    * wip: add cypress tests for recipient
    
    * wip: add cypress tests and fixtures for pf
    
    * test: add cy tests
    refactor: change cypress fixtures
    
    * fix: fix cy test not working after merging with develop
    
    * chore: remove commented code

--- packages/pn-personafisica-webapp/cypress/support/commands.ts
+++ packages/pn-personafisica-webapp/cypress/support/commands.ts
@@ -8,13 +8,17 @@ Cypress.Commands.add('login', () => {
   /**
    * TODO: implement login through api
    */
-   cy.window().then(win => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+
+  cy.window().then((win) => {
     const user = win.sessionStorage.getItem('user');
 
-    if(!user) {
-      win.sessionStorage.setItem("user", JSON.stringify(Cypress.env("user")));
+    if (!user) {
+      win.sessionStorage.setItem('user', JSON.stringify(Cypress.env('user')));
     }
-   });
+  });
 });
 
 /**
@@ -24,26 +28,34 @@ Cypress.Commands.add('logout', () => {
   /**
    * TODO: implement logout through api
    */
-   cy.window().then(win => {
+  cy.window().then((win) => {
     const user = win.sessionStorage.getItem('user');
 
-    if(user) {
-      win.sessionStorage.removeItem("user");
+    if (user) {
+      win.sessionStorage.removeItem('user');
     }
-   });
+  });
 });
 
 Cypress.Commands.add('loginWithUI', () => {
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    return false;
+  });
+
+  cy.viewport(1920, 1080);
   cy.visit('');
 
-  cy.window().then(win => {
+  cy.window().then((win) => {
     const user = win.sessionStorage.getItem('user');
-    const username = Cypress.env("username") as string;
+    const username = Cypress.env('username') as string;
     // const password = Cypress.env("password", {log: false}) as string;
-    const password = Cypress.env("password") as string;
+    const password = Cypress.env('password') as string;
 
-    if(!user) { // no session... login needed
+    if (!user) {
+      // no session... login needed
+      cy.get('.css-zitybv > .MuiButton-root').click();
       cy.get('#spidButton').click();
+      cy.get('#onetrust-accept-btn-handler').click();
 
       cy.get('[alt="test"]').click();
       cy.get('input#username').type(username);
@@ -51,9 +63,8 @@ Cypress.Commands.add('loginWithUI', () => {
       cy.get('input#password').type(password);
       cy.get('button[type="submit"]').first().click();
       cy.get('input[value="Conferma"]').click();
-      cy.get('#onetrust-accept-btn-handler').click();
     }
-  }); 
+  });
 
   cy.contains('Notifiche');
 });
