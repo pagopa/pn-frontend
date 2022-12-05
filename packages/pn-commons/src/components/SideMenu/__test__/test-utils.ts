74af7138b3002e686e6f15e1d17c2c826eb9a06f
Andrea Cimini <andrea.cimini@keypartner.com>
2022-12-05 12:44:24 +0100

    fixed tests

--- packages/pn-commons/src/components/SideMenu/__test__/test-utils.ts
+++ packages/pn-commons/src/components/SideMenu/__test__/test-utils.ts
@@ -11,25 +11,25 @@ export const sideMenuItems: Array<SideMenuItem> = [
   { 
     label: 'Item 2',
     icon: QuestionMarkIcon,
-    route: '',
+    route: 'mocked-route-2',
     children: [{
       label: 'Item 2-1',
       icon: QuestionMarkIcon,
-      route: ''
+      route: 'mocked-route-2/mocked-route-2-1'
     }]
   },
   { 
     label: 'Item 3',
     icon: QuestionMarkIcon,
-    route: '',
+    route: 'mocked-route-3',
     children: [{
       label: 'Item 3-1',
       icon: QuestionMarkIcon,
-      route: ''
+      route: 'mocked-route-3/mocked-route-3-1'
     }, {
       label: 'Item 3-2',
       icon: QuestionMarkIcon,
-      route: ''
+      route: 'mocked-route-3/mocked-route-3-2'
     }]
   }
 ];
