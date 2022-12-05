224ec6c1aabe120e13657da6a0589cd713756e46
Giornetta Nicola <nicola.giornetta@dgsspa.com>
2022-12-05 10:34:18 +0100

    Removed 'only' method from test

--- packages/pn-pa-webapp/cypress/e2e/NewNotificationOperatorWithGroups.cy.ts
+++ packages/pn-pa-webapp/cypress/e2e/NewNotificationOperatorWithGroups.cy.ts
@@ -28,7 +28,7 @@ describe("Notifications New Notification", () => {
     cy.visit('/dashboard/nuova-notifica');
   });
 
-  it.only('Create new notification with groups and operator role', () => {
+  it('Create new notification with groups and operator role', () => {
 
     cy.intercept(/groups/, { fixture: 'groups/groups' });
     
