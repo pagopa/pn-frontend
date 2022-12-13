8caa794639bdcb556158c9d6fafb6f7bc35c2c90
Andrea Cimini <andrea.cimini@keypartner.com>
2022-12-13 12:21:02 +0100

    first setup

---
+++ packages/pn-personagiuridica-webapp/sonarqube-scanner.js
@@ -0,0 +1,23 @@
+const scanner = require('sonarqube-scanner');
+
+const options = {
+  'sonar.organization': 'pagopa',
+  'sonar.projectKey': 'pagopa_pn-personagiuridica-webapp',
+};
+
+if (typeof process.env.PR_NUM !== 'undefined') {
+  options['sonar.pullrequest.target'] = process.env.BRANCH_TARGET;
+  options['sonar.pullrequest.branch'] = process.env.BRANCH_NAME;
+  options['sonar.pullrequest.key'] = process.env.PR_NUM;
+} else {
+  options['sonar.branch.name'] = process.env.BRANCH_NAME;
+}
+
+scanner(
+  {
+    serverUrl: 'https://sonarcloud.io',
+    token: process.env.SONAR_TOKEN,
+    options,
+  },
+  () => process.exit()
+);
