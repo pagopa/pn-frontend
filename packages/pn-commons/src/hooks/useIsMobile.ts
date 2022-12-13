f5aea7b546b1dece523b01a34cb3f272d0756a0d
leleOFA <emanuele.didonato@pagopa.it>
2022-12-13 12:55:50 +0100

    refactor: edit useIsMobile hook to use useMediaQuery from mui chore: install css-mediaquery dev dependency with @types for testing purpose

--- packages/pn-commons/src/hooks/useIsMobile.ts
+++ packages/pn-commons/src/hooks/useIsMobile.ts
@@ -1,21 +1,7 @@
-import { useEffect, useState } from 'react';
+import { Theme } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 /**
- * Checks if we are on a mobile device 
+ * Checks if we are on a mobile device
  */
-export function useIsMobile() {
-  const [width, setWidth] = useState<number>(window.innerWidth);
-
-  const handleWindowSizeChange = () => {
-    setWidth(window.innerWidth);
-  };
-
-  useEffect(() => {
-    window.addEventListener('resize', handleWindowSizeChange);
-    return () => {
-      window.removeEventListener('resize', handleWindowSizeChange);
-    };
-  }, []);
-
-  return width <= 1200;
-}
+export const useIsMobile = () => useMediaQuery((theme: Theme) => theme.breakpoints.down('lg'));
