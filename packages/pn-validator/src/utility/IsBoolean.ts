bd59efdb59324f0a4effc7aef30cf7e9208bce84
Andrea Cimini <andrea.cimini@keypartner.com>
2022-12-22 09:26:20 +0100

    check on Boolean instance in isBoolean utility function

--- packages/pn-validator/src/utility/IsBoolean.ts
+++ packages/pn-validator/src/utility/IsBoolean.ts
@@ -2,7 +2,7 @@ export const isBoolean = (value: unknown): value is Boolean => {
     if (value === null || value === undefined) {
       return false;
     }
-    if (typeof value === 'boolean') {
+    if (typeof value === 'boolean' || value instanceof Boolean) {
       return true;
     }
     return false;
