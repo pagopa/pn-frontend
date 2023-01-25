6f6f95db91d327ea6879d52371b97942d9758569
Lorenzo <l.cocciarelli@appsonrails.it>
2023-01-25 10:41:09 +0100

    fix: restore layout spacing in Recipient physical address

--- packages/pn-pa-webapp/src/pages/components/NewNotification/Recipient.tsx
+++ packages/pn-pa-webapp/src/pages/components/NewNotification/Recipient.tsx
@@ -590,6 +590,8 @@ const Recipient = ({
                           <Typography>{t('add-physical-domicile')}*</Typography>
                         </Stack>
                       </Grid>
+                    </Grid>
+                    <Grid container spacing={2} mt={1}>
                       {values.recipients[index].showPhysicalAddress && (
                         <PhysicalAddress
                           values={values}
