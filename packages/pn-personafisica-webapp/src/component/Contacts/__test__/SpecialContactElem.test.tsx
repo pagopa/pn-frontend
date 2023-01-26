3cf09bd7547447e7174ca8ea034d34ff9ea3fa26
leleOFA <119851973+leleOFA@users.noreply.github.com>
2023-01-26 13:11:22 +0100

    feat(pn-3675): cancel button to roll back (not-confirmed) updates in contact page (#567)

--- packages/pn-personafisica-webapp/src/component/Contacts/__test__/SpecialContactElem.test.tsx
+++ packages/pn-personafisica-webapp/src/component/Contacts/__test__/SpecialContactElem.test.tsx
@@ -43,10 +43,10 @@ describe('SpecialContactElem Component', () => {
     const firstFormButtons = specialContactForms![0].querySelectorAll('button');
     expect(firstFormButtons).toHaveLength(2);
     expect(firstFormButtons[0]).toHaveTextContent('button.modifica');
-    expect(firstFormButtons[1]).toHaveTextContent('button.rimuovi');
+    expect(firstFormButtons[1]).toHaveTextContent('button.elimina');
     const secondFormButtons = specialContactForms![1].querySelectorAll('button');
     expect(secondFormButtons).toHaveLength(2);
     expect(secondFormButtons[0]).toHaveTextContent('button.modifica');
-    expect(secondFormButtons[1]).toHaveTextContent('button.rimuovi');
+    expect(secondFormButtons[1]).toHaveTextContent('button.elimina');
   });
 });
