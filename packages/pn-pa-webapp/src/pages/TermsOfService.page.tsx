923eb902db8366f230a7eaa745ff9eeaac7ee04d
leleOFA <119851973+leleOFA@users.noreply.github.com>
2023-02-03 11:37:11 +0100

    fix(pn-3802): OneTrust links navigation (#576)

--- packages/pn-pa-webapp/src/pages/TermsOfService.page.tsx
+++ packages/pn-pa-webapp/src/pages/TermsOfService.page.tsx
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
+import { useRewriteLinks } from '@pagopa-pn/pn-commons';
 
-import { ONE_TRUST_PORTAL_CDN_TOS } from "../utils/constants";
+import { ONE_TRUST_PORTAL_CDN_TOS } from '../utils/constants';
+import * as routes from '../navigation/routes.const';
 
 declare const OneTrust: {
   NoticeApi: {
@@ -12,17 +14,25 @@ declare const OneTrust: {
 };
 
 const TermsOfServicePage = () => {
+  const [contentLoaded, setContentLoaded] = useState(false);
+
   useEffect(() => {
     if (ONE_TRUST_PORTAL_CDN_TOS) {
       OneTrust.NoticeApi.Initialized.then(function () {
         OneTrust.NoticeApi.LoadNotices([ONE_TRUST_PORTAL_CDN_TOS], false);
+        setContentLoaded(true);
       });
     }
   }, []);
+  useRewriteLinks(contentLoaded, routes.TERMS_OF_SERVICE, '.otnotice-content a');
 
   return (
     <>
-      <div role="article" id="otnotice-083fb982-149c-4241-be09-12ae67d88b66" className="otnotice"></div>
+      <div
+        role="article"
+        id="otnotice-083fb982-149c-4241-be09-12ae67d88b66"
+        className="otnotice"
+      ></div>
     </>
   );
 };
