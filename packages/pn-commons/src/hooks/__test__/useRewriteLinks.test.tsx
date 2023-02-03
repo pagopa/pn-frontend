410fd16e60b181d9a59b34ef1fb1fa2791f3d1e1
leleOFA <emanuele.didonato@pagopa.it>
2023-02-03 10:51:42 +0100

    feat: change logic test hook useRewriteLinks

--- packages/pn-commons/src/hooks/__test__/useRewriteLinks.test.tsx
+++ packages/pn-commons/src/hooks/__test__/useRewriteLinks.test.tsx
@@ -1,7 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import React from 'react';
+import { useRewriteLinks } from '../useRewriteLinks';
+
+const ROUTE = '/informativa-privacy';
 
 const Component = () => {
+  useRewriteLinks(true, ROUTE, '.otnotice-content');
   return (
     <div>
       <a className=".otnotice-content" href="#abc-123"></a>
@@ -9,21 +13,12 @@ const Component = () => {
   );
 };
 
-const ROUTE = '/informativa-privacy';
-
-const editHref = (link: HTMLElement, route: String) => {
-  const href = link.getAttribute('href');
-  if (href?.startsWith('#')) {
-    const newHref = `${route}${href}`;
-    link.setAttribute('href', newHref);
-  }
-};
-
 describe('test useRewriteLinks hook', () => {
   test('hook set new href', async () => {
     const result = render(<Component></Component>);
     const link = await result.findByRole('link');
-    editHref(link, ROUTE);
-    expect(link).toHaveAttribute('href', `${ROUTE}#abc-123`);
+    waitFor(() => {
+      expect(link).toHaveAttribute('href', `${ROUTE}#abc-123`);
+    });
   });
 });
