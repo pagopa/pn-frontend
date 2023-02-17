db6452f9d25ed0037eeeea23e417e946adc6bb00
Maurizio Flauti <maurizio.flauti@appsonrails.it>
2023-02-17 11:50:03 +0100

    test: fix cypress tests failing on cookies non-acceptance

--- packages/pn-personafisica-webapp/cypress/e2e/Auth.cy.ts
+++ packages/pn-personafisica-webapp/cypress/e2e/Auth.cy.ts
@@ -11,9 +11,9 @@ describe('Auth', () => {
 
   it('Should logout properly using APIs', () => {
     cy.logout();
-    cy.window().then(win => {
+    cy.window().then((win) => {
       const user = win.sessionStorage.getItem('user');
       expect(user).to.be.null;
     });
   });
-})
+});
