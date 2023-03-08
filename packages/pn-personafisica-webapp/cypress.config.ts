e22ebba4d4598045c9cae7ac501759028771823b
Andrea Cimini <andrea.cimini@keypartner.com>
2023-03-08 13:43:31 +0100

    increased timeouts

--- packages/pn-personafisica-webapp/cypress.config.ts
+++ packages/pn-personafisica-webapp/cypress.config.ts
@@ -7,8 +7,7 @@ const webpackOptions = require('./cypress/webpack.config');
 export default defineConfig({
   e2e: {
     baseUrl: 'https://portale.dev.pn.pagopa.it',
-    // defaultCommandTimeout: 30000,
-    // requestTimeout: 30000,
+    defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {
       // setting up excludeSpecPattern
       let excludeSpecPattern = [];
