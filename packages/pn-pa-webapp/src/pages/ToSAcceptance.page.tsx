7955ae575df370e86026839ba4bbdb016538f990
LorenzoCocciarelli <l.cocciarelli@appsonrails.it>
2023-03-21 14:57:05 +0100

    fix(pn-4691): align tos acceptance page for pa with figma design

--- packages/pn-pa-webapp/src/pages/ToSAcceptance.page.tsx
+++ packages/pn-pa-webapp/src/pages/ToSAcceptance.page.tsx
@@ -76,10 +76,7 @@ const TermsOfService = ({ tosConsent, privacyConsent }: TermsOfServiceProps) => 
       <Grid item xs={10} sm={8} md={6}>
         <TOSAgreement
           productName={t('tos.title', 'Piattaforma Notifiche')}
-          description={t(
-            tosConsent.isFirstAccept && privacyConsent.isFirstAccept ? 'tos.body' : 'tos.redo-body',
-            'Prima di accedere, accetta i Termini e condizioni d’uso del servizio e leggi l’Informativa Privacy.'
-          )}
+          description={<></>}
           onConfirm={handleAccept}
           confirmBtnLabel={t('tos.button', 'Accedi')}
         >
