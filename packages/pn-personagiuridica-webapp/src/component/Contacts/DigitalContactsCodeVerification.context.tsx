3110b18ab815b036dfa477fa997301cd0553a221
Giornetta Nicola <nicola.giornetta@dgsspa.com>
2023-03-29 15:24:21 +0200

    Fix a11y on generate new OTP link in contacts

--- packages/pn-personagiuridica-webapp/src/component/Contacts/DigitalContactsCodeVerification.context.tsx
+++ packages/pn-personagiuridica-webapp/src/component/Contacts/DigitalContactsCodeVerification.context.tsx
@@ -20,8 +20,9 @@ import {
   AppResponse,
   ErrorMessage,
   DisclaimerModal,
+  buttonNakedInheritStyle,
 } from '@pagopa-pn/pn-commons';
-
+import { ButtonNaked } from '@pagopa/mui-italia';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { CourtesyChannelType, LegalChannelType } from '../../models/contacts';
 import { RootState } from '../../redux/store';
@@ -285,15 +286,14 @@ const DigitalContactsCodeVerificationProvider: FC<ReactNode> = ({ children }) =>
                 {t(`${modalProps.labelRoot}.${modalProps.labelType}-new-code`, { ns: 'recapiti' })}
                 &nbsp;
               </Typography>
-              <Typography
-                variant="body2"
-                display="inline"
-                color="primary"
+              <ButtonNaked
                 onClick={() => handleCodeVerification(undefined, true)}
-                sx={{ cursor: 'pointer' }}
+                sx={buttonNakedInheritStyle}
               >
-                {t(`${modalProps.labelRoot}.new-code-link`, { ns: 'recapiti' })}.
-              </Typography>
+                <Typography color="primary">
+                  {t(`${modalProps.labelRoot}.new-code-link`, { ns: 'recapiti' })}.
+                </Typography>
+              </ButtonNaked>
             </Box>
           }
           cancelLabel={t('button.annulla')}
