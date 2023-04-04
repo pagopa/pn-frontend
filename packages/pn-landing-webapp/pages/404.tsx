9138a7ee934c417aa5ab423479e26e45b92c8185
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2023-04-04 11:29:23 +0200

    added message in notfound aopge

--- packages/pn-landing-webapp/pages/404.tsx
+++ packages/pn-landing-webapp/pages/404.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
 
-const NotFound = () => <Typography variant="h4">Error 404</Typography>;
+const NotFound = () => <Typography variant="h4">Error 404 - Not Found</Typography>;
 
 export default NotFound;
