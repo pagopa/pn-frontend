03d8da7ecfac0ce9fcf2f71fa67986199c84dc79
NicolaGiornetta <105795630+NicolaGiornetta@users.noreply.github.com>
2023-04-14 14:12:02 +0200

    fix(pn-5085): Added new error handler for already existing parameters (#688)

--- packages/pn-pa-webapp/src/utils/AppError/PAAppErrorFactory.ts
+++ packages/pn-pa-webapp/src/utils/AppError/PAAppErrorFactory.ts
@@ -7,6 +7,7 @@ import {
 
 import { GenericInvalidParameterAppError } from './GenericInvalidParameterAppError';
 import { ServerResponseErrorCode } from './types';
+import { GenericInvalidParameterDuplicateAppError } from './GenericInvalidParameterDuplicateAppError';
 
 export class PAAppErrorFactory extends AppErrorFactory {
   private translateFunction: (path: string, ns: string) => string;
@@ -22,6 +23,8 @@ export class PAAppErrorFactory extends AppErrorFactory {
     switch (error.code) {
       case ServerResponseErrorCode.PN_GENERIC_INVALIDPARAMETER:
         return new GenericInvalidParameterAppError(error, this.translateFunction);
+      case ServerResponseErrorCode.PN_GENERIC_INVALIDPARAMETER_DUPLICATED:
+        return new GenericInvalidParameterDuplicateAppError(error, this.translateFunction);
       default:
         return new UnknownAppError(error);
     }
