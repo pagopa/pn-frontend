fd985d6822a4f831d627a7fda26d66f2bc57ae8f
leleOFA <emanuele.didonato@pagopa.it>
2023-04-19 17:33:25 +0200

    fix: change tab panel to react.fc

--- packages/pn-commons/src/components/TabPanel.tsx
+++ packages/pn-commons/src/components/TabPanel.tsx
@@ -3,27 +3,22 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 interface TabPanelProps {
-  children?: React.ReactNode;
   index: number;
   value: number;
 }
+const TabPanel: React.FC<TabPanelProps> = ({ index, value, children }) => (
+  <div
+    role="tabpanel"
+    hidden={value !== index}
+    id={`simple-tabpanel-${index}`}
+    aria-labelledby={`simple-tab-${index}`}
+  >
+    {value === index && (
+      <Box sx={{ p: 3 }}>
+        <Typography>{children}</Typography>
+      </Box>
+    )}
+  </div>
+);
 
-export default function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+export default TabPanel;
