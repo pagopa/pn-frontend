eafa13e47f9b555bfa173e180bca0467e451a627
leleOFA <emanuele.didonato@pagopa.it>
2023-04-26 13:00:30 +0200

    feature: create test to visualize and control verification code

--- packages/pn-personagiuridica-webapp/src/component/Deleghe/__test__/DelegatesByCompany.test.tsx
+++ packages/pn-personagiuridica-webapp/src/component/Deleghe/__test__/DelegatesByCompany.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { fireEvent, render } from '../../../__test__/test-utils';
+import { fireEvent, render, screen, waitFor } from '../../../__test__/test-utils';
 import { arrayOfDelegates } from '../../../redux/delegation/__test__/test.utils';
 import * as routes from '../../../navigation/routes.const';
 import DelegatesByCompany from '../DelegatesByCompany';
+import { forEach } from 'lodash';
 
 const mockNavigateFn = jest.fn();
 
@@ -83,5 +84,44 @@ describe('Delegates Component - assuming delegates API works properly', () => {
     fireEvent.click(addButton);
     expect(mockNavigateFn).toBeCalledTimes(1);
     expect(mockNavigateFn).toBeCalledWith(routes.NUOVA_DELEGA);
+  });
+
+  it('visualize modal code and check code', async () => {
+    const result = render(<DelegatesByCompany />, {
+      preloadedState: {
+        delegationsState: {
+          delegations: {
+            delegates: arrayOfDelegates,
+          },
+          sortDelegates: {
+            orderBy: '',
+            order: 'desc',
+          },
+        },
+      },
+    });
+    const menu = result.getAllByTestId('delegationMenuIcon');
+    fireEvent.click(menu[0]);
+    await waitFor(async () => {
+      const menuOpen = result.getAllByTestId('delegationMenu');
+      expect(menuOpen[0]).toHaveTextContent(/deleghe.show/i);
+    });
+    const menuOpen = result.getAllByTestId('delegationMenu');
+    fireEvent.click(
+      menuOpen[0].getElementsByClassName(
+        'MuiButtonBase-root MuiMenuItem-root MuiMenuItem-gutters Mui-focusVisible MuiMenuItem-root MuiMenuItem-gutters css-kk1bwy-MuiButtonBase-root-MuiMenuItem-root'
+      )[0]
+    );
+    await waitFor(() => {
+      const dialog = result.getAllByTestId('codeDialog');
+      const arrayOfVerificationCode = arrayOfDelegates[0].verificationCode.split('');
+      const inputsValue = dialog[0].getElementsByTagName('input');
+      expect(inputsValue[0].value).toBe(arrayOfVerificationCode[0]);
+      expect(inputsValue[1].value).toBe(arrayOfVerificationCode[1]);
+      expect(inputsValue[2].value).toBe(arrayOfVerificationCode[2]);
+      expect(inputsValue[3].value).toBe(arrayOfVerificationCode[3]);
+      expect(inputsValue[4].value).toBe(arrayOfVerificationCode[4]);
+      expect(inputsValue[5].value).toBe(arrayOfVerificationCode[5]);
+    });
   });
 });
