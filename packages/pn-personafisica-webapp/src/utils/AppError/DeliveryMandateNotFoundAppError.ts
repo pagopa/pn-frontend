409dc1055d8c915bb13ed31f5ba483d8b4d340fd
Alessandro Gelmi <alessandro.gelmi@cinetix-team.it>
2023-06-09 16:13:24 +0200

    fix(PN-6118): PF - specific message for PN_DELIVERY_MANDATENOTFOUND error (#807)
    * fix(PN-6118): handled PN_DELIVERY_MANDATENOTFOUND error code
    
    * fix: removed error handling from PG
    
    ---------
    
    Co-authored-by: Carlos Lombardi <ca.lombardi@mediaticaspa.it>

---
+++ packages/pn-personafisica-webapp/src/utils/AppError/DeliveryMandateNotFoundAppError.ts
@@ -0,0 +1,17 @@
+import { AppError, ServerResponseError } from '@pagopa-pn/pn-commons';
+
+export class DeliveryMandateNotFoundAppError extends AppError {
+  private translateFunction: (path: string, ns: string) => string = (path: string) => path;
+
+  constructor(error: ServerResponseError, translateFunction: (path: string, ns: string) => string) {
+    super(error);
+    this.translateFunction = translateFunction;
+  }
+
+  getMessage() {
+    return {
+      title: this.translateFunction('errors.delivery_mandate_not_found.title', 'deleghe'),
+      content: this.translateFunction('errors.delivery_mandate_not_found.message', 'deleghe'),
+    };
+  }
+}
