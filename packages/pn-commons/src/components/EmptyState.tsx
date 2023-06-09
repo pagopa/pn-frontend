0ddbd82097b308e59427bbf4db432520b7d80cda
leleOFA <emanuele.didonato@pagopa.it>
2023-06-09 16:13:58 +0200

    fix: insert dot

--- packages/pn-commons/src/components/EmptyState.tsx
+++ packages/pn-commons/src/components/EmptyState.tsx
@@ -26,7 +26,7 @@ interface Message {
 
 function EmptyState({
   emptyActionCallback,
-  emptyMessage = 'Non abbiamo trovato risultati: prova con dei filtri diversi',
+  emptyMessage = 'Non abbiamo trovato risultati: prova con dei filtri diversi.',
   emptyActionLabel = 'Rimuovi filtri',
   sentimentIcon = KnownSentiment.DISSATISFIED,
   secondaryMessage = {
