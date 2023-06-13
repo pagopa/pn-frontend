85a441619264642ee92c5f7a79b5c6c4a9353fd0
Andrea Cimini <andrea.cimini@pagopa.it>
2023-06-13 16:04:13 +0200

    work in progress flag fix

--- packages/pn-pa-webapp/src/navigation/SessionGuard.tsx
+++ packages/pn-pa-webapp/src/navigation/SessionGuard.tsx
@@ -149,7 +149,7 @@ const SessionGuard = () => {
    */
   useEffect(() => {
     const doInitalPageDetermination = () => {
-      if (isForbiddenUser || WORK_IN_PROGRESS) {
+      if (isForbiddenUser || (!sessionToken && WORK_IN_PROGRESS)) {
         // ----------------------
         // I'm not sure about this management of the redirects
         // Momentarily I have added the isForbiddenUser variable that is true if login returns 451 error code
