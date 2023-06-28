ee50e39783213270d30fe6e4fa4de0e1a73f4ce3
alessandrogelmi <alessandro.gelmi@cinetix-team.it>
2023-06-28 14:28:55 +0200

    fix: a11y of landing page

--- packages/pn-landing-webapp/api/data/fr/PF.tsx
+++ packages/pn-landing-webapp/api/data/fr/PF.tsx
@@ -29,11 +29,24 @@ const heroCta = !!PN_PF_URL
   : undefined;
 
 /** Hero mocked data */
+const heroSubtitle = `Avec SEND, tu peux recevoir instantanément les communications à valeur juridique d’un organisme : 
+tu peux visualiser, gérer et payer directement en ligne ou dans l’application les courriers recommandés qui te sont généralement envoyés sur papier.`;
+
 export const pfHero: HeroProps = {
   type: "image",
   title: "Les notifications ? Elles sont à portée de main.",
-  subtitle: `Avec SEND, tu peux recevoir instantanément les communications à valeur juridique d’un organisme : 
-      tu peux visualiser, gérer et payer directement en ligne ou dans l’application les courriers recommandés qui te sont généralement envoyés sur papier.`,
+  subtitle: (
+    <Typography
+      component="p"
+      tabIndex={0}
+      aria-label={heroSubtitle}
+      sx={{
+        color: "primary.contrastText",
+      }}
+    >
+      {heroSubtitle}
+    </Typography>
+  ),
   ctaPrimary: heroCta,
   inverse: false,
   image: `${IMAGES_PATH}/pf-hero-foreground.png`,
@@ -43,6 +56,30 @@ export const pfHero: HeroProps = {
 /* ************************************** */
 
 /** Infoblocks mocked data */
+const infoblock1_1 = `Les notifications sont des communications à valeur juridique émises
+officiellement par une administration, telles que des amendes, des
+avis d’imposition, des résultats de procédures administratives
+engagées avec les Administrations publiques ou des remboursements,
+que tu as jusqu’à présent toujours reçus par courrier recommandé. À
+partir d’aujourd’hui, tu peux les recevoir et les consulter en
+numérique, en accédant à SEND via SPID ou CIE ou directement sur
+l’application IO.`;
+const infoblock1_2 = `Tu peux également payer les éventuels coûts grâce à l’intégration
+avec pagoPA, consulter l’historique des notifications reçues et les
+gérer directement en ligne. De plus, il te suffit d’une délégation
+pour gérer les courriers recommandés de tes proches.`;
+const infoblock2 = `Pour envoyer les communications à valeur juridique, SEND donne
+toujours la priorité aux coordonnées numériques du destinataire. Tu
+peux à tout moment te connecter à la plateforme avec SPID et CIE
+pour indiquer ou mettre à jour tes préférences entre l’adresse PEC,
+l’application IO, l’e-mail ou le SMS. Si tu n’indiques aucune
+coordonnée ou si tu n’as pas accès à la plateforme, tu continueras à
+recevoir les notifications par courrier recommandé papier.`;
+const infoblock3_1 =
+  "Actuellement, SEND est testée avec un nombre limité d’administrations.";
+const infoblock3_2 =
+  "Progressivement, la plateforme sera adoptée par les Administrations publiques et utilisée pour envoyer des notifications à tous les citoyens.";
+
 export const pfInfoBlocks: Array<IInfoblockData> = [
   {
     name: "infoblock 1",
@@ -50,21 +87,11 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
       title: "Ne rate plus aucune notification",
       content: (
         <>
-          <Typography variant="body2">
-            Les notifications sont des communications à valeur juridique émises
-            officiellement par une administration, telles que des amendes, des
-            avis d’imposition, des résultats de procédures administratives
-            engagées avec les Administrations publiques ou des remboursements,
-            que tu as jusqu’à présent toujours reçus par courrier recommandé. À
-            partir d’aujourd’hui, tu peux les recevoir et les consulter en
-            numérique, en accédant à SEND via SPID ou CIE ou directement sur
-            l’application IO.
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock1_1}>
+            {infoblock1_1}
           </Typography>
-          <Typography variant="body2">
-            Tu peux également payer les éventuels coûts grâce à l’intégration
-            avec pagoPA, consulter l’historique des notifications reçues et les
-            gérer directement en ligne. De plus, il te suffit d’une délégation
-            pour gérer les courriers recommandés de tes proches.
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock1_2}>
+            {infoblock1_2}
           </Typography>
         </>
       ),
@@ -79,14 +106,8 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
       title: "Choisis comment recevoir les notifications",
       content: (
         <>
-          <Typography variant="body2">
-            Pour envoyer les communications à valeur juridique, SEND donne
-            toujours la priorité aux coordonnées numériques du destinataire. Tu
-            peux à tout moment te connecter à la plateforme avec SPID et CIE
-            pour indiquer ou mettre à jour tes préférences entre l’adresse PEC,
-            l’application IO, l’e-mail ou le SMS. Si tu n’indiques aucune
-            coordonnée ou si tu n’as pas accès à la plateforme, tu continueras à
-            recevoir les notifications par courrier recommandé papier.
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock2}>
+            {infoblock2}
           </Typography>
         </>
       ),
@@ -101,14 +122,11 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
       title: "L’avenir des communications à valeur juridique",
       content: (
         <>
-          <Typography variant="body2">
-            Actuellement, SEND est testée avec un nombre limité
-            d’administrations.
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock3_1}>
+            {infoblock3_1}
           </Typography>
-          <Typography variant="body2">
-            Progressivement, la plateforme sera adoptée par les Administrations
-            publiques et utilisée pour envoyer des notifications à tous les
-            citoyens.
+          <Typography variant="body2" tabIndex={0} aria-label={infoblock3_2}>
+            {infoblock3_2}
           </Typography>
         </>
       ),
@@ -122,6 +140,29 @@ export const pfInfoBlocks: Array<IInfoblockData> = [
 /* ************************************** */
 
 /** Showcase mocked data */
+const showcase1_1 =
+  "L’envoi de notifications numériques entraîne une réduction des coûts de notification et d’expédition";
+const showcase1_2 =
+  "Plus d’attente ou de files d’attente pour le retrait des communications papier";
+const showcase1_3 =
+  "Contribue à réduire la consommation de papier et les émissions liées au transport";
+const showcase1_4 =
+  "Tu n’as plus besoin de conserver les documents imprimés, grâce à la possibilité de télécharger et d’archiver les documents numériques";
+const showcase2_1 = `Si tu as une adresse PEC, les notifications te seront légalement
+remises, sans plus de courriers recommandés papier. L’avis de
+réception qui te sera envoyé contient le lien pour accéder au
+contenu sur SEND.`;
+const showcase2_2 = `Si tu actives le service « Notifications numériques » de SEND, tu
+peux recevoir et gérer directement dans l’application les communications
+à valeur juridique. Si tu n’as pas de PEC et que tu lis
+immédiatement le message, tu ne recevras pas le courrier
+recommandé papier et la notification te sera légalement remise.`;
+const showcase2_3 = `En outre, tu peux également choisir de recevoir une notification
+de courtoisie à ton adresse électronique ou par SMS. Si tu n’as
+pas de PEC et que tu accèdes à la plateforme à partir du lien
+approprié, tu ne recevras pas le courrier recommandé papier et la
+notification te sera légalement remise.`;
+
 export const pfShowcases: Array<IShowcaseData> = [
   {
     name: "showcase 1",
@@ -131,26 +172,38 @@ export const pfShowcases: Array<IShowcaseData> = [
         {
           icon: <PiggyIcon />,
           title: "Commodité",
-          subtitle:
-            "L’envoi de notifications numériques entraîne une réduction des coûts de notification et d’expédition",
+          subtitle: (
+            <Typography variant="body2" tabIndex={0} aria-label={showcase1_1}>
+              {showcase1_1}
+            </Typography>
+          ),
         },
         {
           icon: <HourglassIcon />,
           title: "Temps",
-          subtitle:
-            "Plus d’attente ou de files d’attente pour le retrait des communications papier",
+          subtitle: (
+            <Typography variant="body2" tabIndex={0} aria-label={showcase1_2}>
+              {showcase1_2}
+            </Typography>
+          ),
         },
         {
           icon: <EcologyIcon />,
           title: "Développement durable",
-          subtitle:
-            "Contribue à réduire la consommation de papier et les émissions liées au transport",
+          subtitle: (
+            <Typography variant="body2" tabIndex={0} aria-label={showcase1_3}>
+              {showcase1_3}
+            </Typography>
+          ),
         },
         {
           icon: <CloudIcon />,
           title: "Espace",
-          subtitle:
-            "Tu n’as plus besoin de conserver les documents imprimés, grâce à la possibilité de télécharger et d’archiver les documents numériques",
+          subtitle: (
+            <Typography variant="body2" tabIndex={0} aria-label={showcase1_4}>
+              {showcase1_4}
+            </Typography>
+          ),
         },
       ],
     },
@@ -164,11 +217,8 @@ export const pfShowcases: Array<IShowcaseData> = [
           icon: <PECIcon />,
           title: "PEC",
           subtitle: (
-            <Typography variant="body2">
-              Si tu as une adresse PEC, les notifications te seront légalement
-              remises, sans plus de courriers recommandés papier. L’avis de
-              réception qui te sera envoyé contient le lien pour accéder au
-              contenu sur SEND.
+            <Typography variant="body2" tabIndex={0} aria-label={showcase2_1}>
+              {showcase2_1}
             </Typography>
           ),
         },
@@ -180,7 +230,7 @@ export const pfShowcases: Array<IShowcaseData> = [
           icon: <img src={`${IMAGES_PATH}/IOIcon.svg`} />,
           title: "Application IO",
           subtitle: (
-            <Typography variant="body2">
+            <Typography variant="body2" tabIndex={0} aria-label={showcase2_2}>
               Si tu actives le service « Notifications numériques » de SEND, tu
               peux recevoir et gérer{" "}
               <strong>directement dans l’application</strong> les communications
@@ -194,12 +244,8 @@ export const pfShowcases: Array<IShowcaseData> = [
           icon: <MessageIcon />,
           title: "E-mail ou SMS",
           subtitle: (
-            <Typography variant="body2">
-              En outre, tu peux également choisir de recevoir une notification
-              de courtoisie à ton adresse électronique ou par SMS. Si tu n’as
-              pas de PEC et que tu accèdes à la plateforme à partir du lien
-              approprié, tu ne recevras pas le courrier recommandé papier et la
-              notification te sera légalement remise.
+            <Typography variant="body2" tabIndex={0} aria-label={showcase2_3}>
+              {showcase2_3}
             </Typography>
           ),
         },
@@ -210,48 +256,65 @@ export const pfShowcases: Array<IShowcaseData> = [
 /* ************************************** */
 
 /** Walkthrough mocked data */
+const walkthrough1 = `Pour chaque notification, la plateforme vérifie qu’il y a une PEC qui t’est associée 
+ou que tu as indiquée pour l’envoi de l’avis de réception. Ensuite, elle envoie un avis 
+de courtoisie à tes autres coordonnées numériques (application IO, e-mail et SMS). Si 
+tu n’as pas indiqué de coordonnée numérique et que tu n’as pas accès à la plateforme, 
+tu recevras un courrier recommandé papier.
+`;
+const walkthrough2 = `À partir du message reçu, tu peux accéder à la plateforme pour lire la notification et 
+télécharger les pièces jointes correspondantes. Si tu actives le service sur IO, tu 
+peux visualiser le contenu directement dans l’application : cela équivaut à signer le 
+reçu de retour d’un courrier recommandé traditionnel.
+`;
+const walkthrough3 = `S’il y a un montant à payer, grâce à l’intégration avec pagoPA, tu peux procéder 
+simultanément en ligne depuis la plateforme ou directement depuis l’IO. Si tu préfères 
+te rendre à un guichet, tu devras avoir avec toi le formulaire de paiement reçu avec 
+la notification.
+`;
+const walkthrough4 = `Si tu le souhaites, tu peux déléguer d’autres personnes, physiques ou morales, à recevoir 
+tes notifications en ligne ou à retirer les documents joints en version papier à n’importe 
+quel bureau de poste.
+`;
+
 export const pfWalkthrough: WalkthroughProps = {
   title: "Comment ça fonctionne ?",
   items: [
     {
       icon: <NotificationIcon color="primary" />,
       title: "Tu reçois la notification",
-      subtitle: `
-          Pour chaque notification, la plateforme vérifie qu’il y a une PEC qui t’est associée 
-          ou que tu as indiquée pour l’envoi de l’avis de réception. Ensuite, elle envoie un avis 
-          de courtoisie à tes autres coordonnées numériques (application IO, e-mail et SMS). Si 
-          tu n’as pas indiqué de coordonnée numérique et que tu n’as pas accès à la plateforme, 
-          tu recevras un courrier recommandé papier.
-        `,
+      subtitle: (
+        <Typography variant="body2" tabIndex={0} aria-label={walkthrough1}>
+          {walkthrough1}
+        </Typography>
+      ),
     },
     {
       icon: <DocCheckIcon color="primary" />,
       title: "Lis le contenu",
-      subtitle: `
-          À partir du message reçu, tu peux accéder à la plateforme pour lire la notification et 
-          télécharger les pièces jointes correspondantes. Si tu actives le service sur IO, tu 
-          peux visualiser le contenu directement dans l’application : cela équivaut à signer le 
-          reçu de retour d’un courrier recommandé traditionnel.
-        `,
+      subtitle: (
+        <Typography variant="body2" tabIndex={0} aria-label={walkthrough2}>
+          {walkthrough2}
+        </Typography>
+      ),
     },
     {
       icon: <WalletIcon color="primary" />,
       title: "Paye les frais",
-      subtitle: `
-          S’il y a un montant à payer, grâce à l’intégration avec pagoPA, tu peux procéder 
-          simultanément en ligne depuis la plateforme ou directement depuis l’IO. Si tu préfères 
-          te rendre à un guichet, tu devras avoir avec toi le formulaire de paiement reçu avec 
-          la notification.
-        `,
+      subtitle: (
+        <Typography variant="body2" tabIndex={0} aria-label={walkthrough3}>
+          {walkthrough3}
+        </Typography>
+      ),
     },
     {
       icon: <DelegationIcon color="primary" />,
       title: "Tu peux déléguer ou être délégué",
-      subtitle: `
-          Si tu le souhaites, tu peux déléguer d’autres personnes, physiques ou morales, à recevoir 
-          tes notifications en ligne ou à retirer les documents joints en version papier à n’importe 
-          quel bureau de poste.
-        `,
+      subtitle: (
+        <Typography variant="body2" tabIndex={0} aria-label={walkthrough4}>
+          {walkthrough4}
+        </Typography>
+      ),
       isSequential: false,
     },
   ],
