c3b9f2fdcbc9504411b7d0ffe975ad747e3f7e18
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2023-07-06 11:37:25 +0200

    slight style change already agreed with Arianna Menini

--- packages/pn-commons/src/components/Data/ItemsTable.tsx
+++ packages/pn-commons/src/components/Data/ItemsTable.tsx
@@ -133,7 +133,8 @@ function ItemsTable<ColumnId extends string>({
                       {column.onClick && (
                         <>
                           {/* Even there is a onClick function on the TableCell, leave ButtonNaked below as is.
-                          This makes spacebar key with accessibility to trigger the onClick function. */}
+                          This makes spacebar key with accessibility to trigger the onClick function.
+                          The ButtonNaked "inherits" the onClick action from the outer TableCell, so that is not necessary to replicate it. */}
                           <ButtonNaked
                             tabIndex={column.disableAccessibility ? -1 : 0}
                             sx={buttonNakedInheritStyle}
