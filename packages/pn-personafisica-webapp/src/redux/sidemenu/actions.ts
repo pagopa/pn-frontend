d1f8a8286fc68c6a7bab7b8211020731c945b641
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2023-07-12 10:47:12 +0200

    particular handling for App IO contacts

--- packages/pn-personafisica-webapp/src/redux/sidemenu/actions.ts
+++ packages/pn-personafisica-webapp/src/redux/sidemenu/actions.ts
@@ -2,7 +2,7 @@ import { performThunkAction } from '@pagopa-pn/pn-commons';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ContactsApi } from '../../api/contacts/Contacts.api';
 import { DelegationsApi } from '../../api/delegations/Delegations.api';
-import { DigitalAddress } from '../../models/contacts';
+import { CourtesyChannelType, DigitalAddress, IOAllowedValues } from '../../models/contacts';
 import { Delegator } from '../delegation/types';
 
 export enum SIDEMENU_ACTIONS  {
@@ -21,10 +21,14 @@ export const getSidemenuInformation = createAsyncThunk<Array<Delegator>>(
   }
 );
 
+// PN-7095 - per capire quale categorie di recapito far vedere nel DomicileBanner
+// si devono prendere gli indirizzi default, tranne per AppIO che si prendono tutti (senza verificare default) 
+// il cui valore non sia DISABLED (cfr. description e commenti della issue JIRA)
 export const getDomicileInfo = createAsyncThunk<Array<DigitalAddress>>(
   'getDomicileInfo',
   performThunkAction(async () => {
-    const isDefaultAddress = (address: DigitalAddress) => address.senderId === 'default';
+    const isDefaultAddress = (address: DigitalAddress) => 
+      address.senderId === 'default' || (address.channelType === CourtesyChannelType.IOMSG && address.value !== IOAllowedValues.DISABLED);
     const allAddresses = await ContactsApi.getDigitalAddresses();
     return [...allAddresses.legal.filter(isDefaultAddress), ...allAddresses.courtesy.filter(isDefaultAddress)];
   }
