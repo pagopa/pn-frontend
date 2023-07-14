4d9d9036a1f16495eb9c6b64e4f31799e751648e
Andrea Cimini <andrea.cimini@pagopa.it>
2023-07-14 16:25:34 +0200

    MIME type try 2

--- packages/pn-commons/src/components/Layout/Layout.tsx
+++ packages/pn-commons/src/components/Layout/Layout.tsx
@@ -107,7 +107,7 @@ export default function Layout({
               component="main"
             >
               <ErrorBoundary eventTrackingCallback={eventTrackingCallbackAppCrash}>
-                {"PROVA MIME TYPE 1"}
+                {"PROVA MIME TYPE 2"}
                 {children}
               </ErrorBoundary>
             </Box>
