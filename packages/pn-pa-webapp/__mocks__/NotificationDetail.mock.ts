4c94616acbb9a05ac57523e1dc5d875df50dc92a
alessandrogelmi <alessandro.gelmi@cinetix-team.it>
2023-07-28 15:18:59 +0200

    feat: created mock of notification detail API

---
+++ packages/pn-pa-webapp/__mocks__/NotificationDetail.mock.ts
@@ -0,0 +1,359 @@
+import {
+  NotificationDetail,
+  NotificationFeePolicy,
+  RecipientType,
+  DigitalDomicileType,
+  NotificationStatus,
+  TimelineCategory,
+  LegalFactType,
+  AddressSource,
+  PhysicalCommunicationType,
+  parseNotificationDetail,
+  ExtRegistriesPaymentDetails,
+  PaymentInfoDetail,
+  PaymentStatus,
+} from '@pagopa-pn/pn-commons';
+
+export const PA_NOTIFICATION_FROM_BE: NotificationDetail = {
+  iun: 'c_b963-220220221119',
+  paProtocolNumber: '220220221119',
+  subject: 'Prova - status',
+  abstract: 'mocked-abstract',
+  sentAt: '2022-02-21T10:19:33.440Z',
+  cancelledIun: 'mocked-cancelledIun',
+  cancelledByIun: 'mocked-cancelledByIun',
+  documentsAvailable: true,
+  notificationFeePolicy: NotificationFeePolicy.DELIVERY_MODE,
+  senderPaId: 'mocked-senderPaId',
+  recipients: [
+    {
+      recipientType: RecipientType.PF,
+      taxId: 'CGNNMO80A03H501U',
+      denomination: 'Analogico Ok',
+      digitalDomicile: {
+        address: 'mail@pec.it',
+        type: DigitalDomicileType.PEC,
+      },
+      physicalAddress: {
+        at: 'Presso qualcuno',
+        address: 'In via del tutto eccezionale',
+        addressDetails: 'scala A',
+        zip: '00100',
+        municipality: 'Comune',
+        province: 'PROV',
+        foreignState: '',
+      },
+      payments: [
+        {
+          pagoPA: {
+            creditorTaxId: '302011689142547191',
+            noticeCode: '77777777777',
+            applyCostFlg: true,
+            attachment: {
+              digests: {
+                sha256: 'jezIVxlG1M1woCSUngM6KipUN3/p8cG5RMIPnuEanlE=',
+              },
+              contentType: 'application/pdf',
+              ref: {
+                key: 'PN_NOTIFICATION_ATTACHMENTS-4727f193467c4c5cb26a848f0ea5aee0.pdf',
+                versionToken: 'v1',
+              },
+            },
+          },
+        },
+      ],
+    },
+  ],
+  documents: [
+    {
+      digests: {
+        sha256: '3b56e2b5641d5807fa83d6bc906a35135a6b8b7c21e694b859bbafc3d718d659',
+      },
+      contentType: 'application/pdf',
+      title: 'Mocked document',
+      ref: {
+        key: 'Doc1',
+        versionToken: 'mocked-versionToken',
+      },
+      docIdx: '0',
+    },
+  ],
+  notificationStatus: NotificationStatus.DELIVERED,
+  notificationStatusHistory: [
+    {
+      status: NotificationStatus.DELIVERED,
+      activeFrom: '2022-02-22T10:19:33.440Z',
+      relatedTimelineElements: [],
+    },
+  ],
+  timeline: [
+    {
+      elementId: 'c_b429-202203021814_start',
+      timestamp: '2022-03-02T17:56:46.668Z',
+      category: TimelineCategory.REQUEST_ACCEPTED,
+      details: {},
+      legalFactsIds: [
+        {
+          key: 'sender_ack~0f4Z32eLEiX8NSYR4WYzyvQvnQHh1t7Z',
+          category: LegalFactType.SENDER_ACK,
+        },
+      ],
+    },
+    {
+      elementId: 'c_b429-202203021814_deliveryMode_rec0',
+      timestamp: '2022-03-02T17:56:50.303Z',
+      category: TimelineCategory.SEND_ANALOG_DOMICILE,
+      details: {
+        physicalAddress: {
+          at: '',
+          address: '',
+          addressDetails: '',
+          zip: '',
+          municipality: '',
+          province: '',
+          foreignState: '',
+        },
+      },
+    },
+    {
+      elementId: 'c_b429-202203021814_send_pec_rec0_PLATFORM_n1',
+      timestamp: '2022-03-02T17:56:53.636Z',
+      category: TimelineCategory.SEND_DIGITAL_DOMICILE,
+      details: {
+        digitalAddress: {
+          type: DigitalDomicileType.EMAIL,
+          address: 'mocked@email.it',
+        },
+        digitalAddressSource: AddressSource.GENERAL,
+        retryNumber: 1,
+      },
+    },
+    {
+      elementId: 'c_b429-202203021814_send_pec_rec0_SPECIAL_n1',
+      timestamp: '2022-03-02T17:56:56.856Z',
+      category: TimelineCategory.SEND_DIGITAL_DOMICILE,
+      details: {
+        digitalAddress: {
+          type: DigitalDomicileType.PEC,
+          address: 'nome.cognome@works.demo.it',
+        },
+        digitalAddressSource: AddressSource.GENERAL,
+        retryNumber: 1,
+      },
+    },
+    {
+      elementId: 'c_b429-202203021814_recipient_timeout_rec0',
+      timestamp: '2022-03-02T17:59:10.029Z',
+      category: TimelineCategory.REFINEMENT,
+      details: {},
+    },
+    {
+      elementId: 'c_b4239-202203021814_recipient_timeout_rec0',
+      timestamp: '2022-03-02T17:59:10.029Z',
+      category: TimelineCategory.PAYMENT,
+      details: {
+        paymentSourceChannel: 'EXTERNAL_REGISTRY',
+        recipientType: RecipientType.PF,
+        recIndex: 0,
+      },
+    },
+  ],
+  physicalCommunicationType: PhysicalCommunicationType.REGISTERED_LETTER_890,
+  amount: 130,
+};
+
+export const notificationToFe = parseNotificationDetail(PA_NOTIFICATION_FROM_BE);
+
+export const PA_NOTIFICATION_FROM_BE_MULTI_RECIPIENT: NotificationDetail = {
+  iun: 'c_b963-220220221119',
+  paProtocolNumber: '220220221119',
+  subject: 'Prova - status',
+  abstract: 'mocked-abstract',
+  sentAt: '2022-02-21T10:19:33.440Z',
+  cancelledIun: 'mocked-cancelledIun',
+  cancelledByIun: 'mocked-cancelledByIun',
+  documentsAvailable: true,
+  senderPaId: 'mocked-senderPaId',
+  notificationFeePolicy: NotificationFeePolicy.DELIVERY_MODE,
+  recipients: [
+    {
+      recipientType: RecipientType.PF,
+      taxId: 'CGNNMO80A03H501U',
+      denomination: 'Analogico Ok',
+      digitalDomicile: {
+        address: 'mail@pec.it',
+        type: DigitalDomicileType.PEC,
+      },
+      physicalAddress: {
+        at: 'Presso qualcuno',
+        address: 'In via del tutto eccezionale',
+        addressDetails: 'scala A',
+        zip: '00100',
+        municipality: 'Comune',
+        province: 'PROV',
+        foreignState: '',
+      },
+      payments: [
+        {
+          pagoPA: {
+            creditorTaxId: '302011689142547191',
+            noticeCode: '77777777777',
+            applyCostFlg: true,
+            attachment: {
+              digests: {
+                sha256: 'jezIVxlG1M1woCSUngM6KipUN3/p8cG5RMIPnuEanlE=',
+              },
+              contentType: 'application/pdf',
+              ref: {
+                key: 'PN_NOTIFICATION_ATTACHMENTS-4727f193467c4c5cb26a848f0ea5aee0.pdf',
+                versionToken: 'v1',
+              },
+            },
+          },
+        },
+      ],
+    },
+    {
+      recipientType: RecipientType.PF,
+      taxId: 'CGNNMO80A03H501V',
+      denomination: 'Altro Ok',
+      digitalDomicile: {
+        address: 'mail2@pec.it',
+        type: DigitalDomicileType.PEC,
+      },
+      physicalAddress: {
+        at: 'Presso qualcuno',
+        address: 'In via del tutto eccezionale',
+        addressDetails: 'scala A',
+        zip: '00100',
+        municipality: 'Comune',
+        province: 'PROV',
+        foreignState: '',
+      },
+      payments: [
+        {
+          pagoPA: {
+            creditorTaxId: '302011689142547191',
+            noticeCode: '77777777777',
+            applyCostFlg: true,
+            attachment: {
+              digests: {
+                sha256: 'jezIVxlG1M1woCSUngM6KipUN3/p8cG5RMIPnuEanlE=',
+              },
+              contentType: 'application/pdf',
+              ref: {
+                key: 'PN_NOTIFICATION_ATTACHMENTS-4727f193467c4c5cb26a848f0ea5aee0.pdf',
+                versionToken: 'v1',
+              },
+            },
+          },
+        },
+      ],
+    },
+  ],
+  documents: [
+    {
+      digests: {
+        sha256: '3b56e2b5641d5807fa83d6bc906a35135a6b8b7c21e694b859bbafc3d718d659',
+      },
+      contentType: 'application/pdf',
+      title: 'Mocked document',
+      ref: {
+        key: 'Doc1',
+        versionToken: 'mocked-versionToken',
+      },
+      docIdx: '0',
+    },
+  ],
+  notificationStatus: NotificationStatus.DELIVERED,
+  notificationStatusHistory: [
+    {
+      status: NotificationStatus.DELIVERED,
+      activeFrom: '2022-02-22T10:19:33.440Z',
+      relatedTimelineElements: [],
+    },
+  ],
+  timeline: [
+    {
+      elementId: 'c_b429-202203021814_start',
+      timestamp: '2022-03-02T17:56:46.668Z',
+      category: TimelineCategory.REQUEST_ACCEPTED,
+      details: {},
+      legalFactsIds: [
+        {
+          key: 'sender_ack~0f4Z32eLEiX8NSYR4WYzyvQvnQHh1t7Z',
+          category: LegalFactType.SENDER_ACK,
+        },
+      ],
+    },
+    {
+      elementId: 'c_b429-202203021814_deliveryMode_rec0',
+      timestamp: '2022-03-02T17:56:50.303Z',
+      category: TimelineCategory.SEND_ANALOG_DOMICILE,
+      details: {
+        physicalAddress: {
+          at: '',
+          address: '',
+          addressDetails: '',
+          zip: '',
+          municipality: '',
+          province: '',
+          foreignState: '',
+        },
+      },
+    },
+    {
+      elementId: 'c_b429-202203021814_send_pec_rec0_PLATFORM_n1',
+      timestamp: '2022-03-02T17:56:53.636Z',
+      category: TimelineCategory.SEND_DIGITAL_DOMICILE,
+      details: {
+        digitalAddress: {
+          type: DigitalDomicileType.EMAIL,
+          address: 'mocked@email.it',
+        },
+        digitalAddressSource: AddressSource.GENERAL,
+        retryNumber: 1,
+      },
+    },
+    {
+      elementId: 'c_b429-202203021814_send_pec_rec0_SPECIAL_n1',
+      timestamp: '2022-03-02T17:56:56.856Z',
+      category: TimelineCategory.SEND_DIGITAL_DOMICILE,
+      details: {
+        digitalAddress: {
+          type: DigitalDomicileType.PEC,
+          address: 'nome.cognome@works.demo.it',
+        },
+        digitalAddressSource: AddressSource.GENERAL,
+        retryNumber: 1,
+      },
+    },
+    {
+      elementId: 'c_b429-202203021814_recipient_timeout_rec0',
+      timestamp: '2022-03-02T17:59:10.029Z',
+      category: TimelineCategory.REFINEMENT,
+      details: {
+        recIndex: 0,
+      },
+    },
+  ],
+  physicalCommunicationType: PhysicalCommunicationType.REGISTERED_LETTER_890,
+  amount: 200,
+};
+
+export const notificationToFeMultiRecipient = parseNotificationDetail(
+  PA_NOTIFICATION_FROM_BE_MULTI_RECIPIENT
+);
+
+export const EXTERNAL_REGISTRIES_MOCK: ExtRegistriesPaymentDetails = {
+  creditorTaxId: 'string',
+  noticeCode: 'string',
+  status: PaymentStatus.REQUIRED,
+  detail: PaymentInfoDetail.PAYMENT_UNAVAILABLE,
+  detail_v2: 'PPT_PSP_SCONOSCIUTO',
+  errorCode: 'PPT_AUTORIZZAZIONE',
+  amount: 1200,
+  url: 'https://api.uat.platform.pagopa.it/checkout/auth/payments/v2',
+  causaleVersamento: 'Seconda rata TARI',
+  dueDate: '2025-07-31',
+};
---
+++ packages/pn-personagiuridica-webapp/__mocks__/NotificationDetail.mock.ts
@@ -0,0 +1,244 @@
+import {
+  AddressSource,
+  DigitalDomicileType,
+  ExtRegistriesPaymentDetails,
+  INotificationDetailTimeline,
+  LegalFactType,
+  NotificationDetail,
+  NotificationDetailRecipient,
+  NotificationFeePolicy,
+  NotificationStatus,
+  NotificationStatusHistory,
+  PaymentInfoDetail,
+  PaymentStatus,
+  PhysicalCommunicationType,
+  RecipientType,
+  TimelineCategory,
+  parseNotificationDetail,
+} from '@pagopa-pn/pn-commons';
+
+const recipients: Array<NotificationDetailRecipient> = [
+  {
+    recipientType: RecipientType.PG,
+    taxId: 'CCRMCT06A03A433H',
+    denomination: 'Totito',
+    digitalDomicile: {
+      type: DigitalDomicileType.PEC,
+      address: 'totito@pec.pagopa.it',
+    },
+    physicalAddress: {
+      at: 'Presso',
+      address: 'VIA SENZA NOME',
+      addressDetails: 'SCALA B',
+      zip: '87100',
+      municipality: 'MILANO',
+      municipalityDetails: 'MILANO',
+      province: 'MI',
+      foreignState: 'ITALIA',
+    },
+    payments: [
+      {
+        pagoPA: {
+          creditorTaxId: '302011689142547191',
+          noticeCode: '77777777777',
+          applyCostFlg: true,
+          attachment: {
+            digests: {
+              sha256: 'jezIVxlG1M1woCSUngM6KipUN3/p8cG5RMIPnuEanlE=',
+            },
+            contentType: 'application/pdf',
+            ref: {
+              key: 'PN_NOTIFICATION_ATTACHMENTS-4727f193467c4c5cb26a848f0ea5aee0.pdf',
+              versionToken: 'v1',
+            },
+          },
+        },
+      },
+    ],
+  },
+];
+
+const statusHistory: Array<NotificationStatusHistory> = [
+  {
+    status: NotificationStatus.ACCEPTED,
+    activeFrom: '2023-07-12T06:15:50.416429021Z',
+    relatedTimelineElements: [
+      'REQUEST_ACCEPTED.IUN_AGZY-LZEZ-HDJW-202307-Z-1',
+      'AAR_CREATION_REQUEST.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+      'AAR_GEN.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+      'SEND_COURTESY_MESSAGE.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.COURTESYADDRESSTYPE_EMAIL',
+      'PROBABLE_SCHEDULING_ANALOG_DATE.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+      'SEND_COURTESY_MESSAGE.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.COURTESYADDRESSTYPE_SMS',
+      'GET_ADDRESS.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.SOURCE_PLATFORM.ATTEMPT_0',
+      'GET_ADDRESS.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.SOURCE_SPECIAL.ATTEMPT_0',
+    ],
+  },
+  {
+    status: NotificationStatus.DELIVERING,
+    activeFrom: '2023-07-12T06:17:32.938009007Z',
+    relatedTimelineElements: [
+      'SEND_DIGITAL.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.SOURCE_SPECIAL.REPEAT_false.ATTEMPT_0',
+      'DIGITAL_PROG.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.SOURCE_SPECIAL.REPEAT_false.ATTEMPT_0.IDX_1',
+      'SEND_DIGITAL_FEEDBACK.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0.SOURCE_SPECIAL.REPEAT_false.ATTEMPT_0',
+      'DIGITAL_DELIVERY_CREATION_REQUEST.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+    ],
+  },
+  {
+    status: NotificationStatus.DELIVERED,
+    activeFrom: '2023-07-12T06:18:12.651426861Z',
+    relatedTimelineElements: [
+      'DIGITAL_SUCCESS_WORKFLOW.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+      'SCHEDULE_REFINEMENT_WORKFLOW.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+    ],
+  },
+  {
+    status: NotificationStatus.VIEWED,
+    activeFrom: '2023-07-12T06:18:26.109470956Z',
+    relatedTimelineElements: [
+      'NOTIFICATION_VIEWED.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+      'NOTIFICATION_VIEWED_CREATION_REQUEST.IUN_AGZY-LZEZ-HDJW-202307-Z-1.RECINDEX_0',
+    ],
+  },
+];
+
+const timeline: Array<INotificationDetailTimeline> = [
+  {
+    elementId: 'c_b429-202203021814_start',
+    timestamp: '2022-03-02T17:56:46.668Z',
+    category: TimelineCategory.REQUEST_ACCEPTED,
+    details: { recIndex: 0 },
+    legalFactsIds: [
+      {
+        key: 'sender_ack~0f4Z32eLEiX8NSYR4WYzyvQvnQHh1t7Z',
+        category: LegalFactType.SENDER_ACK,
+      },
+    ],
+  },
+  {
+    elementId: 'c_b429-202203021814_deliveryMode_rec0',
+    timestamp: '2022-03-02T17:56:50.303Z',
+    category: TimelineCategory.SEND_ANALOG_DOMICILE,
+    details: {
+      physicalAddress: {
+        at: '',
+        address: '',
+        addressDetails: '',
+        zip: '',
+        municipality: '',
+        province: '',
+        foreignState: '',
+      },
+    },
+  },
+  {
+    elementId: 'c_b429-202203021814_send_pec_rec0_PLATFORM_n1',
+    timestamp: '2022-03-02T17:56:53.636Z',
+    category: TimelineCategory.SEND_DIGITAL_DOMICILE,
+    details: {
+      recIndex: 0,
+      digitalAddress: {
+        type: DigitalDomicileType.EMAIL,
+        address: 'nome.cognome@works.demo.it',
+      },
+      digitalAddressSource: AddressSource.GENERAL,
+      retryNumber: 1,
+    },
+  },
+  {
+    elementId: 'c_b429-202203021814_send_pec_rec0_SPECIAL_n1',
+    timestamp: '2022-03-02T17:56:56.856Z',
+    category: TimelineCategory.SEND_DIGITAL_DOMICILE,
+    details: {
+      digitalAddress: {
+        type: DigitalDomicileType.PEC,
+        address: 'nome.cognome@works.demo.it',
+      },
+      digitalAddressSource: AddressSource.GENERAL,
+      retryNumber: 1,
+    },
+  },
+  {
+    elementId: 'c_b429-202203021814_send_pec_result_rec0_SPECIAL_n1',
+    timestamp: '2022-03-02T17:57:03.284Z',
+    category: TimelineCategory.NOTIFICATION_VIEWED,
+    legalFactsIds: [
+      {
+        key: 'sender_ack-toto1',
+        category: LegalFactType.SENDER_ACK,
+      },
+    ],
+    details: {
+      digitalAddress: {
+        type: DigitalDomicileType.PEC,
+        address: 'nome.cognome@works.demo.it',
+      },
+      digitalAddressSource: AddressSource.GENERAL,
+      retryNumber: 1,
+    },
+  },
+  {
+    elementId: 'c_b429-202203021814_send_courtesy_rec0',
+    timestamp: '2022-03-02T17:57:06.819Z',
+    category: TimelineCategory.NOTIFICATION_VIEWED,
+    details: {
+      recIndex: 0,
+    },
+    legalFactsIds: [
+      {
+        key: 'digital_delivery_info_ed84b8c9-444e-410d-80d7-cfad6aa12070~QDr7GVmbdGkJJFEgxi0OlxPs.l2F2Wq.',
+        category: LegalFactType.DIGITAL_DELIVERY,
+      },
+    ],
+  },
+  {
+    elementId: 'c_b429-202203021814_recipient_timeout_rec0',
+    timestamp: '2022-03-02T17:59:10.029Z',
+    category: TimelineCategory.REFINEMENT,
+    details: {},
+  },
+];
+
+export const PG_NOTIFICATION_FROM_BE: NotificationDetail = {
+  iun: 'AGZY-LZEZ-HDJW-202307-Z-1',
+  paProtocolNumber: '302011689142547177',
+  subject: 'Mock notification',
+  abstract: 'Abstract della notifica',
+  sentAt: '2023-07-12T06:15:50.416429021Z',
+  senderDenomination: 'Comune di Milano',
+  documentsAvailable: true,
+  notificationFeePolicy: NotificationFeePolicy.DELIVERY_MODE,
+  senderPaId: '026e8c72-7944-4dcd-8668-f596447fec6d',
+  recipients: [recipients[0]],
+  documents: [
+    {
+      digests: {
+        sha256: 'jezIVxlG1M1woCSUngM6KipUN3/p8cG5RMIPnuEanlE=',
+      },
+      contentType: 'application/pdf',
+      ref: {
+        key: 'PN_NOTIFICATION_ATTACHMENTS-a62a5bb5813045cbafc87f55828feb24.pdf',
+        versionToken: 'v1',
+      },
+      docIdx: '0',
+    },
+  ],
+  notificationStatus: NotificationStatus.PAID,
+  notificationStatusHistory: statusHistory,
+  timeline,
+  physicalCommunicationType: PhysicalCommunicationType.REGISTERED_LETTER_890,
+};
+
+export const notificationToFe = parseNotificationDetail(PG_NOTIFICATION_FROM_BE);
+
+export const EXTERNAL_REGISTRIES_MOCK: ExtRegistriesPaymentDetails = {
+  creditorTaxId: 'string',
+  noticeCode: 'string',
+  status: PaymentStatus.REQUIRED,
+  detail: PaymentInfoDetail.PAYMENT_UNAVAILABLE,
+  detail_v2: 'PPT_PSP_SCONOSCIUTO',
+  errorCode: 'PPT_AUTORIZZAZIONE',
+  amount: 1200,
+  url: 'https://api.uat.platform.pagopa.it/checkout/auth/payments/v2',
+  causaleVersamento: 'Seconda rata TARI',
+  dueDate: '2025-07-31',
+};
