6b7393c0dde73380cee8a433b3cda8d36b0276f6
Andrea Cimini <andrea.cimini@pagopa.it>
2023-08-16 17:39:23 +0200

    Recipient select for notifications multi recipients

--- packages/pn-pa-webapp/src/pages/components/Notifications/__test__/NotificationPaymentSender.test.tsx
+++ packages/pn-pa-webapp/src/pages/components/Notifications/__test__/NotificationPaymentSender.test.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
-import { render } from '../../../../__test__/test-utils';
 import {
   notificationToFe,
   notificationToFeMultiRecipient,
 } from '../../../../__mocks__/NotificationDetail.mock';
+import {
+  RenderResult,
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  within,
+} from '../../../../__test__/test-utils';
 import NotificationPaymentSender from '../NotificationPaymentSender';
 
 jest.mock('react-i18next', () => ({
@@ -17,17 +25,54 @@ jest.mock('react-i18next', () => ({
 describe('NotificationPaymentSender Component', () => {
   it('renders component - one recipient', () => {
     // render component
-    const result = render(<NotificationPaymentSender recipients={notificationToFe.recipients} />);
-    expect(result.container).toHaveTextContent('payment.title');
-    expect(result.container).toHaveTextContent('payment.subtitle-single');
+    const { container, queryByTestId } = render(
+      <NotificationPaymentSender recipients={notificationToFe.recipients} />
+    );
+    expect(container).toHaveTextContent('payment.title');
+    expect(container).toHaveTextContent('payment.subtitle-single');
+    const recipientsSelect = queryByTestId('recipients-select');
+    expect(recipientsSelect).not.toBeInTheDocument();
   });
 
   it('renders component - multi recipient', () => {
     // render component
-    const result = render(
+    const { container, getByTestId } = render(
       <NotificationPaymentSender recipients={notificationToFeMultiRecipient.recipients} />
     );
-    expect(result.container).toHaveTextContent('payment.title');
-    expect(result.container).toHaveTextContent('payment.subtitle-multiple');
+    expect(container).toHaveTextContent('payment.title');
+    expect(container).toHaveTextContent('payment.subtitle-multiple');
+    const recipientsSelect = getByTestId('recipients-select');
+    expect(recipientsSelect).toBeInTheDocument();
+  });
+
+  it('select a recipient', async () => {
+    let result: RenderResult;
+    // render component
+    await act(async () => {
+      result = render(
+        <NotificationPaymentSender recipients={notificationToFeMultiRecipient.recipients} />
+      );
+    });
+    // select a recipient
+    const recipientsSelect = document.querySelector('div[id="recipients-select"]');
+    const selectInput = document.querySelector(`input[name="recipients-select"]`);
+    fireEvent.mouseDown(recipientsSelect!);
+    const selectOptionsContainer = await waitFor(() => screen.queryByRole('presentation'));
+    expect(selectOptionsContainer).toBeInTheDocument();
+    const selectOptionsList = await within(selectOptionsContainer!).findByRole('listbox');
+    expect(selectOptionsList).toBeInTheDocument();
+    const selectOptionsListItems = await within(selectOptionsList).findAllByRole('option');
+    expect(selectOptionsListItems).toHaveLength(notificationToFeMultiRecipient.recipients.length);
+    selectOptionsListItems.forEach((opt, index) => {
+      expect(opt).toHaveTextContent(
+        notificationToFeMultiRecipient.recipients[index].denomination +
+          ' - ' +
+          notificationToFeMultiRecipient.recipients[index].taxId
+      );
+    });
+    fireEvent.click(selectOptionsListItems[1]);
+    await waitFor(() => {
+      expect(selectInput).toHaveValue(notificationToFeMultiRecipient.recipients[1].taxId);
+    });
   });
 });
