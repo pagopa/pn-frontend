eb7cac2b0df38697cb3bbc2d3d58df12d7031410
leleOFA <emanuele.didonato@pagopa.it>
2023-08-23 14:05:33 +0200

    refactor: delete error already cancelled

--- packages/pn-personafisica-webapp/src/utils/AppError/PFAppErrorFactory.ts
+++ packages/pn-personafisica-webapp/src/utils/AppError/PFAppErrorFactory.ts
@@ -6,7 +6,6 @@ import {
 } from '@pagopa-pn/pn-commons';
 
 import { ServerResponseErrorCode } from './types';
-import { MandateAlreadyExistsAppError } from './MandateAlreadyExistsAppError';
 import { MandateDelegateHimselfAppError } from './MandateDelegateHimselfAppError';
 import { MandateInvalidVerificationCodeAppError } from './MandateInvalidVerificationCodeAppError';
 import { MandateNotAcceptableAppError } from './MandateNotAcceptableAppError';
@@ -29,8 +28,6 @@ export class PFAppErrorFactory extends AppErrorFactory {
     switch (error.code) {
       case ServerResponseErrorCode.PN_MANDATE_NOTFOUND:
         return new MandateNotFoundAppError(error, this.translateFunction);
-      case ServerResponseErrorCode.PN_MANDATE_ALREADYEXISTS:
-        return new MandateAlreadyExistsAppError(error, this.translateFunction);
       case ServerResponseErrorCode.PN_MANDATE_NOTACCEPTABLE:
         return new MandateNotAcceptableAppError(error, this.translateFunction);
       case ServerResponseErrorCode.PN_MANDATE_DELEGATEHIMSELF:
