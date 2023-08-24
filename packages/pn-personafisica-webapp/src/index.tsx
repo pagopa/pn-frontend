e3eea65945b27f9b5a8b5d0a678907e01d5664fa
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2023-08-24 15:21:44 +0200

    shifted Axios interceptor init from App to index.tsx - in PA / PF / PG

--- packages/pn-personafisica-webapp/src/index.tsx
+++ packages/pn-personafisica-webapp/src/index.tsx
@@ -7,14 +7,15 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { LoadingPage } from '@pagopa-pn/pn-commons';
 import { theme } from '@pagopa/mui-italia';
 
-import App from './App';
 import { initAxiosClients } from './api/apiClients';
-import './i18n';
-import './index.css';
+import { setUpInterceptor } from './api/interceptors';
 import { initStore, store } from './redux/store';
-import reportWebVitals from './reportWebVitals';
 import { loadPfConfiguration } from './services/configuration.service';
 import { initOneTrust } from './utils/onetrust';
+import App from './App';
+import './i18n';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 async function doTheRender() {
   try {
@@ -25,6 +26,8 @@ async function doTheRender() {
     initOneTrust();
     initStore();
     initAxiosClients();
+    // move initialization of the Axios interceptor - PN-7557
+    setUpInterceptor(store);
 
     ReactDOM.render(
       <React.StrictMode>
