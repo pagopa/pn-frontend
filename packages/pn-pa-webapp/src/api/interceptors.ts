4fa92495337d5c7cbe7f4cb812ecfebc1e649b59
leleOFA <emanuele.didonato@pagopa.it>
2023-08-29 16:31:58 +0200

    feat: insert real api for cancel notification and fix path

--- packages/pn-pa-webapp/src/api/interceptors.ts
+++ packages/pn-pa-webapp/src/api/interceptors.ts
@@ -1,9 +1,3 @@
-import {
-  Notification,
-  NotificationDetail,
-  NotificationStatus,
-  TimelineCategory,
-} from '@pagopa-pn/pn-commons';
 import { EnhancedStore } from '@reduxjs/toolkit';
 
 import { apiClient } from './apiClients';
@@ -11,11 +5,6 @@ import { apiClient } from './apiClients';
 export const setUpInterceptor = (store: EnhancedStore) => {
   apiClient.interceptors.request.use(
     (config) => {
-      if (config.url === '/delivery-push/notifications/sent/cancel/PELM-VYNK-XVGV-202308-R-1') {
-        return Promise.reject({ error: true, type: 'cancellation-200' });
-      } else if (config.url?.startsWith('/delivery-push/notifications/sent/cancel/')) {
-        return Promise.reject({ error: true, type: 'cancellation-500' });
-      }
       /* eslint-disable functional/immutable-data */
       const token: string = store.getState().userState.user.sessionToken;
       if (token && config.headers) {
@@ -24,92 +13,5 @@ export const setUpInterceptor = (store: EnhancedStore) => {
       return config;
     },
     (error) => Promise.reject(error)
-  );
-
-  apiClient.interceptors.response.use(
-    (response) => {
-      if (response.config?.url === '/delivery/notifications/sent/NRJP-NZRW-LDTL-202308-L-1') {
-        const data = response.data as NotificationDetail;
-        data.notificationStatus = NotificationStatus.CANCELLATION_IN_PROGRESS;
-        data.timeline.push({
-          elementId: 'NOTIFICATION_CANCELLATION_REQUEST.NRJP-NZRW-LDTL-202308-L-1',
-          timestamp: '2033-08-14T13:42:54.17675939Z',
-          legalFactsIds: [],
-          category: TimelineCategory.NOTIFICATION_CANCELLATION_REQUEST,
-          details: {},
-        });
-        return {
-          data,
-          status: response.status,
-          statusText: '',
-          headers: response.headers,
-          config: response.config,
-          request: response.request,
-        };
-      } else if (
-        response.config?.url === '/delivery/notifications/sent/HYTD-ERPH-WDUE-202308-H-1'
-      ) {
-        const data = response.data as NotificationDetail;
-        data.notificationStatus = NotificationStatus.CANCELLED;
-        data.notificationStatusHistory.push({
-          status: NotificationStatus.CANCELLED,
-          activeFrom: '2043-08-15T13:42:54.17675939Z',
-          relatedTimelineElements: [],
-        });
-        data.timeline.push({
-          elementId: 'NOTIFICATION_CANCELLED.HYTD-ERPH-WDUE-202308-H-1',
-          timestamp: '2033-08-14T13:42:54.17675939Z',
-          legalFactsIds: [],
-          category: TimelineCategory.NOTIFICATION_CANCELLED,
-          details: {},
-        });
-        return {
-          data,
-          status: response.status,
-          statusText: '',
-          headers: response.headers,
-          config: response.config,
-          request: response.request,
-        };
-      } else if (response.config?.url?.startsWith('/delivery/notifications/sent?startDate')) {
-        const data = response.data as { resultsPage: Array<Notification> };
-        const specificIun = data.resultsPage.find(
-          (el: Notification) => el.iun === 'HYTD-ERPH-WDUE-202308-H-1'
-        );
-        if (specificIun) {
-          specificIun.notificationStatus = NotificationStatus.CANCELLED;
-        }
-        return {
-          data,
-          status: response.status,
-          statusText: '',
-          headers: response.headers,
-          config: response.config,
-          request: response.request,
-        };
-      }
-      return response;
-    },
-    (error) => {
-      if (error.error && error.type === 'cancellation-200') {
-        return { data: undefined };
-      } else if (error.error && error.type === 'cancellation-500') {
-        return Promise.reject({
-          response: {
-            status: 500,
-            data: {
-              errors: [
-                {
-                  code: 'PN_GENERIC_ERROR',
-                  element: null,
-                  detail: 'string',
-                },
-              ],
-            },
-          },
-        });
-      }
-      return Promise.reject(error);
-    }
   );
 };
