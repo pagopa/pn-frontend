85783bad9d3b9a4e998ca08218c92a0f1bf11a31
NicolaGiornetta <105795630+NicolaGiornetta@users.noreply.github.com>
2023-08-30 17:05:26 +0200

    fix(pn-7587): fixed clipping statuses tooltips (#966)

--- packages/pn-commons/src/components/__test__/CustomTooltip.test.tsx
+++ packages/pn-commons/src/components/__test__/CustomTooltip.test.tsx
@@ -1,4 +1,6 @@
-import { fireEvent, waitFor, screen } from '@testing-library/react';
+import React from 'react';
+
+import { fireEvent, screen, waitFor } from '@testing-library/react';
 
 import { render } from '../../test-utils';
 import CustomTooltip from '../CustomTooltip';
