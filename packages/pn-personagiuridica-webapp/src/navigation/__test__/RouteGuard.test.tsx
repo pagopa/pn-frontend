6949781839e269802c2adf08c47b178c83eabbb7
Andrea Fuso <andrea.fuso2@gmail.com>
2023-09-06 17:14:12 +0200

    refactored navigation's tests

--- packages/pn-personagiuridica-webapp/src/navigation/__test__/RouteGuard.test.tsx
+++ packages/pn-personagiuridica-webapp/src/navigation/__test__/RouteGuard.test.tsx
@@ -1,49 +1,42 @@
-import { act, screen } from '@testing-library/react';
-import { render } from '../../__test__/test-utils';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { userResponse } from '../../__mocks__/Auth.mock';
+import { act, render, screen } from '../../__test__/test-utils';
 import RouteGuard from '../RouteGuard';
 
-jest.mock('react-router-dom', () => {
-  const original = jest.requireActual('react-router-dom');
-  return {
-    ...original,
-    Outlet: () => <div>Generic Page</div>,
-  };
-});
+const mockReduxState = {
+  userState: {
+    user: userResponse,
+  },
+};
 
-jest.mock('@pagopa-pn/pn-commons', () => {
-  const original = jest.requireActual('@pagopa-pn/pn-commons');
-  return {
-    ...original,
-    AccessDenied: ({ isLogged }: {isLogged: boolean}) => <>
-			<div>Access Denied</div>
-			<div>{isLogged ? "sì è loggato" : "non è loggato"}</div>
-		</>,
-  };
-});
+const Guard = () => (
+  <Routes>
+    <Route path="/" element={<RouteGuard />}>
+      <Route path="/" element={<div>Generic Page</div>} />
+    </Route>
+  </Routes>
+);
 
 describe('RouteGuard component', () => {
   it('Logged user', async () => {
-		const mockReduxState = { 
-			userState: { user: { sessionToken: 'mocked-token' }, tos: true },
-		};
-
-    await act(async () => void render(<RouteGuard />, {preloadedState: mockReduxState}));
-		const pageComponent = screen.queryByText("Generic Page");
-		const accessDeniedComponent = screen.queryByText("Access Denied");
-		expect(pageComponent).toBeTruthy();
+    await act(async () => {
+      render(<Guard />, { preloadedState: mockReduxState });
+    });
+    const pageComponent = screen.queryByText('Generic Page');
+    const accessDeniedComponent = screen.queryByTestId('access-denied');
+    expect(pageComponent).toBeTruthy();
     expect(accessDeniedComponent).toBeNull();
   });
 
   it('No user logged', async () => {
-    await act(async () => void render(<RouteGuard />));
-		const pageComponent = screen.queryByText("Generic Page");
-		const accessDeniedComponent = screen.queryByText("Access Denied");
-		const deniedLogged = screen.queryByText("sì è loggato");
-		const deniedNotLogged = screen.queryByText("non è loggato");
+    await act(async () => {
+      render(<Guard />);
+    });
+    const pageComponent = screen.queryByText('Generic Page');
+    const accessDeniedComponent = screen.queryByTestId('access-denied');
     expect(pageComponent).toBeNull();
-		expect(accessDeniedComponent).toBeTruthy();
-    expect(deniedLogged).toBeNull();
-		expect(deniedNotLogged).toBeTruthy();
+    expect(accessDeniedComponent).toBeTruthy();
   });
 });
-
--- packages/pn-personagiuridica-webapp/src/navigation/__test__/navigation.utility.test.ts
+++ packages/pn-personagiuridica-webapp/src/navigation/__test__/navigation.utility.test.ts
@@ -1,29 +1,26 @@
 // momentarily commented for pn-5157
 // import { AppRouteType } from '@pagopa-pn/pn-commons';
+import { getConfiguration } from '../../services/configuration.service';
 import { goToLoginPortal } from '../navigation.utility';
-import { getConfiguration } from "../../services/configuration.service";
 
 const replaceFn = jest.fn();
 
 describe('Tests navigation utility methods', () => {
-  const { location } = window;
+  const original = window.location;
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: { href: '', replace: replaceFn },
+    });
+  });
 
   beforeEach(() => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    delete window.location;
-    // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-    // @ts-ignore
-    window.location = {
-      href: '',
-      replace: replaceFn,
-    };
-    jest.restoreAllMocks();
-    jest.resetAllMocks();
+    jest.clearAllMocks();
   });
 
   afterAll((): void => {
-    window.location = location;
+    Object.defineProperty(window, 'location', { writable: true, value: original });
   });
 
   it('goToLoginPortal', () => {
@@ -36,18 +33,19 @@ describe('Tests navigation utility methods', () => {
     expect(replaceFn).toBeCalledWith(`${getConfiguration().URL_FE_LOGOUT}`);
   });
 
-  // momentarily commented for pn-5157
-  /*
-  it('goToLoginPortal - aar', () => {
-    goToLoginPortal(AppRouteType.PG, 'fake-aar-token');
-    expect(replaceFn).toBeCalledTimes(1);
-    expect(replaceFn).toBeCalledWith(`${getConfiguration().URL_FE_LOGOUT}?type=PG&aar=fake-aar-token`);
-  });
+  // it('goToLoginPortal - aar', () => {
+  //   goToLoginPortal('fake-aar-token');
+  //   expect(replaceFn).toBeCalledTimes(1);
+  //   expect(replaceFn).toBeCalledWith(
+  //     `${getConfiguration().URL_FE_LOGOUT}?type=PG&aar=fake-aar-token`
+  //   );
+  // });
 
-  it('goToLoginPortal - aar', () => {
-    goToLoginPortal(AppRouteType.PG, '<script>malicious code</script>malicious-aar-token');
-    expect(replaceFn).toBeCalledTimes(1);
-    expect(replaceFn).toBeCalledWith(`${getConfiguration().URL_FE_LOGOUT}?type=PG&aar=malicious-aar-token`);
-  });
-  */
+  // it('goToLoginPortal - aar', () => {
+  //   goToLoginPortal('<script>malicious code</script>malicious-aar-token');
+  //   expect(replaceFn).toBeCalledTimes(1);
+  //   expect(replaceFn).toBeCalledWith(
+  //     `${getConfiguration().URL_FE_LOGOUT}?type=PG&aar=malicious-aar-token`
+  //   );
+  // });
 });
