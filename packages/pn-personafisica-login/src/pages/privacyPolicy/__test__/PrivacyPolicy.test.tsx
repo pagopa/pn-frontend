69954fce7744958f7ecf1c76ab8d29a8b00f27c8
Andrea Cimini <andrea.cimini@pagopa.it>
2023-09-13 13:57:29 +0200

    fixed failing test

--- packages/pn-personafisica-login/src/pages/privacyPolicy/__test__/PrivacyPolicy.test.tsx
+++ packages/pn-personafisica-login/src/pages/privacyPolicy/__test__/PrivacyPolicy.test.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { compileOneTrustPath } from '@pagopa-pn/pn-commons';
 import { render } from '@testing-library/react';
 
+import { getConfiguration } from '../../../services/configuration.service';
 import PrivacyPolicy from '../PrivacyPolicy';
 
 describe('test the Privacy Policy page', () => {
@@ -24,9 +25,13 @@ describe('test the Privacy Policy page', () => {
   });
 
   it('check that Privacy Policy page container is rendered', () => {
+    const { ONE_TRUST_DRAFT_MODE, ONE_TRUST_PP } = getConfiguration();
     const result = render(<PrivacyPolicy />);
     expect(loadNoticesFn).toBeCalledTimes(1);
-    expect(loadNoticesFn).toBeCalledWith([compileOneTrustPath('mocked-id')], false);
+    expect(loadNoticesFn).toBeCalledWith(
+      [compileOneTrustPath(ONE_TRUST_PP, ONE_TRUST_DRAFT_MODE)],
+      false
+    );
     expect(result.getByRole('article')).toBeInTheDocument();
   });
 });
