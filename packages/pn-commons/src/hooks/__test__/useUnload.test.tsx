cc01846ea4532289ffe8e17b92d153cae4910ecc
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2023-09-18 15:00:46 +0200

    test: useunload test

---
+++ packages/pn-commons/src/hooks/__test__/useUnload.test.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { unmountComponentAtNode } from "react-dom";
+import { useUnload } from "../useUnload";
+import { render } from '../../test-utils';
+
+describe("useUnload", () => {
+
+    it("should call the provided function on 'beforeunload'", () => {
+        const mockCallback = jest.fn();
+
+        const { unmount } = render(<MockComponent fn={mockCallback} />);
+
+        // Trigger the beforeunload event
+        window.dispatchEvent(new Event('beforeunload'));
+
+        // Expect the callback to have been called
+        expect(mockCallback).toHaveBeenCalled();
+
+        // Trigger the 'beforeunload' event listener
+        unmount();
+        // Trigger the beforeunload event again
+        window.dispatchEvent(new Event('beforeunload'));
+
+        // Expect the callback not to have been called this time (therefore it has been called only once during test execution)
+        expect(mockCallback).toHaveBeenCalledTimes(1);
+    });
+});
+
+function MockComponent({ fn }) {
+    useUnload(fn);
+    return <div>Mock Component</div>;
+}
--- packages/pn-commons/src/hooks/useUnload.ts
+++ packages/pn-commons/src/hooks/useUnload.ts
@@ -1,13 +1,36 @@
 import { useEffect, useRef } from "react";
 
-export const useUnload = (fn: any) => {
-    const cb = useRef(fn);
-  
-    useEffect(() => {
-      const onUnload = cb.current;
-      window.addEventListener('beforeunload', onUnload);
-      return () => {
-        window.removeEventListener('beforeunload', onUnload);
-      };
-    }, [cb]);
+/**
+ * This hook is designed to allow you to execute a function when 
+ * the user is about to navigate away from the current page or close 
+ * the browser tab/window. 
+ * It uses the beforeunload event to accomplish this.
+ * @example
+ * import { useUnload } from './useUnload';
+    function MyComponent() {
+      useUnload(() => {
+        // Your cleanup or confirmation logic here
+      });
+
+      // Rest of your component code
+    }
+ * @param {() => any} fn:() => any
+ * @returns {any}
+ */
+export const useUnload = (fn: () => any) => {
+  const cb = useRef(fn);
+
+  useEffect(() => {
+    const onUnload = cb.current;
+
+    const handleBeforeUnload = () => {
+      onUnload();
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, [cb]);
 };
