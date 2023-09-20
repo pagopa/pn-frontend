f22f58f92b8f93961a7690a128b8f6ddcb728698
NicolaGiornetta <105795630+NicolaGiornetta@users.noreply.github.com>
2023-09-20 12:45:06 +0200

    refactor(PN-3911): explicit DOM element detection in links replacement for Privacy page (#984)

--- packages/pn-commons/src/utils/index.ts
+++ packages/pn-commons/src/utils/index.ts
@@ -18,6 +18,7 @@ import {
   tenYearsAgo,
   today,
 } from './date.utility';
+import { waitForElement } from './dom.utility';
 import { calcUnit8Array } from './file.utility';
 import { filtersApplied, getValidValue, sortArray } from './genericFunctions.utility';
 import { IUN_regex, formatIun } from './iun.utility';
@@ -109,4 +110,5 @@ export {
   sortArray,
   calcUnit8Array,
   lazyRetry,
+  waitForElement,
 };
