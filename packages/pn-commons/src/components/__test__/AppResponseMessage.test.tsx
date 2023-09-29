52a139e4f13fdd42f198f689f6bc9983747e4a08
Andrea Cimini <andrea.cimini@pagopa.it>
2023-09-29 12:17:17 +0200

    wrote missing test

--- packages/pn-commons/src/components/__test__/AppResponseMessage.test.tsx
+++ packages/pn-commons/src/components/__test__/AppResponseMessage.test.tsx
@@ -1,1 +1,44 @@
-describe('AppResponseMessage Component', () => {});
+import React from 'react';
+
+import { createAppMessage } from '../../services/message.service';
+import { render, waitFor } from '../../test-utils';
+import { AppResponse, ServerResponseErrorCode } from '../../types/AppResponse';
+import { AppResponsePublisher } from '../../utility';
+import AppResponseMessage from '../AppResponseMessage';
+
+describe('AppResponseMessage Component', () => {
+  const action = 'Event';
+
+  const appErrorResponse: AppResponse = {
+    action,
+    status: 400,
+    traceId: 'TraceId-A',
+    timestamp: '2022-10-05',
+    errors: [
+      {
+        code: ServerResponseErrorCode.BAD_REQUEST_ERROR,
+        message: { title: 'Error', content: 'This is a test error message' },
+      },
+    ],
+  };
+
+  it('dispatch errors', async () => {
+    // render component
+    const { testStore } = render(<AppResponseMessage />);
+    let state = testStore.getState().appState;
+    expect(state.messages.errors).toStrictEqual([]);
+    // publish the event
+    AppResponsePublisher.error.publish(action, appErrorResponse);
+    // check the store
+    await waitFor(() => {
+      state = testStore.getState().appState;
+      const message = createAppMessage(
+        appErrorResponse.errors![0].message.title,
+        appErrorResponse.errors![0].message.content,
+        undefined,
+        action
+      );
+      expect(state.messages.errors).toStrictEqual([{ ...message, id: '1' }]);
+    });
+  });
+});
