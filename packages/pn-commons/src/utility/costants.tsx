4b69cc6c8d5024e199627763f41cf2ba84f1946a
Giornetta Nicola <nicola.giornetta@dgsspa.com>
2023-10-12 13:49:43 +0200

    Removed French and Slovenian languages

--- packages/pn-commons/src/utility/costants.tsx
+++ packages/pn-commons/src/utility/costants.tsx
@@ -7,9 +7,7 @@ declare const OneTrust: any;
 export const LANGUAGES: Languages = {
   it: { it: 'Italiano', en: 'Inglese', fr: 'Francese', de: 'Tedesco', sl: 'Sloveno' },
   en: { it: 'Italian', en: 'English', fr: 'French', de: 'German', sl: 'Slovenian' },
-  fr: { it: 'Italien', en: 'Anglais', fr: 'Français', de: 'Allemand', sl: 'Slovène' },
   de: { it: 'Italienisch', en: 'Englisch', fr: 'Französisch', de: 'Deutsch', sl: 'Slowenisch' },
-  sl: { it: 'Italijansko', en: 'Angleško', fr: 'Francosko', de: 'Nemško', sl: 'Slovensko' },
 };
 
 export const PRIVACY_LINK_RELATIVE_PATH = '/informativa-privacy';
