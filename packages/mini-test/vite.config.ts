93e98c87dc618b295ec7800df268158a6956f106
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2023-10-17 12:28:13 +0200

    launched a mini test using pn-validator, works OK

--- packages/mini-test/vite.config.ts
+++ packages/mini-test/vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
   return {
-    plugins: [react(), basicSsl()],
-    server: {
-      host: env.MINI_TEST_HOST,
-      https: true,
-      port: 443,
-    }
+    plugins: [react()]
   };
 });
