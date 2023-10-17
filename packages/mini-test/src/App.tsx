18fac8bb1ebfd3bf983090a2bebf3dc2794c7226
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2023-10-17 12:37:47 +0200

    launched a mini test using pn-validator and pn-commons, including mui-italia; works OK

--- packages/mini-test/src/App.tsx
+++ packages/mini-test/src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
-// import { EmptyState, KnownSentiment, formatEurocentToCurrency, } from '@pagopa-pn/pn-commons';
+import { EmptyState, KnownSentiment, formatEurocentToCurrency, } from '@pagopa-pn/pn-commons';
 import { Person, PersonValidator } from "./validator/PersonValidator";
 
 const badPerson: Person = {
@@ -21,9 +21,9 @@ export function App() {
 
   return <div style={{ display: 'flex', flexDirection: 'column'}}>
     <div>Validation result signals {validationResult ? Object.keys(validationResult).length : 0} errors</div>
-    {/* <div style={{ marginBottom: "2rem" }}>The amout is {formatEurocentToCurrency(8100)}</div>
+    <div style={{ marginBottom: "2rem" }}>The amout is {formatEurocentToCurrency(8100)}</div>
     <EmptyState sentimentIcon={KnownSentiment.SATISFIED}>
       <span>dentro l'empty state</span>
-    </EmptyState> */}
+    </EmptyState>
   </div>;
 }
--- packages/mini-test/src/main.tsx
+++ packages/mini-test/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// import { CssBaseline, ThemeProvider } from '@mui/material';
-// import { theme } from '@pagopa/mui-italia';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from '@pagopa/mui-italia';
 import { App } from './App';
 
 const container = document.getElementById('root');
@@ -9,10 +9,10 @@ const root = createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
-      {/* <CssBaseline /> */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
-    {/* </ThemeProvider> */}
+    </ThemeProvider>
   </React.StrictMode>
 );
 
