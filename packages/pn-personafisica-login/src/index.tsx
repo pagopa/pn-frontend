3631fd3504a1fe8720d8200a46c5d9744278239f
Andrea Cimini <andrea.cimini@pagopa.it>
2023-11-02 11:19:17 +0100

    added cdn script for 403 and 404 fallbacks

--- packages/pn-personafisica-login/src/index.tsx
+++ packages/pn-personafisica-login/src/index.tsx
@@ -22,7 +22,7 @@ async function doTheRender() {
 
     ReactDOM.render(
       <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <Suspense fallback={'loading...'}>
