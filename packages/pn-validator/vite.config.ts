81752c970bd4032a10b2ac14c1c62a79d097cb34
Carlos Lombardi <ca.lombardi@mediaticaspa.it>
2023-11-08 13:39:19 +0100

    coverage and report OK in pn-validator, all references to jest eliminated

--- packages/pn-validator/vite.config.ts
+++ packages/pn-validator/vite.config.ts
@@ -6,6 +6,12 @@ export default defineConfig(() => {
   return {
     test: {
       globals: true,
+      reporters: ['vitest-sonar-reporter', 'default'],
+      outputFile: 'test-report.xml',
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'json', 'html'],
+      }
     }
   };
 });
