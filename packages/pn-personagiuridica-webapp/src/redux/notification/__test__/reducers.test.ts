e53a46be64a59397f643da7f9de56288e032b068
alessandrogelmi <alessandro.gelmi@cinetix-team.it>
2023-12-07 17:12:45 +0100

    fix: reset cache only where necessary

--- packages/pn-personagiuridica-webapp/src/redux/notification/__test__/reducers.test.ts
+++ packages/pn-personagiuridica-webapp/src/redux/notification/__test__/reducers.test.ts
@@ -97,7 +97,6 @@ describe('Notification detail redux state tests', () => {
   });
 
   afterEach(() => {
-    sessionStorage.removeItem(PAYMENT_CACHE_KEY);
     mock.reset();
   });
 
@@ -288,6 +287,7 @@ describe('Notification detail redux state tests', () => {
   });
 
   it('Should be able to fetch payment info and not replace the payment if is equal', async () => {
+    sessionStorage.removeItem(PAYMENT_CACHE_KEY);
     const mockedStore = createMockedStore({
       notificationState: {
         notification: notificationToFe,
