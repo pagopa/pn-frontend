6c803c3a85b9747900b1435684fc4e3861974479
Andrea Cimini <andrea.cimini@pagopa.it>
2024-01-10 09:01:33 +0100

    removed base url configuration

--- packages/pn-personafisica-login/src/setupTests.ts
+++ packages/pn-personafisica-login/src/setupTests.ts
@@ -2,9 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import 'whatwg-fetch';
 import { Configuration } from '@pagopa-pn/pn-commons';
+import '@testing-library/jest-dom';
 
 beforeAll(async () => {
   Configuration.setForTest<any>({
