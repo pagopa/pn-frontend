2cd6801c926d7ac3bf6a23b09028d3be22f97888
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2024-01-11 11:43:35 +0100

    refactor: refactored basepath according to vitejs config

--- packages/pn-personafisica-login/vite.config.ts
+++ packages/pn-personafisica-login/vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/auth/',
     plugins: [react(), basicSsl(), splitVendorChunkPlugin()],
     server: {
       host: env.HOST,
