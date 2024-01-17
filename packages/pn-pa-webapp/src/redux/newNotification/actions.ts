042800c31b4f0e435909312fa40af10575bb1476
Sarah Donvito <sarah.donvito@pagopa.it>
2024-01-17 16:54:49 +0100

    fix(PN-9145): catched error when protocol number is already used during creation of notification in PA.

--- packages/pn-pa-webapp/src/redux/newNotification/actions.ts
+++ packages/pn-pa-webapp/src/redux/newNotification/actions.ts
@@ -164,8 +164,12 @@ export const uploadNotificationPaymentDocument = createAsyncThunk<
 
 export const createNewNotification = createAsyncThunk<NewNotificationResponse, NewNotification>(
   'createNewNotification',
-  performThunkAction(async (notification: NewNotification) => {
-    const mappedNotification = newNotificationMapper(notification);
-    return await NotificationsApi.createNewNotification(mappedNotification);
-  })
+  async (notification: NewNotification, { rejectWithValue }) => {
+    try {
+      const mappedNotification = newNotificationMapper(notification);
+      return await NotificationsApi.createNewNotification(mappedNotification);
+    } catch (e) {
+      return rejectWithValue(e);
+    }
+  }
 );
