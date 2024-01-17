8d1eac4f3fc564132e9096460c6f9fb0e1a5781d
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2024-01-17 18:16:08 +0100

    chore: enabled mixpanel tracking for portale login

--- packages/pn-personafisica-login/src/utility/mixpanel.ts
+++ packages/pn-personafisica-login/src/utility/mixpanel.ts
@@ -7,15 +7,7 @@ import { TrackEventType, events } from './events';
  * @param trackEventType event name
  * @param attributes event attributes
  */
-export const trackEventByType = (trackEventType: TrackEventType, attributes?: object) => 
-  // PN-9008 - turn off Mixpanel tracking in pf-personafisica-login
-  // I leave a nonsense implementation in order to avoid compilation errors
-  trackEventType && attributes;
-
-
-// PN-9008 - The actual implementation is kept in a separate function in order to ease its recovering,
-//           in particular to leave the imports.
-export const trackEventByTypeReal = (trackEventType: TrackEventType, attributes?: object) => {
+export const trackEventByType = (trackEventType: TrackEventType, attributes?: object) => {
   const eventParameters = attributes
     ? { ...events[trackEventType], ...attributes }
     : events[trackEventType];
