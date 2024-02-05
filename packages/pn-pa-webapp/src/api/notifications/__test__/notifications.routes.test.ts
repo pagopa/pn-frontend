a3e5546913f3e541288b21b50a929e12788e9ddf
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2024-02-05 12:44:29 +0100

    feat: added new version segment for notification detail and creation API

--- packages/pn-pa-webapp/src/api/notifications/__test__/notifications.routes.test.ts
+++ packages/pn-pa-webapp/src/api/notifications/__test__/notifications.routes.test.ts
@@ -29,7 +29,7 @@ describe('Notifications routes', () => {
 
   it('should compile NOTIFICATION_DETAIL', () => {
     const route = NOTIFICATION_DETAIL('mocked-iun');
-    expect(route).toEqual('/delivery/v2.1/notifications/sent/mocked-iun');
+    expect(route).toEqual('/delivery/v2.3/notifications/sent/mocked-iun');
   });
 
   it('should compile NOTIFICATION_DETAIL_DOCUMENTS', () => {
@@ -65,7 +65,7 @@ describe('Notifications routes', () => {
 
   it('should compile CREATE_NOTIFICATION', () => {
     const route = CREATE_NOTIFICATION();
-    expect(route).toEqual('/delivery/v2.1/requests');
+    expect(route).toEqual('/delivery/v2.3/requests');
   });
 
   it('should compile NOTIFICATION_PAYMENT_ATTACHMENT', () => {
