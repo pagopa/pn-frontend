cf8df031f08c5f0a9b824213d6c6533ff8eb86ce
Carlotta Dimatteo <carlotta.dimatteo@pagopa.it>
2024-02-13 16:28:46 +0100

    chore(pn-9774): changed property type for SEND_AUTH_SUCCESS event (#1133)

--- packages/pn-personafisica-webapp/src/navigation/SessionGuard.tsx
+++ packages/pn-personafisica-webapp/src/navigation/SessionGuard.tsx
@@ -191,7 +191,7 @@ const SessionGuard = () => {
           // ----------------------
           // Andrea Cimini, 2023.01.27
           // ----------------------
-          trackEventByType(TrackEventType.SEND_AUTH_SUCCESS);
+          trackEventByType(TrackEventType.SEND_AUTH_SUCCESS, { from_qrcode: false });
           navigate({ pathname: routes.NOTIFICHE, search: location.search }, { replace: true });
         } else {
           const hashAsObject = new URLSearchParams(location.hash);
@@ -206,7 +206,7 @@ const SessionGuard = () => {
           // ----------------------
           // Carlos Lombardi, 2022.12.27
           // ----------------------
-          trackEventByType(TrackEventType.SEND_AUTH_SUCCESS, { source: "QRcode" });
+          trackEventByType(TrackEventType.SEND_AUTH_SUCCESS, { from_qrcode: true });
           const newHash = hashAsObject.toString().replace(/%23/g, '#');
           navigate(
             { pathname: location.pathname, search: location.search, hash: newHash },
