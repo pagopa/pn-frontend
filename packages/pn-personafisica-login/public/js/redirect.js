48420870516e8c9511fdbfec0da688c96921b16a
Alessandro Gelmi <alessandro.gelmi@cinetix-team.it>
2024-03-19 11:07:24 +0100

    feat(pn-10199): Meta tag noindex  for environments lower than prod (#1157)

--- packages/pn-personafisica-login/public/js/redirect.js
+++ packages/pn-personafisica-login/public/js/redirect.js
@@ -1,10 +1,10 @@
-const regex = new RegExp('https://login\.((dev|test|uat|hotfix)?\.?)notifichedigitali\.it');
+const regex = new RegExp('https://login.((dev|test|uat|hotfix)?.?)notifichedigitali.it');
 const origin = window.origin;
 if (regex.test(origin)) {
+  const matches = origin.match(regex);
+  const newloginElements = ['https://cittadini.', matches[1], 'notifichedigitali.it/auth'];
 
-    const matches = origin.match(regex);
-    const newloginElements = ["https://cittadini.", matches[1], "notifichedigitali.it/auth"];
-
-    const newlogin = newloginElements.join('');
-    document.write(`<meta http-equiv="refresh" content="0; url=${newlogin}">`);
+  const newlogin = newloginElements.join('');
+  document.write(`<meta http-equiv="refresh" content="0; url=${newlogin}">`);
+  document.write(`<meta name="robots" content="noindex">`);
 }
