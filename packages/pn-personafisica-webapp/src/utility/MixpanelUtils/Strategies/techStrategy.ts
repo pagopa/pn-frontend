e102fee8684a7378bf0ff25ff17bc2b9247befcb
Alessandro Gelmi <alessandro.gelmi@cinetix-team.it>
2024-03-22 11:38:35 +0100

    Delete packages/pn-personafisica-webapp/src/utility/MixpanelUtils/Strategies/techStrategy.ts

--- packages/pn-personafisica-webapp/src/utility/MixpanelUtils/Strategies/techStrategy.ts
+++
@@ -1,9 +0,0 @@
-import { EventCategory, EventStrategy, TrackedEvent } from '@pagopa-pn/pn-commons';
-
-export class TechStrategy implements EventStrategy {
-  performComputations(): TrackedEvent {
-    return {
-      event_category: EventCategory.TECH,
-    };
-  }
-}
