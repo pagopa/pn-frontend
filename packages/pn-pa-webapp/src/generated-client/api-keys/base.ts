594ae0104d2ff23d5d29a9730bcb6c6931f7c962
Andrea Cimini <andrea.cimini@pagopa.it>
2024-04-22 09:58:58 +0200

    regenerated clients and models

--- packages/pn-pa-webapp/src/generated-client/api-keys/base.ts
+++ packages/pn-pa-webapp/src/generated-client/api-keys/base.ts
@@ -1,7 +1,5 @@
 /* tslint:disable */
-
 /* eslint-disable */
-
 /**
  * PN BFF BE Microservice - ApiKeys Mittenti
  * Documentation APIs v1.0
@@ -13,24 +11,25 @@
  * https://openapi-generator.tech
  * Do not edit the class manually.
  */
-// Some imports not used depending on template conditions
-// @ts-ignore
-import type { AxiosInstance, AxiosPromise, RawAxiosRequestConfig } from 'axios';
-import globalAxios from 'axios';
+
 
 import type { Configuration } from './configuration';
+// Some imports not used depending on template conditions
+// @ts-ignore
+import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
+import globalAxios from 'axios';
 
-export const BASE_PATH = 'https://webapi.notifichedigitali.it'.replace(/\/+$/, '');
+export const BASE_PATH = "https://webapi.notifichedigitali.it".replace(/\/+$/, "");
 
 /**
  *
  * @export
  */
 export const COLLECTION_FORMATS = {
-  csv: ',',
-  ssv: ' ',
-  tsv: '\t',
-  pipes: '|',
+    csv: ",",
+    ssv: " ",
+    tsv: "\t",
+    pipes: "|",
 };
 
 /**
@@ -39,8 +38,8 @@ export const COLLECTION_FORMATS = {
  * @interface RequestArgs
  */
 export interface RequestArgs {
-  url: string;
-  options: RawAxiosRequestConfig;
+    url: string;
+    options: RawAxiosRequestConfig;
 }
 
 /**
@@ -49,19 +48,15 @@ export interface RequestArgs {
  * @class BaseAPI
  */
 export class BaseAPI {
-  protected configuration: Configuration | undefined;
+    protected configuration: Configuration | undefined;
 
-  constructor(
-    configuration?: Configuration,
-    protected basePath: string = BASE_PATH,
-    protected axios: AxiosInstance = globalAxios
-  ) {
-    if (configuration) {
-      this.configuration = configuration;
-      this.basePath = configuration.basePath ?? basePath;
+    constructor(configuration?: Configuration, protected basePath: string = BASE_PATH, protected axios: AxiosInstance = globalAxios) {
+        if (configuration) {
+            this.configuration = configuration;
+            this.basePath = configuration.basePath ?? basePath;
+        }
     }
-  }
-}
+};
 
 /**
  *
@@ -70,21 +65,22 @@ export class BaseAPI {
  * @extends {Error}
  */
 export class RequiredError extends Error {
-  constructor(public field: string, msg?: string) {
-    super(msg);
-    this.name = 'RequiredError';
-  }
+    constructor(public field: string, msg?: string) {
+        super(msg);
+        this.name = "RequiredError"
+    }
 }
 
 interface ServerMap {
-  [key: string]: {
-    url: string;
-    description: string;
-  }[];
+    [key: string]: {
+        url: string,
+        description: string,
+    }[];
 }
 
 /**
  *
  * @export
  */
-export const operationServerMap: ServerMap = {};
+export const operationServerMap: ServerMap = {
+}
