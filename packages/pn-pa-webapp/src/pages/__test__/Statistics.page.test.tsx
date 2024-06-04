2299f092c790e42b21994d099f28ee94b567be0c
Maurizio Flauti <maurizio.flauti@cinetix-team.it>
2024-06-04 20:25:59 +0200

    test: add test case to verify the api error handling for Statistics.page

--- packages/pn-pa-webapp/src/pages/__test__/Statistics.page.test.tsx
+++ packages/pn-pa-webapp/src/pages/__test__/Statistics.page.test.tsx
@@ -1,7 +1,12 @@
+import MockAdapter from 'axios-mock-adapter';
 import { vi } from 'vitest';
 
-import { render } from '../../__test__/test-utils';
+import { AppResponseMessage, ResponseEventDispatcher } from '@pagopa-pn/pn-commons';
+
+import { RenderResult, act, render, screen } from '../../__test__/test-utils';
+import { apiClient } from '../../api/apiClients';
 import Statistics from '../Statistics.page';
+import { statisticsMockResponse } from './statistics.mock';
 
 vi.mock('react-i18next', () => ({
   // this mock makes sure any components using the translate hook can use it without a warning being shown
@@ -12,6 +17,21 @@ vi.mock('react-i18next', () => ({
 
 // TODO enable this test after completing the feature PN-10851
 describe('Statistics Page tests', () => {
+  let result: RenderResult;
+  let mock: MockAdapter;
+
+  beforeAll(() => {
+    mock = new MockAdapter(apiClient);
+  });
+
+  afterEach(() => {
+    mock.reset();
+    vi.clearAllMocks();
+  });
+
+  afterAll(() => {
+    mock.restore();
+  });
   it.skip('renders Statistics Page', () => {
     const result = render(<Statistics />, {
       preloadedState: {
@@ -21,5 +41,20 @@ describe('Statistics Page tests', () => {
 
     expect(result.container).toHaveTextContent('title');
     expect(result.container).toHaveTextContent('subtitle');
+  });
+
+  it('api return error', async () => {
+    mock.onGet('/bff/v1/sender-dashboard/dashboard-data-request').reply(500);
+    await act(async () => {
+      result = render(
+        <>
+          <ResponseEventDispatcher />
+          <AppResponseMessage />
+          <Statistics />
+        </>
+      );
+    });
+    expect(result.container).toHaveTextContent('error-fetch');
+    screen.debug();
   });
 });
