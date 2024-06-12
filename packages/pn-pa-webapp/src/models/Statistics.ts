8c9790102135d5b32cb772c3247d26c12e65cdb0
Maurizio Flauti <maurizio.flauti@cinetix-team.it>
2024-06-12 18:16:44 +0200

    fix: set default value for statistics filter to 12 months fix: arrange quick choices of statistics filter in descending order

--- packages/pn-pa-webapp/src/models/Statistics.ts
+++ packages/pn-pa-webapp/src/models/Statistics.ts
@@ -201,10 +201,10 @@ export enum WEEK_DAYS {
 }
 
 export const SelectedStatisticsFilter = {
-  lastMonth: 'lastMonth',
-  last3Months: 'last3Months',
-  last6Months: 'last6Months',
   last12Months: 'last12Months',
+  last6Months: 'last6Months',
+  last3Months: 'last3Months',
+  lastMonth: 'lastMonth',
   custom: 'custom',
 } as const;
 
