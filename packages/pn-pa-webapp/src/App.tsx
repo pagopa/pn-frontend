696ef4478c38cb6cdfd1e8b494a9aad3f262b7b9
Andrea Cimini <andrea.cimini@pagopa.it>
2024-07-11 12:38:46 +0200

    fake commit to run sonarcloud analysis (sixth time)

--- packages/pn-pa-webapp/src/App.tsx
+++ packages/pn-pa-webapp/src/App.tsx
@@ -45,7 +45,7 @@ const App = () => {
   const { t } = useTranslation(['common', 'notifiche']);
   const [isInitialized, setIsInitialized] = useState(false);
 
-  console.log('app loaded five times');
+  console.log('app loaded six times');
 
   useEffect(() => {
     if (!isInitialized) {
