798606f55dc4422ce9b19fecdc474fd41a2588c0
alessandrogelmi <a.gelmi@codermine.com>
2024-07-23 14:29:02 +0200

    fix: load lang param on every page of pf-login

--- packages/pn-personafisica-login/src/App.tsx
+++ packages/pn-personafisica-login/src/App.tsx
@@ -2,7 +2,12 @@ import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Box } from '@mui/material';
-import { initLocalization, useMultiEvent, useTracking } from '@pagopa-pn/pn-commons';
+import {
+  getSessionLanguage,
+  initLocalization,
+  useMultiEvent,
+  useTracking,
+} from '@pagopa-pn/pn-commons';
 
 import Router from './navigation/routes';
 import { getConfiguration } from './services/configuration.service';
@@ -10,21 +15,27 @@ import './utility/onetrust';
 
 const App = () => {
   const configuration = useMemo(() => getConfiguration(), []);
-  
+
   useTracking(configuration.MIXPANEL_TOKEN, process.env.NODE_ENV);
 
   const [clickVersion] = useMultiEvent({
     callback: () => console.log(`v${configuration.VERSION}`),
   });
 
-  const { t } = useTranslation(['common']);
+  const { t, i18n } = useTranslation(['common']);
   const [isInitialized, setIsInitialized] = useState(false);
+
+  const handleSetUserLanguage = async () => {
+    const language = getSessionLanguage() || 'it';
+    await i18n.changeLanguage(language);
+  };
 
   useEffect(() => {
     if (!isInitialized) {
       setIsInitialized(true);
       // init localization
       initLocalization((namespace, path, data) => t(path, { ns: namespace, ...data }));
+      void handleSetUserLanguage();
     }
   }, [isInitialized]);
 
