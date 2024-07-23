7f5f7cc8f42770f3f7edb449d5045b094687460e
alessandrogelmi <a.gelmi@codermine.com>
2024-07-23 16:27:38 +0200

    fix: update login button label on error page

--- packages/pn-personafisica-login/src/pages/loginError/LoginError.tsx
+++ packages/pn-personafisica-login/src/pages/loginError/LoginError.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from 'react-i18next';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { Box, Button, Dialog, Typography } from '@mui/material';
-import { getLocalizedOrDefaultLabel } from '@pagopa-pn/pn-commons/src/utility/localization.utility';
 import { IllusError } from '@pagopa/mui-italia';
 
 import { PFLoginEventsType } from '../../models/PFLoginEventsType';
@@ -94,7 +93,7 @@ const LoginError = () => {
           {getErrorMessage()}
         </Typography>
         <Button id="login-button" variant="contained" onClick={goToLogin}>
-          {getLocalizedOrDefaultLabel('common', 'button.go-to-login', 'Accedi')}
+          {t('loginPage.loginBox.title')}
         </Button>
       </Box>
     </Dialog>
