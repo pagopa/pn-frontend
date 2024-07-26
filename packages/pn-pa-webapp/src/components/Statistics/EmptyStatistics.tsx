59fe6250b4bb0503cdac57aff4a191dd4da1562d
alessandrogelmi <a.gelmi@codermine.com>
2024-07-26 15:13:57 +0200

    fix: rework copy of not enough data on sender dashboard

--- packages/pn-pa-webapp/src/components/Statistics/EmptyStatistics.tsx
+++ packages/pn-pa-webapp/src/components/Statistics/EmptyStatistics.tsx
@@ -1,17 +1,58 @@
 /* eslint-disable functional/immutable-data */
 import React from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
-import { Box, Stack, SxProps, Theme, Typography } from '@mui/material';
+import { Box, Link, Stack, SxProps, Theme, Typography } from '@mui/material';
 import { IllusStatistics } from '@pagopa-pn/pn-commons';
+
+import * as routes from '../../navigation/routes.const';
 
 type Props = {
   description?: string;
   sx?: SxProps<Theme>;
 };
 
+type LinkDashboardProps = {
+  children?: React.ReactNode;
+};
+
+const LinkDashboard: React.FC<LinkDashboardProps> = ({ children }) => {
+  const { t } = useTranslation(['statistics']);
+  const navigate = useNavigate();
+
+  return (
+    <Link
+      component="button"
+      variant="body1"
+      id="link-dashboard"
+      key="link-dashboard"
+      data-testid="link-dashboard"
+      aria-label={t('empty.notification_section_aria_label')}
+      onClick={() => navigate(routes.DASHBOARD)}
+      sx={{ display: 'inline', verticalAlign: 'unset', fontSize: 'inherit' }}
+    >
+      {children}
+    </Link>
+  );
+};
+
 const EmptyStatistics: React.FC<Props> = ({ description = 'empty.no_data_found', sx }) => {
   const { t } = useTranslation(['statistics']);
+
+  const getMessage = (description: string) => {
+    if (description === 'empty.not_enough_data') {
+      return (
+        <Trans
+          ns="statistics"
+          i18nKey="empty.not_enough_data"
+          components={[<LinkDashboard key="link-dashboard" />]}
+        />
+      );
+    }
+
+    return t(description);
+  };
 
   return (
     <Stack
@@ -22,7 +63,7 @@ const EmptyStatistics: React.FC<Props> = ({ description = 'empty.no_data_found',
     >
       <Box sx={{ textAlign: 'center' }}>
         <Typography sx={{ mt: 3, mb: 5 }} variant="body1" color="text.primary">
-          {t(description)}
+          {getMessage(description)}
         </Typography>
       </Box>
       <Box data-testid="empty-image" textAlign={'center'}>
--- packages/pn-pa-webapp/src/components/Statistics/__test__/EmptyStatistics.test.tsx
+++ packages/pn-pa-webapp/src/components/Statistics/__test__/EmptyStatistics.test.tsx
@@ -1,5 +1,29 @@
-import { render } from '../../../__test__/test-utils';
+import { ReactNode } from 'react';
+import { vi } from 'vitest';
+
+import { fireEvent, render } from '../../../__test__/test-utils';
+import * as routes from '../../../navigation/routes.const';
 import EmptyStatistics from '../EmptyStatistics';
+
+const mockNavigateFn = vi.fn();
+
+vi.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => ({
+    t: (str: string) => str,
+    i18n: { language: 'it' },
+  }),
+  Trans: (props: { i18nKey: string; components?: Array<ReactNode> }) => (
+    <>
+      {props.i18nKey} {props.components!.map((c) => c)}
+    </>
+  ),
+}));
+
+vi.mock('react-router-dom', async () => ({
+  ...(await vi.importActual<any>('react-router-dom')),
+  useNavigate: () => mockNavigateFn,
+}));
 
 describe('EmptyStatistics component tests', () => {
   it('renders the component', () => {
@@ -15,6 +39,24 @@ describe('EmptyStatistics component tests', () => {
     const { container, getByTestId } = render(<EmptyStatistics description="custom-description" />);
 
     expect(container).toHaveTextContent('custom-description');
+
+    const emptyImg = getByTestId('empty-image');
+    expect(emptyImg).toBeInTheDocument();
+  });
+
+  it('renders the component with not enough data description and link to dashboard', () => {
+    const { container, getByTestId } = render(
+      <EmptyStatistics description="empty.not_enough_data" />
+    );
+
+    expect(container).toHaveTextContent('empty.not_enough_data');
+
+    const link = getByTestId('link-dashboard');
+    expect(link).toBeInTheDocument();
+
+    fireEvent.click(link);
+    expect(mockNavigateFn).toBeCalledTimes(1);
+    expect(mockNavigateFn).toBeCalledWith(routes.DASHBOARD);
 
     const emptyImg = getByTestId('empty-image');
     expect(emptyImg).toBeInTheDocument();
