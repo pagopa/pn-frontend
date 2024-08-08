e7b53ec5b4eaab8d2928ca8c016549cac5991f3d
alessandrogelmi <a.gelmi@codermine.com>
2024-08-08 14:23:05 +0200

    fix: rework CourtesyContactItem

--- packages/pn-personafisica-webapp/src/components/Contacts/CourtesyContactItem.tsx
+++ packages/pn-personafisica-webapp/src/components/Contacts/CourtesyContactItem.tsx
@@ -1,23 +1,28 @@
 import { useFormik } from 'formik';
 import { ChangeEvent, useRef, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import * as yup from 'yup';
 
-import { Button, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { Box, Button, InputAdornment, Stack, TextField, Typography } from '@mui/material';
+import { CodeModal, DisclaimerModal, ErrorMessage, appStateActions } from '@pagopa-pn/pn-commons';
+import { ButtonNaked } from '@pagopa/mui-italia';
 
 import { PFEventsType } from '../../models/PFEventsType';
 import { AddressType, ChannelType } from '../../models/contacts';
-import { deleteAddress } from '../../redux/contact/actions';
-import { useAppDispatch } from '../../redux/hooks';
+import { createOrUpdateAddress, deleteAddress } from '../../redux/contact/actions';
+import { SaveDigitalAddressParams } from '../../redux/contact/types';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { RootState } from '../../redux/store';
 import PFEventStrategyFactory from '../../utility/MixpanelUtils/PFEventStrategyFactory';
 import {
+  contactAlreadyExists,
   emailValidationSchema,
   internationalPhonePrefix,
   phoneValidationSchema,
 } from '../../utility/contacts.utility';
 import DeleteDialog from './DeleteDialog';
 import DigitalContactElem from './DigitalContactElem';
-import { useDigitalContactsCodeVerificationContext } from './DigitalContactsCodeVerification.context';
+import ExistingContactDialog from './ExistingContactDialog';
 
 interface Props {
   type: ChannelType.EMAIL | ChannelType.SMS;
@@ -25,13 +30,28 @@ interface Props {
   blockDelete?: boolean;
 }
 
+enum ModalType {
+  EXISTING = 'existing',
+  DISCLAIMER = 'disclaimer',
+  CODE = 'code',
+}
+
 const CourtesyContactItem = ({ type, value, blockDelete }: Props) => {
   const { t } = useTranslation(['common', 'recapiti']);
-  const { initValidation } = useDigitalContactsCodeVerificationContext();
   const contactType = type.toLowerCase();
-  const digitalElemRef = useRef<{ editContact: () => void }>({ editContact: () => {} });
+  const digitalElemRef = useRef<{ editContact: () => void; toggleEdit: () => void }>({
+    editContact: () => {},
+    toggleEdit: () => {},
+  });
   const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [modalOpen, setModalOpen] = useState<ModalType | null>(null);
   const dispatch = useAppDispatch();
+  const labelType = type === ChannelType.EMAIL ? 'email' : 'sms';
+  const codeModalRef =
+    useRef<{ updateError: (error: ErrorMessage, codeNotValid: boolean) => void }>(null);
+
+  const digitalAddresses =
+    useAppSelector((state: RootState) => state.contactsState.digitalAddresses) ?? [];
 
   const emailValidationSch = yup.object().shape({
     email: emailValidationSchema(t),
@@ -52,17 +72,27 @@ const CourtesyContactItem = ({ type, value, blockDelete }: Props) => {
     validateOnMount: true,
     validationSchema: type === ChannelType.EMAIL ? emailValidationSch : phoneValidationSch,
     onSubmit: () => {
-      const contactValue =
-        type === ChannelType.EMAIL
-          ? formik.values[contactType]
-          : internationalPhonePrefix + formik.values[contactType];
-      if (value) {
-        digitalElemRef.current.editContact();
-      } else {
-        initValidation(type, contactValue, 'default');
+      const contactValue = getContactValue(type);
+      PFEventStrategyFactory.triggerEvent(
+        type === ChannelType.SMS
+          ? PFEventsType.SEND_ADD_SMS_START
+          : PFEventsType.SEND_ADD_EMAIL_START,
+        'default'
+      );
+
+      if (contactAlreadyExists(digitalAddresses, contactValue, 'default', type)) {
+        setModalOpen(ModalType.EXISTING);
+        return;
       }
+
+      setModalOpen(ModalType.DISCLAIMER);
     },
   });
+
+  const getContactValue = (type: ChannelType) =>
+    type === ChannelType.EMAIL
+      ? formik.values[contactType]
+      : internationalPhonePrefix + formik.values[contactType];
 
   const handleChangeTouched = async (
     event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
@@ -79,6 +109,11 @@ const CourtesyContactItem = ({ type, value, blockDelete }: Props) => {
     if (status === 'cancelled') {
       await formik.setFieldValue(contactType, value, true);
     }
+  };
+
+  const handleCodeModalClose = () => {
+    codeModalRef.current?.updateError({ title: '', content: '' }, false);
+    setModalOpen(null);
   };
 
   const deleteConfirmHandler = () => {
@@ -99,6 +134,64 @@ const CourtesyContactItem = ({ type, value, blockDelete }: Props) => {
             : PFEventsType.SEND_REMOVE_SMS_SUCCESS,
           'default'
         );
+      })
+      .catch(() => {});
+  };
+
+  const handleCodeVerification = (verificationCode?: string) => {
+    if (verificationCode) {
+      PFEventStrategyFactory.triggerEvent(
+        type === ChannelType.SMS
+          ? PFEventsType.SEND_ADD_SMS_UX_CONVERSION
+          : PFEventsType.SEND_ADD_EMAIL_UX_CONVERSION,
+        'default'
+      );
+    }
+
+    const digitalAddressParams: SaveDigitalAddressParams = {
+      addressType: AddressType.COURTESY,
+      senderId: 'default',
+      senderName: 'default',
+      channelType: type,
+      value: getContactValue(type),
+      code: verificationCode,
+    };
+
+    dispatch(createOrUpdateAddress(digitalAddressParams))
+      .unwrap()
+      .then((res) => {
+        // contact to verify
+        // open code modal
+        if (!res) {
+          // aprire la code modal
+          setModalOpen(ModalType.CODE);
+          return;
+        }
+
+        PFEventStrategyFactory.triggerEvent(
+          type === ChannelType.SMS
+            ? PFEventsType.SEND_ADD_SMS_UX_SUCCESS
+            : PFEventsType.SEND_ADD_EMAIL_UX_SUCCESS,
+          'default'
+        );
+
+        // contact has already been verified
+        // show success message
+        dispatch(
+          appStateActions.addSuccess({
+            title: '',
+            message: t(
+              `courtesy-contacts.${type === ChannelType.SMS ? 'sms' : 'email'}-added-successfully`,
+              {
+                ns: 'recapiti',
+              }
+            ),
+          })
+        );
+        // chiudere la code modal
+        setModalOpen(null);
+        // nel caso siamo in modifica (property value defined), bisogna passare alla modalità noEdit del componente DigitalContactElem
+        // return;
       })
       .catch(() => {});
   };
@@ -204,6 +297,64 @@ const CourtesyContactItem = ({ type, value, blockDelete }: Props) => {
         handleModalClose={() => setShowDeleteModal(false)}
         confirmHandler={deleteConfirmHandler}
         blockDelete={blockDelete}
+      />
+
+      <ExistingContactDialog
+        isConfirmationModalVisible={modalOpen === ModalType.EXISTING}
+        value={formik.values[contactType]}
+        handleDiscard={() => setModalOpen(null)}
+        handleConfirm={() => handleCodeVerification()}
+      />
+
+      {modalOpen === ModalType.DISCLAIMER && (
+        <DisclaimerModal
+          onConfirm={() => {
+            setModalOpen(null);
+            handleCodeVerification();
+          }}
+          onCancel={() => setModalOpen(null)}
+          confirmLabel={t('button.conferma')}
+          checkboxLabel={t('button.capito')}
+          content={t(`alert-dialog-${type}`, { ns: 'recapiti' })}
+        />
+      )}
+
+      <CodeModal
+        title={
+          t(`courtesy-contacts.${labelType}-verify`, { ns: 'recapiti' }) +
+          ` ${formik.values[contactType]}`
+        }
+        subtitle={<Trans i18nKey={`courtesy-contacts.${labelType}-verify-descr`} ns="recapiti" />}
+        open={modalOpen === ModalType.CODE}
+        initialValues={new Array(5).fill('')}
+        codeSectionTitle={t(`courtesy-contacts.insert-code`, { ns: 'recapiti' })}
+        codeSectionAdditional={
+          <>
+            <Typography variant="body2" display="inline">
+              {t(`courtesy-contacts.${labelType}-new-code`, { ns: 'recapiti' })}
+              &nbsp;
+            </Typography>
+            <ButtonNaked
+              component={Box}
+              onClick={() => handleCodeVerification(undefined)}
+              sx={{ verticalAlign: 'unset', display: 'inline' }}
+            >
+              <Typography
+                display="inline"
+                color="primary"
+                variant="body2"
+                sx={{ textDecoration: 'underline' }}
+              >
+                {t(`courtesy-contacts.new-code-link`, { ns: 'recapiti' })}.
+              </Typography>
+            </ButtonNaked>
+          </>
+        }
+        cancelLabel={t('button.annulla')}
+        confirmLabel={t('button.conferma')}
+        cancelCallback={handleCodeModalClose}
+        confirmCallback={(values: Array<string>) => handleCodeVerification(values.join(''))}
+        ref={codeModalRef}
       />
     </>
   );
