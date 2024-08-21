9d6d86700a740cab85cc28deb2663db03b183989
alessandrogelmi <a.gelmi@codermine.com>
2024-08-21 11:34:11 +0200

    fix: i18n config on pf-login

--- packages/pn-personafisica-login/src/i18n.ts
+++ packages/pn-personafisica-login/src/i18n.ts
@@ -3,7 +3,7 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
-i18next
+void i18next
   .use(LanguageDetector)
   .use(initReactI18next)
   .use(HttpApi)
@@ -11,7 +11,6 @@ i18next
     backend: {
       loadPath: '/auth/locales/{{lng}}/{{ns}}.json',
     },
-    lng: 'it',
     fallbackLng: 'it',
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
@@ -23,12 +22,6 @@ i18next
       lookupQuerystring: 'lang',
       lookupSessionStorage: 'lang',
     },
-  })
-  .then(async () => {
-    await i18next.changeLanguage();
-  })
-  .catch((err: any) => {
-    throw new Error(err);
   });
 
 export default i18next;
