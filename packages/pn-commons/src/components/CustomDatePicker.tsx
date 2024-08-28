fdb2d232e67f9c1e7f271a3311c0cdb13bb2d423
leleOFA <emanuele.didonato@pagopa.it>
2024-08-28 10:42:22 +0200

    fix: use one component

--- packages/pn-commons/src/components/CustomDatePicker.tsx
+++ packages/pn-commons/src/components/CustomDatePicker.tsx
@@ -1,16 +1,9 @@
 /* eslint-disable sonarjs/no-identical-functions */
 import { de, enGB, fr, it, sl } from 'date-fns/locale';
 
-import {
-  DesktopDatePicker,
-  DesktopDatePickerProps,
-  LocalizationProvider,
-  MobileDatePicker,
-  MobileDatePickerProps,
-} from '@mui/x-date-pickers';
+import { DatePicker, DatePickerProps, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
-import { useIsMobile } from '../hooks';
 import { getLocalizedOrDefaultLabel } from '../utility/localization.utility';
 
 const locales: { [key: string]: Locale } = {
@@ -23,67 +16,34 @@ const locales: { [key: string]: Locale } = {
 
 export type DatePickerTypes = Date | null;
 const CustomDatePicker = (
-  props: (DesktopDatePickerProps<DatePickerTypes> | MobileDatePickerProps<DatePickerTypes>) &
+  props: DatePickerProps<DatePickerTypes> &
     React.RefAttributes<HTMLDivElement> & { language?: string }
 ) => {
   const language = props.language ? props.language : 'it';
-  const isMobile = useIsMobile();
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={locales[`${language}`]}>
-      {isMobile ? (
-        <MobileDatePicker
-          {...props}
-          localeText={{
-            toolbarTitle: '',
-            openPreviousView: getLocalizedOrDefaultLabel('common', 'date-picker.left-arrow'),
-            openNextView: getLocalizedOrDefaultLabel('common', 'date-picker.right-arrow'),
-            calendarViewSwitchingButtonAriaLabel: (view) =>
-              view === 'year'
-                ? getLocalizedOrDefaultLabel('common', 'date-picker.switch-to-calendar')
-                : getLocalizedOrDefaultLabel('common', 'date-picker.switch-to-year'),
-            openDatePickerDialogue: (value, utils) => {
-              if (value instanceof Date && !isNaN(value.getTime())) {
-                const date = utils.format(utils.date(value), 'fullDate');
-                return getLocalizedOrDefaultLabel(
-                  'common',
-                  'date-picker.date-selected',
-                  undefined,
-                  {
-                    date,
-                  }
-                );
-              }
-              return getLocalizedOrDefaultLabel('common', 'date-picker.select-date');
-            },
-          }}
-        />
-      ) : (
-        <DesktopDatePicker
-          {...props}
-          localeText={{
-            openPreviousView: getLocalizedOrDefaultLabel('common', 'date-picker.left-arrow'),
-            openNextView: getLocalizedOrDefaultLabel('common', 'date-picker.right-arrow'),
-            calendarViewSwitchingButtonAriaLabel: (view) =>
-              view === 'year'
-                ? getLocalizedOrDefaultLabel('common', 'date-picker.switch-to-calendar')
-                : getLocalizedOrDefaultLabel('common', 'date-picker.switch-to-year'),
-            openDatePickerDialogue: (value, utils) => {
-              if (value instanceof Date && !isNaN(value.getTime())) {
-                const date = utils.format(utils.date(value), 'fullDate');
-                return getLocalizedOrDefaultLabel(
-                  'common',
-                  'date-picker.date-selected',
-                  undefined,
-                  {
-                    date,
-                  }
-                );
-              }
-              return getLocalizedOrDefaultLabel('common', 'date-picker.select-date');
-            },
-          }}
-        />
-      )}
+      <DatePicker
+        {...props}
+        desktopModeMediaQuery="@media (pointer: fine)"
+        localeText={{
+          toolbarTitle: '',
+          openPreviousView: getLocalizedOrDefaultLabel('common', 'date-picker.left-arrow'),
+          openNextView: getLocalizedOrDefaultLabel('common', 'date-picker.right-arrow'),
+          calendarViewSwitchingButtonAriaLabel: (view) =>
+            view === 'year'
+              ? getLocalizedOrDefaultLabel('common', 'date-picker.switch-to-calendar')
+              : getLocalizedOrDefaultLabel('common', 'date-picker.switch-to-year'),
+          openDatePickerDialogue: (value, utils) => {
+            if (value instanceof Date && !isNaN(value.getTime())) {
+              const date = utils.format(utils.date(value), 'fullDate');
+              return getLocalizedOrDefaultLabel('common', 'date-picker.date-selected', undefined, {
+                date,
+              });
+            }
+            return getLocalizedOrDefaultLabel('common', 'date-picker.select-date');
+          },
+        }}
+      />
     </LocalizationProvider>
   );
 };
