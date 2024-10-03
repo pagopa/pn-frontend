4514bfb6a65570f8cf3c6df602ad9dede10f732d
alessandrogelmi <a.gelmi@codermine.com>
2024-10-03 10:18:41 +0200

    fix: done CRs and changed check on show public keys section

--- packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/PublicKeys.tsx
+++ packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/PublicKeys.tsx
@@ -2,46 +2,35 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { Button, Stack, Typography } from '@mui/material';
-import { EmptyState, KnownSentiment, useHasPermissions, useIsMobile } from '@pagopa-pn/pn-commons';
-
-import { PNRole } from '../../redux/auth/types';
-import { useAppSelector } from '../../redux/hooks';
-import { RootState } from '../../redux/store';
+import { EmptyState, KnownSentiment } from '@pagopa-pn/pn-commons';
 
 const PublicKeys: React.FC = () => {
   const { t } = useTranslation(['integrazioneApi']);
-  const isMobile = useIsMobile();
-  const currentUser = useAppSelector((state: RootState) => state.userState.user);
-  const role = currentUser.organization?.roles ? currentUser.organization?.roles[0] : null;
-  const userHasAdminPermissions = useHasPermissions(role ? [role.role] : [], [PNRole.ADMIN]);
-
-  const isAdminWithoutGroups = userHasAdminPermissions && !currentUser.hasGroup;
 
   return (
     <>
       <Stack
-        direction={isMobile ? 'column' : 'row'}
+        direction={{ xs: 'column', lg: 'row' }}
         sx={{
           justifyContent: 'space-between',
-          alignItems: 'center',
-          marginBottom: 3,
-          marginTop: 5,
+          alignItems: { xs: 'start', lg: 'center' },
+          mb: 3,
+          mt: 5,
         }}
       >
-        <Typography variant="h6" sx={{ marginBottom: isMobile ? 3 : undefined }}>
+        <Typography variant="h6" sx={{ mb: { xs: 3, lg: 0 } }}>
           {t('public-keys-title')}
         </Typography>
-        {isAdminWithoutGroups && (
-          <Button
-            id="generate-public-key"
-            data-testid="generatePublicKey"
-            variant="contained"
-            sx={{ marginBottom: isMobile ? 3 : undefined }}
-            //   onClick={handleGeneratePublicKey}
-          >
-            {t('new-public-key-button')}
-          </Button>
-        )}
+
+        <Button
+          id="generate-public-key"
+          data-testid="generatePublicKey"
+          variant="contained"
+          sx={{ mb: { xs: 3, lg: 0 } }}
+          //   onClick={handleGeneratePublicKey}
+        >
+          {t('new-public-key-button')}
+        </Button>
       </Stack>
 
       <EmptyState sentimentIcon={KnownSentiment.NONE}>{t('public-keys-empty-state')}</EmptyState>
