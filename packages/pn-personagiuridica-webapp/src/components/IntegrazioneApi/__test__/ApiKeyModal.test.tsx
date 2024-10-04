b2bf721d3249721408aebed33e7d570cf1f11915
alessandrogelmi <a.gelmi@codermine.com>
2024-10-04 15:02:47 +0200

    feat: add some tests to avoid build failure

--- packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/__test__/ApiKeyModal.test.tsx
+++ packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/__test__/ApiKeyModal.test.tsx
@@ -1,1 +1,32 @@
-describe('Api Key Modal', () => {});
+import { vi } from 'vitest';
+
+import { render, screen } from '../../../__test__/test-utils';
+import ApiKeyModal, { ApiKeyModalProps } from '../ApiKeyModal';
+
+const closeModalFn = vi.fn();
+const actionModalFn = vi.fn();
+
+const defaultProps: ApiKeyModalProps = {
+  title: 'mock-title',
+  subTitle: 'mock-subtitle',
+  content: <p>mocked-content</p>,
+  closeButtonLabel: 'mock-close',
+  closeModalHandler: closeModalFn,
+  actionButtonLabel: 'mock-action',
+  actionHandler: actionModalFn,
+};
+
+describe('Api Key Modal', () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it('render component', () => {
+    const { queryByTestId } = render(<ApiKeyModal {...defaultProps} />);
+    const dialog = screen.getByTestId('dialog');
+    expect(dialog).toHaveTextContent('mock-title');
+    expect(dialog).toHaveTextContent('mocked-content');
+    expect(queryByTestId('subtitle-top')).toBeInTheDocument();
+    expect(queryByTestId('subtitle-bottom')).not.toBeInTheDocument();
+  });
+});
--- packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/__test__/ApiKeysDataSwitch.test.tsx
+++ packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/__test__/ApiKeysDataSwitch.test.tsx
@@ -1,1 +1,37 @@
-describe('Api Keys Data Switch', () => {});
+import { vi } from 'vitest';
+
+import { Row } from '@pagopa-pn/pn-commons';
+
+import { publicKeys } from '../../../__mocks__/ApiKeys.mock';
+import { render } from '../../../__test__/test-utils';
+import { ApiKeyColumnData } from '../../../models/ApiKeys';
+import ApiKeysDataSwitch from '../ApiKeysDataSwitch';
+
+vi.mock('react-i18next', () => ({
+  // this mock makes sure any components using the translate hook can use it without a warning being shown
+  useTranslation: () => ({
+    t: (str: string) => str,
+  }),
+}));
+
+const data: Row<ApiKeyColumnData> = {
+  id: publicKeys.items[0].kid!,
+  name: publicKeys.items[0].name,
+  value: publicKeys.items[0].value,
+  date: publicKeys.items[0].createdAt,
+  status: publicKeys.items[0].status,
+  statusHistory: publicKeys.items[0].statusHistory,
+  menu: '',
+};
+
+describe('Api Keys Data Switch', () => {
+  const mockClick = vi.fn();
+
+  it('renders component - name', () => {
+    const { container } = render(
+      <ApiKeysDataSwitch handleModalClick={mockClick} keys={publicKeys} data={data} type="name" />
+    );
+    const regexp = new RegExp(`^${data.name}$`, 'ig');
+    expect(container).toHaveTextContent(regexp);
+  });
+});
