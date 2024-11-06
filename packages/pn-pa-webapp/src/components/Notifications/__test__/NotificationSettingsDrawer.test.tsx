0d1cd4188df8175b7699485f3991d9da973b6248
Francesco Bianchi <f.bianchi@codermine.com>
2024-11-06 15:10:42 +0100

    Drawer: add test of initial render

--- packages/pn-pa-webapp/src/components/Notifications/__test__/NotificationSettingsDrawer.test.tsx
+++ packages/pn-pa-webapp/src/components/Notifications/__test__/NotificationSettingsDrawer.test.tsx
@@ -1,9 +1,10 @@
+import { act } from 'react-dom/test-utils';
 import { vi } from 'vitest';
 
 import { testRadio } from '@pagopa-pn/pn-commons/src/test-utils';
 import userEvent from '@testing-library/user-event';
 
-import { RenderResult, act, render } from '../../../__test__/test-utils';
+import { RenderResult, render } from '../../../__test__/test-utils';
 import NotificationSettingsDrawer from '../NotificationSettingsDrawer';
 
 // mock imports
@@ -22,12 +23,41 @@ describe('Notification Settings Drawer', async () => {
     vi.clearAllMocks();
   });
 
-  it('should pre-select de language in form', async () => {
+  it('renders button component', async () => {
+    result = render(<NotificationSettingsDrawer />);
+
+    const settingsLangBtn = result.queryByTestId('settingsLangBtn');
+    expect(settingsLangBtn).toBeInTheDocument();
+  });
+
+  it('open drawer with default value', async () => {
+    result = render(<NotificationSettingsDrawer />);
+
+    const settingsLangBtn = result.queryByTestId('settingsLangBtn');
+    expect(settingsLangBtn).toBeInTheDocument();
+    userEvent.click(settingsLangBtn!);
+
     await act(async () => {
-      result = render(<NotificationSettingsDrawer />, {
-        preloadedState: { userState: { additionalLanguages: ['de'] } },
-      });
+      // time to appear the drawer
+      await new Promise((r) => setTimeout(r, 500));
     });
+
+    const drawer = document.querySelector("[data-testid='settingsLangDrawer']") as HTMLElement;
+    expect(drawer).toBeInTheDocument();
+
+    await testRadio(
+      drawer,
+      'notificationLanguageRadio',
+      ['Italiano', 'new-notification.steps.preliminary-informations.italian-and-other-language'],
+      0
+    );
+  });
+
+  it('should pre-select de language in form', async () => {
+    result = render(<NotificationSettingsDrawer />, {
+      preloadedState: { userState: { additionalLanguages: ['de'] } },
+    });
+
     const settingsLangBtn = result.queryByTestId('settingsLangBtn');
     expect(settingsLangBtn).toBeInTheDocument();
     userEvent.click(settingsLangBtn!);
