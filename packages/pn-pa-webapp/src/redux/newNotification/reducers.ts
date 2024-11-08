e8e1b8a0a382500bc75b7a063bbd5942b6f3bcc2
Francesco Bianchi <f.bianchi@codermine.com>
2024-11-08 15:17:16 +0100

    newNotification: remove unused reducer setAdditionalLang

--- packages/pn-pa-webapp/src/redux/newNotification/reducers.ts
+++ packages/pn-pa-webapp/src/redux/newNotification/reducers.ts
@@ -2,10 +2,8 @@ import { PhysicalCommunicationType } from '@pagopa-pn/pn-commons';
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 import {
-  BILINGUALISM_LANGUAGES,
   NewNotification,
   NewNotificationDocument,
-  NewNotificationLangOther,
   NewNotificationRecipient,
   NotificationFeePolicy,
   PaymentModel,
@@ -99,21 +97,6 @@ const newNotificationSlice = createSlice({
     setIsCompleted: (state) => {
       state.isCompleted = true;
     },
-    setNewNotificationLang: (state, action: PayloadAction<{ lang: string }>) => {
-      if (BILINGUALISM_LANGUAGES.includes(action.payload.lang)) {
-        state.notification = {
-          ...state.notification,
-          lang: NewNotificationLangOther,
-          additionalLang: action.payload.lang,
-        };
-        return;
-      }
-      state.notification = {
-        ...state.notification,
-        lang: 'it',
-        additionalLang: undefined,
-      };
-    },
     resetState: () => initialState,
   },
   extraReducers: (builder) => {
@@ -143,7 +126,6 @@ export const {
   setPaymentDocuments,
   resetState,
   setIsCompleted,
-  setNewNotificationLang,
 } = newNotificationSlice.actions;
 
 export default newNotificationSlice;
