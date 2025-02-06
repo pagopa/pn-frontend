a7cffa845182d22f01be8e6685f70b2888bb2456
leleOFA <emanuele.didonato@pagopa.it>
2025-02-06 11:53:16 +0100

    fix: cancel comment

--- packages/pn-commons/src/components/Data/SmartTable/__test__/SmartData.test.tsx
+++ packages/pn-commons/src/components/Data/SmartTable/__test__/SmartData.test.tsx
@@ -92,11 +92,7 @@ const RenderSmartData: React.FC<{
             <SmartBodyCell
               key={column.id.toString()}
               columnId={column.id}
-              tableProps={
-                {
-                  // onClick: column.tableConfiguration.onClick,
-                }
-              }
+              tableProps={{}}
               cardProps={column.cardConfiguration}
               isCardHeader={column.cardConfiguration?.isCardHeader}
               testId="rowCell"
