b972ff4d008d79caf2dc73057b97a09b62d03a0f
Francesco Bianchi <f.bianchi@codermine.com>
2025-02-19 12:23:24 +0100

    PF e PG: regex e test

--- packages/pn-personafisica-webapp/public/js/config.js
+++ packages/pn-personafisica-webapp/public/js/config.js
@@ -1,4 +1,4 @@
-const regex = new RegExp('https://cittadini.((dev|test|uat|hotfix).?)notifichedigitali.it');
+const regex = new RegExp('https://cittadini(.(dev|test|uat|hotfix))?.notifichedigitali.it');
 const origin = window.origin;
 if (regex.test(origin)) {
   document.write(`<meta name="robots" content="noindex">`);
--- packages/pn-personagiuridica-webapp/public/js/config.js
+++ packages/pn-personagiuridica-webapp/public/js/config.js
@@ -1,4 +1,4 @@
-const regex = new RegExp('https://imprese.((dev|test|uat|hotfix).?)notifichedigitali.it');
+const regex = new RegExp('https://imprese(.(dev|test|uat|hotfix))?.notifichedigitali.it');
 const origin = window.origin;
 if (regex.test(origin)) {
   document.write(`<meta name="robots" content="noindex">`);
