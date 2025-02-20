9faef071fae52ef43e5ad5a511ea31ce8473948a
Sarah Donvito <sarah.donvito@pagopa.it>
2025-02-20 11:45:15 +0100

    feat(PN-13921): replace spyOn with mockAdapter

--- packages/pn-pa-webapp/src/components/NewNotification/__test__/PaymentMethods.test.tsx
+++ packages/pn-pa-webapp/src/components/NewNotification/__test__/PaymentMethods.test.tsx
@@ -1,16 +1,9 @@
-import { Mock, vi } from 'vitest';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { vi } from 'vitest';
 
 import { newNotification } from '../../../__mocks__/NewNotification.mock';
-import {
-  RenderResult,
-  act,
-  fireEvent,
-  render,
-  waitFor,
-  within,
-} from '../../../__test__/test-utils';
-import { PaymentObject } from '../../../models/NewNotification';
-import * as actions from '../../../redux/newNotification/actions';
+import { RenderResult, act, fireEvent, render, within } from '../../../__test__/test-utils';
 import PaymentMethods from '../PaymentMethods';
 
 // mock imports
@@ -31,13 +24,11 @@ function uploadDocument(elem: HTMLElement) {
 
 describe('PaymentMethods Component', () => {
   let result: RenderResult;
-  let mockActionFn: Mock;
   const confirmHandlerMk = vi.fn();
 
   beforeEach(async () => {
-    // mock action
-    mockActionFn = vi.fn();
-    vi.spyOn(actions, 'uploadNotificationPaymentDocument').mockImplementation(mockActionFn);
+    const mock = new MockAdapter(axios);
+    mock.onPost('https://mocked-url.com').reply(200, { success: true });
 
     // render component
     await act(async () => {
@@ -97,63 +88,62 @@ describe('PaymentMethods Component', () => {
     // PN-1843 Carlotta Dimatteo 12/08/2022
   });
 
-  it.skip('adds all payment documents and clicks on confirm', async () => {
-    const form = result.container.querySelector('form');
-    const paymentBoxes = result.queryAllByTestId('paymentBox');
-    uploadDocument(paymentBoxes[0].parentElement!);
-    uploadDocument(paymentBoxes[1].parentElement!);
-    uploadDocument(paymentBoxes[2].parentElement!);
-    uploadDocument(paymentBoxes[3].parentElement!);
-    const buttons = await waitFor(() => form?.querySelectorAll('button'));
-    // Avendo cambiato posizione nella lista dei bottoni (in modo da avere sempre il bottone "continua" a dx, qui vado a prendere il primo bottone)
-    // vedi flexDirection row-reverse
-    // PN-1843 Carlotta Dimatteo 12/08/2022
-    expect(buttons![0]).toBeEnabled();
-    fireEvent.click(buttons![0]);
-    await waitFor(() => {
-      expect(mockActionFn).toBeCalledTimes(1);
-      expect(mockActionFn).toBeCalledWith(
-        newNotification.recipients.reduce((obj: { [key: string]: PaymentObject }, r, index) => {
-          obj[r.taxId] = {
-            pagoPa: {
-              id: index === 0 ? 'MRARSS90P08H501Q-pagoPaDoc' : 'SRAGLL00P48H501U-pagoPaDoc',
-              idx: 0,
-              name: 'pagopa-notice',
-              file: {
-                sha256: {
-                  hashBase64: 'mocked-hasBase64',
-                  hashHex: 'mocked-hashHex',
-                },
-                data: file,
-              },
-              contentType: 'application/pdf',
-              ref: {
-                key: '',
-                versionToken: '',
-              },
-            },
-            f24: {
-              id:
-                index === 0 ? 'MRARSS90P08H501Q-f24standardDoc' : 'SRAGLL00P48H501U-f24standardDoc',
-              idx: 0,
-              name: 'pagopa-notice-f24',
-              file: {
-                sha256: {
-                  hashBase64: 'mocked-hasBase64',
-                  hashHex: 'mocked-hashHex',
-                },
-                data: file,
-              },
-              contentType: 'application/pdf',
-              ref: {
-                key: '',
-                versionToken: '',
-              },
-            },
-          };
-          return obj;
-        }, {})
-      );
-    });
-  });
+  // it.skip('adds all payment documents and clicks on confirm', async () => {
+  //   const form = result.container.querySelector('form');
+  //   const paymentBoxes = result.queryAllByTestId('paymentBox');
+  //   uploadDocument(paymentBoxes[0].parentElement!);
+  //   uploadDocument(paymentBoxes[1].parentElement!);
+  //   uploadDocument(paymentBoxes[2].parentElement!);
+  //   uploadDocument(paymentBoxes[3].parentElement!);
+  //   const buttons = await waitFor(() => form?.querySelectorAll('button'));
+  //   // Avendo cambiato posizione nella lista dei bottoni (in modo da avere sempre il bottone "continua" a dx, qui vado a prendere il primo bottone)
+  //   // vedi flexDirection row-reverse
+  //   // PN-1843 Carlotta Dimatteo 12/08/2022
+  //   expect(buttons![0]).toBeEnabled();
+  //   fireEvent.click(buttons![0]);
+  //   await waitFor(() => {
+  //     expect(mockActionFn).toHaveBeenCalledTimes(1);
+  //     expect(mockActionFn).toHaveBeenCalledWith(
+  //       newNotification.recipients.reduce((obj: { [key: string]: PaymentObject }, r, index) => {
+  //         obj[r.taxId] = {
+  //           pagoPa: {
+  //             id: index === 0 ? 'MRARSS90P08H501Q-pagoPaDoc' : 'SRAGLL00P48H501U-pagoPaDoc',
+  //             idx: 0,
+  //             name: 'pagopa-notice',
+  //             file: {
+  //               sha256: {
+  //                 hashBase64: 'mocked-hasBase64',
+  //                 hashHex: 'mocked-hashHex',
+  //               },
+  //               data: file,
+  //             },
+  //             contentType: 'application/pdf',
+  //             ref: {
+  //               key: '',
+  //               versionToken: '',
+  //             },
+  //           },
+  //           f24: {
+  //             id: index === 0 ? 'MRARSS90P08H501Q-f24' : 'SRAGLL00P48H501U-f24',
+  //             idx: 0,
+  //             name: 'pagopa-notice-f24',
+  //             file: {
+  //               sha256: {
+  //                 hashBase64: 'mocked-hasBase64',
+  //                 hashHex: 'mocked-hashHex',
+  //               },
+  //               data: file,
+  //             },
+  //             contentType: 'application/json',
+  //             ref: {
+  //               key: '',
+  //               versionToken: '',
+  //             },
+  //           },
+  //         };
+  //         return obj;
+  //       }, {})
+  //     );
+  //   });
+  // });
 });
