5a748d5fb3c196e78cac7af7a8450675d57c8475
alessandrogelmi <a.gelmi@codermine.com>
2025-02-24 10:37:51 +0100

    wip on PublicKeys tests

--- packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/__test__/PublicKeys.test.tsx
+++ packages/pn-personagiuridica-webapp/src/components/IntegrazioneApi/__test__/PublicKeys.test.tsx
@@ -1,6 +1,8 @@
 import { vi } from 'vitest';
 
-import { render } from '../../../__test__/test-utils';
+import { publicKeys } from '../../../__mocks__/ApiKeys.mock';
+import { fireEvent, render } from '../../../__test__/test-utils';
+import { PublicKeyStatus } from '../../../generated-client/pg-apikeys';
 import PublicKeys from '../PublicKeys';
 
 describe('Public Keys', () => {
@@ -14,5 +16,57 @@ describe('Public Keys', () => {
 
     const button = getByTestId('generatePublicKey');
     expect(button).toBeInTheDocument();
+  });
+
+  it('render component with public key list and not show button if there is an active key', async () => {
+    const keys = {
+      ...publicKeys,
+      items: [
+        {
+          ...publicKeys.items[0],
+          status: PublicKeyStatus.Active,
+        },
+      ],
+    };
+
+    const { container, getByTestId, queryByTestId } = render(<PublicKeys />, {
+      preloadedState: {
+        apiKeysState: {
+          publicKeys: keys,
+        },
+      },
+    });
+    expect(container).not.toHaveTextContent(/empty-state/i);
+
+    const table = getByTestId('publicKeysTableDesktop');
+    expect(table).toBeInTheDocument();
+    const button = queryByTestId('generatePublicKey');
+    expect(button).not.toBeInTheDocument();
+  });
+
+  it('check view code modal', async () => {
+    const { getByTestId, getAllByTestId } = render(<PublicKeys />, {
+      preloadedState: {
+        apiKeysState: {
+          publicKeys,
+        },
+      },
+    });
+
+    const contextMenuButton = getAllByTestId('contextMenuButton')[0];
+    fireEvent.click(contextMenuButton);
+    const viewCodeButton = getByTestId('buttonView');
+    fireEvent.click(viewCodeButton);
+
+    const dialog = getByTestId('dialog');
+    expect(dialog).toBeInTheDocument();
+
+    expect(dialog).toHaveTextContent(/view-title/i);
+    expect(dialog).toHaveTextContent(/view-subtitle/i);
+
+    const closeButton = getByTestId('close-modal-button');
+    fireEvent.click(closeButton);
+
+    expect(dialog).not.toBeInTheDocument();
   });
 });
