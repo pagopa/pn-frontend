3514bf397d253fc334c14b4dd98768965225bc53
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2025-03-14 12:29:11 +0100

    feat: enhance mixpanel SEND_HAS_PEC and SEND_HAS_SERCQ_SEND properties settings fix: resolve party list not updating adding a special contact fix: improve AddSpecialContact layout fixing space between buttons in mobile mode

--- packages/pn-personafisica-webapp/src/utility/MixpanelUtils/Strategies/__test__/SendAddSercqSendUxSuccessStrategy.test.ts
+++ packages/pn-personafisica-webapp/src/utility/MixpanelUtils/Strategies/__test__/SendAddSercqSendUxSuccessStrategy.test.ts
@@ -6,16 +6,22 @@ describe('Mixpanel - Add Sercq SEND UX confirm success strategy', () => {
   it('should return add contact action event', () => {
     const strategy = new SendAddSercqSendUxSuccessStrategy();
 
-    const isSpecialContactEvent = strategy.performComputations('not-default');
+    const isSpecialContactEvent = strategy.performComputations(true);
     expect(isSpecialContactEvent).toEqual({
       [EventPropertyType.TRACK]: {
         event_category: EventCategory.UX,
         event_type: EventAction.CONFIRM,
-        other_contact: 'yes',
+        other_contact: 'no',
       },
+      [EventPropertyType.PROFILE]: {
+        SEND_HAS_PEC: 'no'
+      },
+      [EventPropertyType.SUPER_PROPERTY]: {
+        SEND_HAS_PEC: 'no',
+      }
     });
 
-    const isDefaultContactEvent = strategy.performComputations('default');
+    const isDefaultContactEvent = strategy.performComputations(false);
     expect(isDefaultContactEvent).toEqual({
       [EventPropertyType.TRACK]: {
         event_category: EventCategory.UX,
