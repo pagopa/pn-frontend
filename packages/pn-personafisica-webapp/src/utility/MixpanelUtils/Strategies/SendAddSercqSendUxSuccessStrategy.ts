84c4185c05e6dfc67bd16bf2961b20c2a202b99c
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2025-03-17 09:25:41 +0100

    chore: format unformatted file

--- packages/pn-personafisica-webapp/src/utility/MixpanelUtils/Strategies/SendAddSercqSendUxSuccessStrategy.ts
+++ packages/pn-personafisica-webapp/src/utility/MixpanelUtils/Strategies/SendAddSercqSendUxSuccessStrategy.ts
@@ -11,24 +11,26 @@ type SendHasAddresses = {
 };
 
 export class SendAddSercqSendUxSuccessStrategy implements EventStrategy {
-  performComputations(removePec: boolean): TrackedEvent<{ other_contact: 'yes' | 'no' } | SendHasAddresses> {
+  performComputations(
+    removePec: boolean
+  ): TrackedEvent<{ other_contact: 'yes' | 'no' } | SendHasAddresses> {
     const retVal = {
       [EventPropertyType.TRACK]: {
         event_category: EventCategory.UX,
         event_type: EventAction.CONFIRM,
         other_contact: 'no',
-      }
+      },
     };
 
-    if(removePec) {
+    if (removePec) {
       return {
         ...retVal,
         [EventPropertyType.PROFILE]: {
-          SEND_HAS_PEC: 'no'
+          SEND_HAS_PEC: 'no',
         },
         [EventPropertyType.SUPER_PROPERTY]: {
           SEND_HAS_PEC: 'no',
-        }
+        },
       };
     }
     return retVal;
