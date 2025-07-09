61fef6a082e31b35e1f3805742c3f260c84b00bf
Francesco Bianchi <105631409+fbianchicodermine@users.noreply.github.com>
2025-07-09 12:11:09 +0200

    fix(PN-15509): add TPP available type in courtesy message (#1583)

--- packages/pn-commons/src/utility/TimelineUtils/__test__/SendCourtesyMessageStep.test.ts
+++ packages/pn-commons/src/utility/TimelineUtils/__test__/SendCourtesyMessageStep.test.ts
@@ -8,11 +8,16 @@ describe('SendCourtesyMessageStep', () => {
     initLocalizationForTest();
   });
 
-  it('test getTimelineStepInfo app IO', () => {
+  it.each([
+    { type: DigitalDomicileType.APPIO, expectedType: 'app IO' },
+    { type: DigitalDomicileType.SMS, expectedType: 'sms' },
+    { type: DigitalDomicileType.EMAIL, expectedType: 'email' },
+    { type: DigitalDomicileType.TPP, expectedType: 'notifiche - detail.timeline.tpp-type' },
+  ])('test getTimelineStepInfo $type', ({ type, expectedType }) => {
     const timelineElem = getTimelineElem(TimelineCategory.SEND_COURTESY_MESSAGE, {
       digitalAddress: {
         address: '',
-        type: DigitalDomicileType.APPIO,
+        type,
       },
     });
     const payload = {
@@ -27,7 +32,7 @@ describe('SendCourtesyMessageStep', () => {
       description: `notifiche - detail.timeline.send-courtesy-message-description - ${JSON.stringify(
         {
           ...sendCourtesyMessageStep.nameAndTaxId(payload),
-          type: 'app IO',
+          type: expectedType,
         }
       )}`,
     });
@@ -39,81 +44,7 @@ describe('SendCourtesyMessageStep', () => {
       description: `notifiche - detail.timeline.send-courtesy-message-description-multirecipient - ${JSON.stringify(
         {
           ...sendCourtesyMessageStep.nameAndTaxId(payload),
-          type: 'app IO',
-        }
-      )}`,
-    });
-  });
-
-  it('test getTimelineStepInfo sms', () => {
-    const timelineElem = getTimelineElem(TimelineCategory.SEND_COURTESY_MESSAGE, {
-      digitalAddress: {
-        address: '3333333333',
-        type: DigitalDomicileType.SMS,
-      },
-    });
-    const payload = {
-      step: timelineElem,
-      recipient: notificationDTO.recipients[0],
-      isMultiRecipient: false,
-    };
-    const sendCourtesyMessageStep = new SendCourtesyMessageStep();
-    // mono recipient
-    expect(sendCourtesyMessageStep.getTimelineStepInfo(payload)).toStrictEqual({
-      label: `notifiche - detail.timeline.send-courtesy-message`,
-      description: `notifiche - detail.timeline.send-courtesy-message-description - ${JSON.stringify(
-        {
-          ...sendCourtesyMessageStep.nameAndTaxId(payload),
-          type: 'sms',
-        }
-      )}`,
-    });
-    // multi recipient
-    expect(
-      sendCourtesyMessageStep.getTimelineStepInfo({ ...payload, isMultiRecipient: true })
-    ).toStrictEqual({
-      label: `notifiche - detail.timeline.send-courtesy-message`,
-      description: `notifiche - detail.timeline.send-courtesy-message-description-multirecipient - ${JSON.stringify(
-        {
-          ...sendCourtesyMessageStep.nameAndTaxId(payload),
-          type: 'sms',
-        }
-      )}`,
-    });
-  });
-
-  it('test getTimelineStepInfo email', () => {
-    const timelineElem = getTimelineElem(TimelineCategory.SEND_COURTESY_MESSAGE, {
-      digitalAddress: {
-        address: 'nome.cognome@mail.it',
-        type: DigitalDomicileType.EMAIL,
-      },
-    });
-    const payload = {
-      step: timelineElem,
-      recipient: notificationDTO.recipients[0],
-      isMultiRecipient: false,
-    };
-    const sendCourtesyMessageStep = new SendCourtesyMessageStep();
-    // mono recipient
-    expect(sendCourtesyMessageStep.getTimelineStepInfo(payload)).toStrictEqual({
-      label: `notifiche - detail.timeline.send-courtesy-message`,
-      description: `notifiche - detail.timeline.send-courtesy-message-description - ${JSON.stringify(
-        {
-          ...sendCourtesyMessageStep.nameAndTaxId(payload),
-          type: 'email',
-        }
-      )}`,
-    });
-    // multi recipient
-    expect(
-      sendCourtesyMessageStep.getTimelineStepInfo({ ...payload, isMultiRecipient: true })
-    ).toStrictEqual({
-      label: `notifiche - detail.timeline.send-courtesy-message`,
-      description: `notifiche - detail.timeline.send-courtesy-message-description-multirecipient - ${JSON.stringify(
-        {
-          ...sendCourtesyMessageStep.nameAndTaxId(payload),
-          type: 'email',
+          type: expectedType,
         }
       )}`,
     });
