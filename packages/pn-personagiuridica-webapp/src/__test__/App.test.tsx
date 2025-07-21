70d94c36e12c07bebebdee91efe837c18aae5cdc
Francesco Bianchi <105631409+fbianchicodermine@users.noreply.github.com>
2025-07-21 11:34:03 +0200

    chore: add test for logout API called (#1593)

--- packages/pn-personagiuridica-webapp/src/__test__/App.test.tsx
+++ packages/pn-personagiuridica-webapp/src/__test__/App.test.tsx
@@ -9,7 +9,7 @@ import { currentStatusDTO } from '../__mocks__/AppStatus.mock';
 import { userResponse } from '../__mocks__/Auth.mock';
 import { tosPrivacyConsentMock } from '../__mocks__/Consents.mock';
 import { digitalAddresses } from '../__mocks__/Contacts.mock';
-import { apiClient } from '../api/apiClients';
+import { apiClient, authClient } from '../api/apiClients';
 import { DelegationStatus } from '../models/Deleghe';
 import { PNRole, PartyRole } from '../models/User';
 import { SELFCARE_LOGOUT } from '../navigation/routes.const';
@@ -46,12 +46,14 @@ const reduxInitialState = {
 
 describe('App', async () => {
   let mock: MockAdapter;
+  let mockAuth: MockAdapter;
   let result: RenderResult;
   const mockOpenFn = vi.fn();
   const originalOpen = window.open;
 
   beforeAll(() => {
     mock = new MockAdapter(apiClient);
+    mockAuth = new MockAdapter(authClient);
     // FooterPreLogin (mui-italia) component calls an api to fetch selfcare products list.
     // this causes an error, so we mock to avoid it
     global.fetch = () =>
@@ -67,11 +69,13 @@ describe('App', async () => {
 
   afterEach(() => {
     mock.reset();
+    mockAuth.reset();
     vi.clearAllMocks();
   });
 
   afterAll(() => {
     mock.restore();
+    mockAuth.restore();
     global.fetch = unmockedFetch;
     Object.defineProperty(window, 'open', { configurable: true, value: originalOpen });
   });
@@ -264,6 +268,8 @@ describe('App', async () => {
   });
 
   it('render component - user logs out', async () => {
+    mockAuth.onPost('/logout').reply(200);
+
     const clearSpy = vi.spyOn(Storage.prototype, 'clear');
 
     await act(async () => {
@@ -284,6 +290,7 @@ describe('App', async () => {
       const url = `${getConfiguration().SELFCARE_BASE_URL}${SELFCARE_LOGOUT}`;
       expect(mockOpenFn).toHaveBeenCalledWith(url, '_self');
       expect(clearSpy).toHaveBeenCalled();
+      expect(mockAuth.history.post.length).toBe(1);
     });
   });
 });
