f5a788b76abcfec1170af62702ecaa5a428c55c9
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2025-08-27 13:42:14 +0200

    fix(PN-13213): add hidden label to payment Radio button to improve component a11y (#1606)

---
+++ packages/pn-commons/src/components/NotificationDetail/NotificationPaymentPagoPa/NotificationPaymentPagoPaDescription.tsx
@@ -0,0 +1,50 @@
+import { Box, Typography } from '@mui/material';
+
+import { PagoPAPaymentFullDetails } from '../../../models';
+import { formatDate } from '../../../utility';
+import { getLocalizedOrDefaultLabel } from '../../../utility/localization.utility';
+
+type Props = {
+  pagoPAItem: PagoPAPaymentFullDetails;
+  isCancelled: boolean;
+};
+
+const NotificationPaymentPagoPaDescription: React.FC<Props> = ({ pagoPAItem, isCancelled }) => (
+  <Box flexGrow={1}>
+    {pagoPAItem.causaleVersamento && (
+      <Typography variant="sidenav" color="text.primary">
+        {pagoPAItem.causaleVersamento}
+      </Typography>
+    )}
+    <Box lineHeight="1.4rem">
+      <Typography variant="caption" color="text.secondary" mr={0.5}>
+        {getLocalizedOrDefaultLabel('notifications', 'detail.payment.notice-code')}
+      </Typography>
+      <Typography variant="caption-semibold" color="text.secondary">
+        {pagoPAItem.noticeCode}
+      </Typography>
+    </Box>
+    {isCancelled && (
+      <Box lineHeight="1.4rem">
+        <Typography variant="caption" color="text.secondary" mr={0.5}>
+          {getLocalizedOrDefaultLabel('notifications', 'detail.creditor-tax-id')}
+        </Typography>
+        <Typography variant="caption-semibold" color="text.secondary" data-testid="creditorTaxId">
+          {pagoPAItem.creditorTaxId}
+        </Typography>
+      </Box>
+    )}
+    {pagoPAItem.dueDate && (
+      <Box lineHeight="1.4rem">
+        <Typography variant="caption" color="text.secondary" mr={0.5}>
+          {getLocalizedOrDefaultLabel('notifications', 'detail.payment.due')}
+        </Typography>
+        <Typography variant="caption-semibold" color="text.secondary">
+          {formatDate(pagoPAItem.dueDate, false)}
+        </Typography>
+      </Box>
+    )}
+  </Box>
+);
+
+export default NotificationPaymentPagoPaDescription;
