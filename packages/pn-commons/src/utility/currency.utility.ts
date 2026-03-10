2de4b8a7fc9c0f7472fa52a1ec51068c1e1ac76f
Alessandro Gelmi <a.gelmi@codermine.com>
2026-03-10 14:01:19 +0100

    feat(PN-18582): implement bottom sheet with notification costs details (#1763)

--- packages/pn-commons/src/utility/currency.utility.ts
+++ packages/pn-commons/src/utility/currency.utility.ts
@@ -3,10 +3,24 @@ const euro = Intl.NumberFormat('it-IT', {
   currency: 'EUR',
 });
 
+const euroRounded = Intl.NumberFormat('it-IT', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+});
+
 export function formatCurrency(currency: number) {
   return euro.format(currency);
 }
 
-export function formatEurocentToCurrency(eurocent: number) {
-  return formatCurrency(eurocent / 100);
+export function formatEurocentToCurrency(eurocent: number, round = false) {
+  const value = eurocent / 100;
+  const hasDecimals = eurocent % 100 !== 0;
+
+  if (round && !hasDecimals) {
+    return euroRounded.format(value);
+  }
+
+  return euro.format(value);
 }
--- packages/pn-personagiuridica-webapp/src/pages/NotificationDetail.page.tsx
+++ packages/pn-personagiuridica-webapp/src/pages/NotificationDetail.page.tsx
@@ -78,9 +78,9 @@ const NotificationDetail = () => {
   const [pageReady, setPageReady] = useState(false);
   const {
     F24_DOWNLOAD_WAIT_TIME,
-    LANDING_SITE_URL,
     DOWNTIME_EXAMPLE_LINK,
     NOTIFICATION_CANCELLED_HELP_LINK,
+    NOTIFICATION_COST_DETAILS_ASSISTANCE_LINK,
   } = getConfiguration();
   const navigate = useNavigate();
 
@@ -477,7 +477,8 @@ const NotificationDetail = () => {
                         handleFetchPaymentsInfo={fetchPaymentsInfo}
                         getPaymentAttachmentAction={getPaymentAttachmentAction}
                         timerF24={F24_DOWNLOAD_WAIT_TIME}
-                        landingSiteUrl={LANDING_SITE_URL}
+                        costDetailsAssistanceLink={NOTIFICATION_COST_DETAILS_ASSISTANCE_LINK}
+                        costDetails={notification.notificationCostDetails}
                       />
                     </ApiErrorWrapper>
                   </Paper>
