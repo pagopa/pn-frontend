96ed747f37876a6088093d0f6389d2d87ba22b7a
Alessandro Gelmi <a.gelmi@codermine.com>
2026-03-27 14:31:48 +0100

    feat(PN-18982): adapt UI for support users by hiding routes and actions (#1796)

--- packages/pn-pa-webapp/src/api/auth/Auth.api.ts
+++ packages/pn-pa-webapp/src/api/auth/Auth.api.ts
@@ -4,28 +4,6 @@ import { User } from '../../models/user';
 import { authClient } from '../apiClients';
 import { AUTH_LOGOUT, AUTH_TOKEN_EXCHANGE } from './auth.routes';
 
-// const supportOrganization = {
-//   id: '5b994d4a-0fa8-47ac-9c7b-354f1d44a1ce',
-//   name: 'Comune di Palermo',
-//   roles: [
-//     {
-//       partyRole: PartyRole.SUPPORT,
-//       role: PNRole.SUPPORT,
-//     },
-//   ],
-//   fiscal_code: '80016350821',
-//   ipaCode: 'c_g273',
-// };
-
-// // eslint-disable-next-line functional/immutable-data
-// response.data = {
-//   uid: '4253a451-4b19-476f-8f8f-baa10f9934de',
-//   email: 'f.bianchi@codermine.com',
-//   organization: supportOrganization,
-//   desired_exp: response.data.desired_exp,
-//   sessionToken: response.data.sessionToken,
-// };
-
 export const AuthApi = {
   exchangeToken: (selfCareToken: string): Promise<User> =>
     authClient.post<User>(AUTH_TOKEN_EXCHANGE(), { authorizationToken: selfCareToken }).then(
