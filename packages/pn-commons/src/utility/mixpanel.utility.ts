9df1cfd162ac7b14c23a4ac34f19082b1970a700
Andrea Cimini <andrea.cimini@pagopa.it>
2026-04-22 14:05:54 +0200

    fix(PN-19560): remove drty data saved in localStorage

--- packages/pn-commons/src/utility/mixpanel.utility.ts
+++ packages/pn-commons/src/utility/mixpanel.utility.ts
@@ -14,6 +14,7 @@ export function mixpanelInit(mixpanelToken: string, nodeEnv: string): void {
     mixpanel.init(mixpanelToken, {
       api_host: 'https://api-eu.mixpanel.com',
       persistence: 'localStorage',
+      stop_utm_persistence: true,
       // if this is true, Mixpanel will automatically determine
       // City, Region and Country data using the IP address of
       // the client
@@ -25,5 +26,10 @@ export function mixpanelInit(mixpanelToken: string, nodeEnv: string): void {
       // function called after mixpanel has finished loading
     });
     mixpanel.identify(mixpanel.get_distinct_id());
+
+    // Remove dirty data saved on localStorage
+    mixpanel.unregister('utm_source');
+    mixpanel.unregister('utm_campaign');
+    mixpanel.unregister('utm_medium');
   }
 }
