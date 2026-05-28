3f85a33afba1591ab29da466c9082a4158fdeac5
alessandrogelmi <a.gelmi@codermine.com>
2026-05-28 12:30:00 +0200

    fix(PN-20069): redirect to login when error is already handled by OneIdentity

--- packages/pn-personafisica-login/src/pages/oneIdentityLoginError/__test__/OneIdentityLoginError.test.tsx
+++ packages/pn-personafisica-login/src/pages/oneIdentityLoginError/__test__/OneIdentityLoginError.test.tsx
@@ -7,16 +7,26 @@ import OneIdentityLoginError from '../OneIdentityLoginError';
 
 describe('OneIdentityLoginError component', () => {
   it('renders the error dialog with title and default message', () => {
-    render(<OneIdentityLoginError />);
+    render(<OneIdentityLoginError />, { route: '/?error=server_error' });
     const errorDialog = getById(document.body, 'oneIdentityErrorDialog');
     expect(errorDialog).toHaveTextContent('loginError.title');
     expect(getById(errorDialog, 'message')).toHaveTextContent('loginError.message');
   });
 
   it('navigates to login on button click', async () => {
-    const { router } = render(<OneIdentityLoginError />);
+    const { router } = render(<OneIdentityLoginError />, { route: '/?error=server_error' });
     fireEvent.click(getById(document.body, 'login-button'));
     await waitFor(() => expect(router.state.location.pathname).toBe(ROUTE_ONE_IDENTITY_LOGIN));
     expect(router.state.location.search).toBe('');
+  });
+
+  it('redirects to login immediately when error is unknown', async () => {
+    const { router } = render(<OneIdentityLoginError />, { route: '/?error=unknown_error' });
+    await waitFor(() => expect(router.state.location.pathname).toBe(ROUTE_ONE_IDENTITY_LOGIN));
+  });
+
+  it('redirects to login immediately when error param is missing', async () => {
+    const { router } = render(<OneIdentityLoginError />);
+    await waitFor(() => expect(router.state.location.pathname).toBe(ROUTE_ONE_IDENTITY_LOGIN));
   });
 });
