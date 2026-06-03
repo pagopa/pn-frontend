5cbe6b57c96d10e8dc03d15cd499ac2c91663a51
Sarah Donvito <sarah.donvito@pagopa.it>
2026-06-03 11:48:09 +0200

    add prop to tests

--- packages/pn-pa-webapp/src/redux/notification/__test__/reducers.test.ts
+++ packages/pn-pa-webapp/src/redux/notification/__test__/reducers.test.ts
@@ -24,6 +24,7 @@ import { resetState } from '../reducers';
 const initialState = {
   loading: false,
   notification: {
+    additionalLanguages: [],
     subject: '',
     recipients: [],
     documents: [],
