70b6e546a148a58028bb4bd6454b68af85590983
Gaetano Amato <gaetano.amato@pagopa.it>
2026-06-25 17:44:03 +0200

    feat(PN-20242): add initial ComBoDetail page structure(DRAFT)

--- packages/pn-pa-webapp/src/pages/ComBoDetail.page.tsx
+++ packages/pn-pa-webapp/src/pages/ComBoDetail.page.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const ComBoDetail: React.FC = () => <h1>COMBO WORKS</h1>;
+import { Box, Typography } from '@mui/material';
+import { PnBreadcrumb, TitleBox } from '@pagopa-pn/pn-commons';
+
+import * as routes from '../navigation/routes.const';
+
+const ComBoDetail: React.FC = () => {
+  const { t } = useTranslation(['common', 'notifiche']);
+
+  return (
+    <Box sx={{ p: { xs: 3, lg: 0 } }}>
+      <PnBreadcrumb
+        linkRoute={routes.DASHBOARD}
+        linkLabel={t('detail.breadcrumb-root', { ns: 'notifiche' })}
+        currentLocationLabel="Dettaglio comunicazione bonaria"
+        goBackLabel={t('button.indietro', { ns: 'common' })}
+      />
+
+      <TitleBox
+        variantTitle="h4"
+        title="Avviso di pagamento per la fornitura idrica"
+        sx={{ pt: 3, mb: 2 }}
+        mbTitle={0}
+      />
+
+      <Typography variant="body1" mb={{ xs: 3, md: 4 }} sx={{ overflowWrap: 'anywhere' }}>
+        Questa è una comunicazione bonaria di esempio.
+      </Typography>
+    </Box>
+  );
+};
 
 export default ComBoDetail;
