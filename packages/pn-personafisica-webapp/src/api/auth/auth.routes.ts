c5b1dab12110e05cb818a8d3564ccf34e31e566f
Francesco Bianchi <f.bianchi@codermine.com>
2026-07-06 09:13:22 +0200

    rename oidc api

--- packages/pn-personafisica-webapp/src/api/auth/auth.routes.ts
+++ packages/pn-personafisica-webapp/src/api/auth/auth.routes.ts
@@ -2,7 +2,7 @@ import { compileRoute } from '@pagopa-pn/pn-commons';
 
 // Segments
 const API_AUTH_TOKEN_EXCHANGE = 'token-exchange';
-const API_AUTH_ONE_IDENTITY_TOKEN_EXCHANGE = 'oidc-token';
+const API_AUTH_ONE_IDENTITY_TOKEN_EXCHANGE = 'oidc/token';
 const API_AUTH_LOGOUT = 'logout';
 
 // APIs
