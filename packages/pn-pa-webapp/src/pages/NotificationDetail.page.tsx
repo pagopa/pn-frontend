7d23f37a76650263151160626d1549eb28062622
Maurizio Flauti <68643346+mflauti@users.noreply.github.com>
2026-07-23 17:34:26 +0200

    remove forced disableDownloads from attachments

--- packages/pn-pa-webapp/src/pages/NotificationDetail.page.tsx
+++ packages/pn-pa-webapp/src/pages/NotificationDetail.page.tsx
@@ -428,7 +428,6 @@ const NotificationDetail: React.FC = () => {
                     downloadFilesLink={t('detail.download-files-link', { ns: 'notifiche' })}
                     titleVariant="h5"
                     inlineDownloadFilesMessage
-                    disableDownloads
                   />
                   {notification.radd && (
                     <MIAlert
