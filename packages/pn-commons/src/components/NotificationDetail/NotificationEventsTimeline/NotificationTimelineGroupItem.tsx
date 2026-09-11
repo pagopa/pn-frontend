c42e5b8264ec62b8228ad4e64b4461e38a8d23b4
Andrea Cimini <andrea.cimini@pagopa.it>
2026-09-11 18:19:51 +0200

    fixed layout

--- packages/pn-commons/src/components/NotificationDetail/NotificationEventsTimeline/NotificationTimelineGroupItem.tsx
+++ packages/pn-commons/src/components/NotificationDetail/NotificationEventsTimeline/NotificationTimelineGroupItem.tsx
@@ -61,10 +61,12 @@ const NotificationTimelineGroupItem = ({
       >
         <Stack
           component="span"
-          direction={{ xs: 'column', sm: 'row' }}
+          direction="row"
           spacing={1}
           alignItems={{ xs: 'flex-start', sm: 'center' }}
           minWidth={0}
+          useFlexGap
+          sx={{ flexWrap: 'wrap' }}
         >
           <ChannelIcon
             fontSize="small"
@@ -79,7 +81,7 @@ const NotificationTimelineGroupItem = ({
           >
             {headerLabel}
           </Typography>
-          {group.hasReworkedEvents && (
+          {!group.hasReworkedEvents && (
             <Tag
               variant="warning"
               value={getLocalizedOrDefaultLabel('notifications', 'status.reworked-status-group')}
@@ -102,7 +104,7 @@ const NotificationTimelineGroupItem = ({
           data-testid="timeline-group-body"
           sx={{
             listStyleType: 'disc',
-            pl: { xs: 3, sm: 10 },
+            pl: { xs: 3, sm: 3.5 },
             my: 1,
             display: 'grid',
             rowGap: 1,
